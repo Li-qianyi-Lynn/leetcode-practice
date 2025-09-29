a459f41825d85088e4b4601000b2434235b82f16
Qianyi Li <li.qianyi@northeastern.edu>
2025-09-29 12:46:03 -0700

    Add solution for Max Area of Island

---
+++ LeetCode/Max-Area-of-Island/Solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea,dfs(grid,i,j));
+
+                }
+
+            }
+
+        }
+        return maxArea;
+           
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length 
+        || j < 0 || j >= grid[0].length 
+        || grid[i][j] == 0 ) {
+            return 0;
+        }
+
+        grid[i][j] =0;
+
+        int area = 1;
+        area += dfs(grid, i+1, j); //down
+        area += dfs(grid, i, j+1); //right
+        area += dfs(grid, i-1, j); //up
+        area += dfs(grid, i, j-1); //left
+        
+        return area;
+
+
+    }
+    
+    
+}
