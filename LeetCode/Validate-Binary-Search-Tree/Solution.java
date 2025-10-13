31cb9b7f537f271a0c0d1dc8368561827f55e6ed
Qianyi Li <li.qianyi@northeastern.edu>
2025-10-12 23:58:38 -0700

    Add solution for Validate Binary Search Tree

---
+++ LeetCode/Validate-Binary-Search-Tree/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return checkNode(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean checkNode(TreeNode node, long min, long max) {
+        if (node == null){
+            return true;
+        }
+        if(node.val > min && node.val < max){
+            return (checkNode(node.left, min, node.val) && checkNode(node.right, node.val, max));
+        } else {
+            return false;
+        }
+    }
+}
