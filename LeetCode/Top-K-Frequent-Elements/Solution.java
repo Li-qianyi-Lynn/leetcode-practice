206992a7d581e71d40ac6966e11b2eef465bfa2b
Qianyi Li <li.qianyi@northeastern.edu>
2025-10-14 22:59:32 -0700

    Add solution for Top K Frequent Elements

--- LeetCode/Top-K-Frequent-Elements/Solution.java
+++ LeetCode/Top-K-Frequent-Elements/Solution.java
@@ -1,6 +1,30 @@
-def topKFrequent(self, nums, k):
-    bucket = [[] for _ in range(len(nums) + 1)]
-    Count = Counter(nums).items()  
-    for num, freq in Count: bucket[freq].append(num) 
-    flat_list = [item for sublist in bucket for item in sublist]
-    return flat_list[::-1][:k]
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        // use map to restore the count of each integer
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num,count.getOrDefault(num,0) +1);
+        }
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        for (Map.Entry<Integer, Integer> entry: count.entrySet()) {
+            int fre = entry.getValue();
+            int num = entry.getKey();
+            int[] element = new int[]{fre,num};
+
+            heap.offer(element);
+            if (heap.size() > k) {
+                heap.poll();
+
+            }
+
+        }
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = heap.poll()[1];
+
+        }
+        return res;
+        
+        
+    }
+}
