b183c37b66c5299ca9d971e8a9108d996bc092e2
Qianyi Li <li.qianyi@northeastern.edu>
2025-10-14 22:40:39 -0700

    Add solution for Sliding Window Maximum

---
+++ LeetCode/Sliding-Window-Maximum/Solution.java
@@ -0,0 +1,21 @@
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if len(nums) <= k:
+            return [max(nums)]
+        
+        n = len(nums)
+        res = []        
+        
+        pq = [(-num, i) for i, num in enumerate(nums[:k])]
+        heapify(pq)        
+        res.append(-pq[0][0])
+        
+        s = 0        
+        for i in range(k, n):
+            s += 1            
+            while pq and pq[0][1] < s:
+                heappop(pq)   
+            
+            heappush(pq, (-nums[i], i))            
+            res.append(-pq[0][0])
+        
+        return res
