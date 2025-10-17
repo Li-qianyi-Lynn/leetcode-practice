cfca32c07474e2c6ceeba22c5fa40d4a4810b6e5
Qianyi Li <li.qianyi@northeastern.edu>
2025-10-17 00:36:02 -0700

    Add solution for Find Largest Value in Each Tree Row

---
+++ LeetCode/Find-Largest-Value-in-Each-Tree-Row/Solution.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> deque = new LinkedList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        deque.addLast(root);
+
+        while (!deque.isEmpty()) {
+            int levelNodes = deque.size();
+            int max = Integer.MIN_VALUE;
+
+            for (int i = 1;i <= levelNodes;i++) {
+                TreeNode poll = deque.pollFirst();
+                max = Math.max(max, poll.val);
+                if (poll.left != null) {
+                    deque.addLast(poll.left);
+                }
+                if (poll.right != null) {
+                    deque.addLast(poll.right);
+                }
+            }
+            res.add(max);
+
+        }
+        return res;
+        
+    }
+}
