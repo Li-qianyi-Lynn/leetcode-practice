960d8a76a0542c3a49deaff9c57c91c4c8599228
Qianyi Li <li.qianyi@northeastern.edu>
2025-10-24 17:22:22 -0700

    Add solution for Top K Frequent Elements

--- LeetCode/Top-K-Frequent-Elements/Solution.java
+++ LeetCode/Top-K-Frequent-Elements/Solution.java
@@ -1,27 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int num : nums) {
-            count.put(num, count.getOrDefault(num,0)+1);
-        }
-
-        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) -> a[0] - b[0]);
-        for (Map.Entry<Integer, Integer> entry : count.entrySet()) { // - 获取HashMap中所有的键值对
-            int frequency = entry.getValue();  // 获取频次
-            int number = entry.getKey();       // 获取数字
-            int[] element = new int[]{frequency, number};  // 创建数组
-            heap.offer(element);
-            if (heap.size() > k) {
-                heap.poll();
-            }
-        }
-
         int[] res = new int[k];
+        // map: store num(key) and its frequency(value)
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        //put K-V pait to the pq, and do the comperation
+        // min heap: heap topwill store the least frequent element, if the size is larger than k, we can just pop out he top
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        for(Map.Entry<Integer, Integer> pair: map.entrySet()) {
+            int freq = pair.getValue();
+            int num = pair.getKey();
+            int[] element = new int[]{num, freq};
+            pq.offer(element);
+
+            if (pq.size() > k) {
+                pq.poll();
+
+            }
+
+        }
+
+        // add num to the res list;
         for (int i = 0; i < k; i++) {
-            res[i] = heap.poll()[1];
+            res[i] = pq.poll()[0];
+
         }
         return res;
-
         
     }
 }
