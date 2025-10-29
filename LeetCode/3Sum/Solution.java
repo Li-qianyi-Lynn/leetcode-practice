b04032f369db985960521544c1b95aca99dedf39
Qianyi Li <li.qianyi@northeastern.edu>
2025-10-29 16:27:34 -0700

    Add solution for 3Sum

--- LeetCode/3Sum/Solution.java
+++ LeetCode/3Sum/Solution.java
@@ -1,29 +1,39 @@
-public class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        // use two pointers (l & r) 
+        /**
+        if nums[l] + nums[r] < target
+        l ++;
+        if res > target
+        r --;
+        else return their index;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) break;
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        [2,7,11,15]
+        |   |     
+        
+         */
 
-            int l = i + 1, r = nums.length - 1;
-            while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                if (sum > 0) {
-                    r--;
-                } else if (sum < 0) {
-                    l++;
-                } else {
-                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                    l++;
-                    r--;
-                    while (l < r && nums[l] == nums[l - 1]) {
-                        l++;
-                    }
-                }
+
+        int l = 0;
+        int r = numbers.length -1;
+        int[] res = new int[2];
+
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum < target) {
+                l++;
+            } else if (sum > target) {
+                r--;
+            } else {
+                res[0] = l+1;
+                res[1] = r+1;
+                return res;
+
             }
-        }
+            
+
+        } 
         return res;
+        
     }
 }
