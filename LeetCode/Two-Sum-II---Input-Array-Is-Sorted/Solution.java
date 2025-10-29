51cd0ff9ef4216b53d9e08e233578a7ad08f30c6
Qianyi Li <li.qianyi@northeastern.edu>
2025-10-29 15:59:21 -0700

    Add solution for Two Sum II - Input Array Is Sorted

--- LeetCode/Two-Sum-II---Input-Array-Is-Sorted/Solution.java
+++ LeetCode/Two-Sum-II---Input-Array-Is-Sorted/Solution.java
@@ -1,27 +1,31 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int[] res = new int[2];
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int i = 0; i < numbers.length; i++) {
-            int tar = target - numbers[i];
-            if (map.containsKey(tar)) {
-                if (i < map.get(tar)) {
-                    res[0] = i+1;
-                    res[1] = map.get(tar)+1;
-                    return res;
 
-                } else {
-                    res[1] = i+1;
-                    res[0] = map.get(tar)+1;
-                    return res;
+        int[] ans = new int[2];
 
-                }
+        int r = numbers.length -1;
+        int l = 0;
+        while (l < r) {
+        
+
+            if ( numbers[l] + numbers[r] < target) {
+                l ++;
+
+
+            } else if ( numbers[l] + numbers[r] > target) {
+                r --;
+            } else {
+                ans[0] = l +1;
+                ans[1] = r +1;
+                break;
 
             }
 
-            map.put(numbers[i], i);
         }
-        return res;
+
+        return ans;
+
+
         
     }
 }
