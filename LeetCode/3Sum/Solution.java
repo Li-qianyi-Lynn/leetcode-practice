a12e842d7c829024e708aa8faee5b606db8a0418
Qianyi Li <li.qianyi@northeastern.edu>
2025-10-29 17:45:53 -0700

    Add solution for 3Sum

--- LeetCode/3Sum/Solution.java
+++ LeetCode/3Sum/Solution.java
@@ -1,29 +1,60 @@
-public class Solution {
+class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        /**
+        step1: sort, prepare for two pointers O(nlogn)
+        step2: iterate nums,use two pointers (l = i+1; r = length-1) to check 
+        if nums[i] + nums[l] +nums[r] == 0. O(n)
+            - it is possible to have duplicate res, we need to remove duplicates
+            - i == i+1 continue;
+            - l = l+1 continue;
+            - r = r -1 continue;
+
+
+        step3: add res to results;
+        
+     
+         */
+
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < len; i++) {
             if (nums[i] > 0) break;
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i > 0 && nums[i] == nums[i-1]) continue;
 
-            int l = i + 1, r = nums.length - 1;
+            int l = i +1;
+            int r = len -1;
+
             while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                if (sum > 0) {
-                    r--;
-                } else if (sum < 0) {
-                    l++;
+                if (nums[i] + nums[l] + nums[r] < 0) {
+                    l ++;
+
+                } else if (nums[i] + nums[l] + nums[r] > 0) {
+                    r --;
+
                 } else {
-                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    results.add(Arrays.asList(nums[i], nums[r],nums[l]));
                     l++;
                     r--;
+
                     while (l < r && nums[l] == nums[l - 1]) {
                         l++;
+
                     }
+                    while (l < r && nums[r] == nums[r + 1]) {
+                        r--;
+
+                    }
+
+                    // when i found one solution, skip duplicated l!!
                 }
+
             }
+
         }
-        return res;
+
+        return results;
+        
     }
 }
