54831147f9889c826cccdf28550f05a8a0c39b44
Qianyi Li <li.qianyi@northeastern.edu>
2025-10-29 22:15:38 -0700

    Add solution for Reverse Linked List

--- LeetCode/Reverse-Linked-List/Solution.java
+++ LeetCode/Reverse-Linked-List/Solution.java
@@ -1,18 +1,35 @@
-class Solution(object):        
-    def reverseList(self, head):  # Iterative
-        prev, curr = None, head
-        while curr:
-            curr.next, prev, curr = prev, curr, curr.next
-        return prev
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        /**
+        use ListNode cur pointer to reverse this linked List
+    
         
-    def reverseList_v1(self, head):  # Recursive
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """     
-        if not head or not head.next:
-            return head
-        p = self.reverseList(head.next)
-        head.next.next = head
-        head.next = None
-        return p 
+         */
+     
+         ListNode cur = head;
+         ListNode pre = null;
+         
+         while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+
+        }
+         return pre;
+
+
+        
+    }
+}
