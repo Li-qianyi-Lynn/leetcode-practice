37a2d06d58fa19396d29abfd1aef40722a3d8d26
Qianyi Li <li.qianyi@northeastern.edu>
2025-11-02 01:41:46 -0800

    Add solution for Reverse Linked List II

--- LeetCode/Reverse-Linked-List-II/Solution.java
+++ LeetCode/Reverse-Linked-List-II/Solution.java
@@ -1,35 +1,7 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-public class Solution {
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode leftPrev = dummy, cur = head;
-
-        for (int i = 0; i < left - 1; i++) {
-            leftPrev = cur;
-            cur = cur.next;
-        }
-
-        ListNode prev = null;
-        for (int i = 0; i < right - left + 1; i++) {
-            ListNode tmpNext = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = tmpNext;
-        }
-
-        leftPrev.next.next = cur;
-        leftPrev.next = prev;
-
-        return dummy.next;
+    ListNode reverse(ListNode head) {
+        if (head.next == null) return head;
+        ListNode last = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
     }
-}
