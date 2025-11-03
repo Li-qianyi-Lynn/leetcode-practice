bd0458ab272c76fbd45d419ad98770b3e3233de2
Qianyi Li <li.qianyi@northeastern.edu>
2025-11-03 00:09:34 -0800

    Add solution for Reorder List

--- LeetCode/Reorder-List/Solution.java
+++ LeetCode/Reorder-List/Solution.java
@@ -1,59 +1,24 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public void reorderList(ListNode head) {
-        if (head == null || head.next == null) {
-            return;
-        }
+    Stack<ListNode> stack = new Stack<>();
+    ListNode node = head;
+    
+    while(node != null) {
+        stack.add(node);
+        node = node.next;
+    }
+    
+    node = head;
+    
+    while(node != null) {
+        ListNode next = node.next;
+        ListNode endNode = stack.pop();
         
-        // 1. 找到中点
-        ListNode slow = head, fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        
-        // 2. 分割链表并反转后半部分
-        ListNode secondHalf = slow.next;
-        slow.next = null;  // 断开前半部分
-        secondHalf = reverseList(secondHalf);
-        
-        // 3. 合并两个链表
-        ListNode first = head;
-        ListNode second = secondHalf;
-        
-        while (second != null) {
-            ListNode temp1 = first.next;
-            ListNode temp2 = second.next;
-            
-            first.next = second;
-            second.next = temp1;
-            
-            first = temp1;
-            second = temp2;
+        node.next = endNode;
+        endNode.next = next;
+        node = next;
+        if(node != null && node.next == endNode) {
+            node.next = null;
+            break;
         }
     }
     
-    // 反转链表
-    private ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        
-        return prev;
-    }
 }
