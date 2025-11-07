cfe3b8d46ad9b46650e8eecb6615fee8aca57af6
Qianyi Li <li.qianyi@northeastern.edu>
2025-11-07 11:58:58 -0800

    Add solution for Binary Tree Right Side View

--- LeetCode/Binary-Tree-Right-Side-View/Solution.java
+++ LeetCode/Binary-Tree-Right-Side-View/Solution.java
@@ -15,27 +15,37 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        // edge case
         List<Integer> res = new ArrayList<>();
-        if (root == null) return res;
+        if (root == null) {
+            return res;
+        }
+        bfs(root,res);
+        return res;
+        
+    }
+    private void bfs(TreeNode root, List<Integer> res) {
         Deque<TreeNode> deque = new LinkedList<>();
         deque.offerLast(root);
         while (!deque.isEmpty()) {
             int size = deque.size();
-
             for (int i = 1; i <= size; i++) {
                 TreeNode poll = deque.pollFirst();
                 if (poll.left != null) {
                     deque.offerLast(poll.left);
+
                 }
                 if (poll.right != null) {
                     deque.offerLast(poll.right);
+
                 }
                 if (i == size) {
                     res.add(poll.val);
+
                 }
+
             }
+
         }
-        return res;     
+
     }
 }
