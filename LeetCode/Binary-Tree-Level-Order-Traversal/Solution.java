635c3ae65f25ebb2e5ab98209805dcbf093d9e4a
Qianyi Li <li.qianyi@northeastern.edu>
2025-11-07 00:42:19 -0800

    Add solution for Binary Tree Level Order Traversal

--- LeetCode/Binary-Tree-Level-Order-Traversal/Solution.java
+++ LeetCode/Binary-Tree-Level-Order-Traversal/Solution.java
@@ -1,18 +1,40 @@
-class Solution:
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
-        levels = []
-        if not root:
-            return levels
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> resList = new ArrayList<List<Integer>>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        bfs(root, 0);
+        return resList;   
+    }
 
-        stack = [(root, 0)]
-        while stack:
-            curr_node, level = stack.pop()
-            if len(levels) == level:
-                levels.append([])
-            levels[level].append(curr_node.val)
-            if curr_node.right:
-                stack.append((curr_node.right, level + 1))
-            if curr_node.left:
-                stack.append((curr_node.left, level + 1))
+    private void bfs(TreeNode node, Integer deep) {
+        if (node == null) {
+            return;
 
-        return levels
+        }
+
+        deep++;
+        if (resList.size() < deep) {
+            List<Integer> item = new ArrayList<Integer>();
+            resList.add(item);
+
+        }
+        resList.get(deep-1).add(node.val);
+        bfs(node.left, deep);
+        bfs(node.right, deep);
+
+    }
+}
