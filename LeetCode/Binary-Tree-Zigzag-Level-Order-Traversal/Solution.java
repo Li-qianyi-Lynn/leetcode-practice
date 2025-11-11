fdd21d2a3bfa4f55d0be1689ca0ffbb6710b6913
Qianyi Li <li.qianyi@northeastern.edu>
2025-11-11 15:02:14 -0800

    Add solution for Binary Tree Zigzag Level Order Traversal

--- LeetCode/Binary-Tree-Zigzag-Level-Order-Traversal/Solution.java
+++ LeetCode/Binary-Tree-Zigzag-Level-Order-Traversal/Solution.java
@@ -13,8 +13,6 @@
  *     }
  * }
  */
-
-
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
@@ -31,32 +29,26 @@ class Solution {
         deque.offerLast(root);
         while(!deque.isEmpty()) {
             int size = deque.size();
+            List<Integer> levelNode= new LinkedList<>();
 
-            List<Integer> temp = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-                if (leftToRight) {
-                    TreeNode poll = deque.pollFirst();
-                    if (poll.left != null) {
+            for (int i = 1; i <= size; i++) {
+                TreeNode poll = deque.pollFirst();
+                levelNode.add(poll.val);
+                if (poll.left != null) {
                         deque.offerLast(poll.left);
-                    }
+                    } 
                     if (poll.right != null) {
                         deque.offerLast(poll.right);
                     }
-                    temp.add(poll.val);
-                    continue;
-                }
-                TreeNode poll = deque.pollLast();
-                if (poll.right != null) {
-                    deque.offerFirst(poll.right);
-                }
-                if (poll.left != null) {
-                    deque.offerFirst(poll.left);
-                }
-                temp.add(poll.val);
+                
             }
-            res.add(temp);
+            if (!leftToRight) {
+                Collections.reverse(levelNode);
+            }
+            res.add(levelNode);
             leftToRight = !leftToRight;
         }
+        
+    
     }
 }
