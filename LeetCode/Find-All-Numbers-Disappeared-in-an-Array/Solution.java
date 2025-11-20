e41e5440a8ba949fcde3a61d1edd1bf4fa253213
Qianyi Li <li.qianyi@northeastern.edu>
2025-11-20 00:40:36 -0800

    Add solution for Find All Numbers Disappeared in an Array

--- LeetCode/Find-All-Numbers-Disappeared-in-an-Array/Solution.java
+++ LeetCode/Find-All-Numbers-Disappeared-in-an-Array/Solution.java
@@ -1,26 +1,31 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        List<Integer> res = new ArrayList<>();
-        for (int num: nums) {
-            set.add(num);
-        }
-        Deque<Integer> deque = new ArrayDeque<>();
-        for (int i = 1; i <= nums.length; i++) {
-            deque.offerLast(i);
-            if (set.contains(i)) {
-                deque.pollLast();
+        for(int i = 0; i < nums.length; i++){
+            int idx = nums[i]-1;
+            // swap until the nums[i] is at index nums[i]-1
+            while(idx != nums[i]){
+                swap(nums, idx, i);
+                idx = nums[i] - 1;
+                // if we are going to swap same number: break
+                if(nums[idx] == nums[i]){
+                    break;
+                }
             }
-
         }
-        while (!deque.isEmpty()) {
-            res.add(deque.pollLast());
-
+        // In this point, the array looks like this: [1,2,3,4,3,2,7,8]
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            if(i != nums[i]-1){
+                res.add(i+1);
+            }
         }
+        
         return res;
-        
-
-
-        
+    }
+    
+    private void swap(int[] nums, int idx1, int idx2){
+        int temp = nums[idx1];
+        nums[idx1] = nums[idx2];
+        nums[idx2] = temp;
     }
 }
