d71eeaf783e52e038a693c0c1b8a75071cc6984e
Qianyi Li <li.qianyi@northeastern.edu>
2025-11-19 23:36:33 -0800

    Add solution for How Many Numbers Are Smaller Than the Current Number

--- LeetCode/How-Many-Numbers-Are-Smaller-Than-the-Current-Number/Solution.java
+++ LeetCode/How-Many-Numbers-Are-Smaller-Than-the-Current-Number/Solution.java
@@ -2,19 +2,23 @@ class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int arr[] = new int[101];
 
-        for (int i : nums) {
-            arr[i]++;
-        }
-
-        for (int i = 1; i < 101; i++) {
-            arr[i] += arr[i - 1];
-        }
-
         for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            if (num != 0) nums[i] = arr[num - 1];
+            arr[nums[i]]++;
         }
 
+        for (int j = 1; j < 101; j++) {
+            arr[j] += arr[j-1];
+        }
+
+        for (int m = 0; m < nums.length; m++) {
+            // int num = nums[i];
+            // if (num != 0) nums[i] = arr[num - 1];
+            if (nums[m] != 0) {
+                nums[m] = arr[nums[m] - 1];
+            }
+
+        }
         return nums;
+        
     }
 }
