cbe7b7021765aab9a778ecc769d1845482cbd993
Qianyi Li <li.qianyi@northeastern.edu>
2025-12-04 16:08:28 -0800

    Add solution for Binary Tree Level Order Traversal

--- LeetCode/Binary-Tree-Level-Order-Traversal/Solution.java
+++ LeetCode/Binary-Tree-Level-Order-Traversal/Solution.java
@@ -1,60 +1,18 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        levels = []
+        if not root:
+            return levels
 
- /**
- 
- BFS(root, res) to update res
- - deque to store the treenode of each level FIFO
- - List<Integer> to store the cur level node 
- - int size of deque means how many nodes in this level
+        stack = [(root, 0)]
+        while stack:
+            curr_node, level = stack.pop()
+            if len(levels) == level:
+                levels.append([])
+            levels[level].append(curr_node.val)
+            if curr_node.right:
+                stack.append((curr_node.right, level + 1))
+            if curr_node.left:
+                stack.append((curr_node.left, level + 1))
 
- step1: put the cur level node(if root is not null) to the deque;
- step2: iterate current level nodes, check if it has children nodes (if it has , add to the deque);
- step3: add current level nodes to the res;
- 
-  */
-class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (root == null) {
-            return res;
-
-        }
-        bfs(root, res);
-        return res;
-        
-    }
-
-    private void bfs(TreeNode root, List<List<Integer>>res) {
-        Deque<TreeNode> deque = new LinkedList<>();
-        deque.offerLast(root);
-        while (!deque.isEmpty()) {
-            int size = deque.size();
-            List<Integer> cur = new ArrayList<>();
-            for (int i = 1; i <= size; i++) {
-                TreeNode poll = deque.pollFirst();
-                if (poll.left != null) {
-                    deque.offerLast(poll.left);
-                }
-                if (poll.right != null) {
-                    deque.offerLast(poll.right);
-                }
-                cur.add(poll.val);
-            }
-            res.add(cur);
-        }
-    }
-}
+        return levels
