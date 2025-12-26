c71d5fec690b433f290d30cfed39a644d0bdbf70
Qianyi Li <li.qianyi@northeastern.edu>
2025-12-26 12:49:28 -1000

    Add solution for Permutations

--- LeetCode/Permutations/Solution.java
+++ LeetCode/Permutations/Solution.java
@@ -11,29 +11,30 @@
 11        // base case
 12        if (perm.size() == nums.length) {
 13            res.add(new ArrayList<>(perm));
-14            return;
-15        }
-16
-17        for (int i = 0; i < nums.length; i++) {
-18            if (used[i] == false) {
-19                perm.add(nums[i]);
-20                used[i] = true;
-21                backtrack(res, perm, nums, used);
-22
-23                perm.remove(perm.size() -1);
-24                used[i] = false;
-25            }
-26        }
-27
-28    }
-29}
-30
-31/**
-32backtrack:
-331. take nums[i] one by one to be one possible result(perm), boolean int[nums.length] = false
-342. if the num is used, marked as used (true), and then use backtrack to continue choosing next num
-353. end condition: size of perm == length of nums
-36
-374. recover: remove nums[i] from perm, marked as false for next use
-38
-39 */
+14            // System.out.println(perm);
+15            return;
+16        }
+17
+18        for (int i = 0; i < nums.length; i++) {
+19            if (used[i] == false) {
+20                perm.add(nums[i]);
+21                used[i] = true;
+22                backtrack(res, perm, nums, used);
+23
+24                perm.remove(perm.size() -1);
+25                used[i] = false;
+26            }
+27        }
+28
+29    }
+30}
+31
+32/**
+33backtrack:
+341. take nums[i] one by one to be one possible result(perm), boolean int[nums.length] = false
+352. if the num is used, marked as used (true), and then use backtrack to continue choosing next num
+363. end condition: size of perm == length of nums
+37
+384. recover: remove nums[i] from perm, marked as false for next use
+39
+40 */
