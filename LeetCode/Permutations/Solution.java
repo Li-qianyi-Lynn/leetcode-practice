dc48c370145d24d02cbc4a62f2b196e550340630
Qianyi Li <li.qianyi@northeastern.edu>
2025-12-26 12:16:52 -1000

    Add solution for Permutations

--- LeetCode/Permutations/Solution.java
+++ LeetCode/Permutations/Solution.java
@@ -1,8 +1,8 @@
 1public class Solution {
-2    // List<List<Integer>> res;
-3    public List<List<Integer>> permute(int[] nums) {
-4        List<List<Integer>> res = new ArrayList<>();
-5        backtrack(res, new ArrayList<>(), nums, new boolean[nums.length]); // all false, which means not used
+2    public List<List<Integer>> permute(int[] nums) {
+3        List<List<Integer>> res = new ArrayList<>();
+4        List<Integer> perm = new ArrayList<>();
+5        backtrack(res, perm, nums, new boolean[nums.length]); // all false, which means not used
 6        return res;
 7    }
 8
@@ -12,14 +12,18 @@
 12            res.add(new ArrayList<>(perm));
 13            return;
 14        }
-15        for (int i = 0; i < nums.length; i++) {
-16            if (!pick[i]) {
-17                perm.add(nums[i]);
-18                pick[i] = true;
-19                backtrack(res, perm, nums, pick);
-20                perm.remove(perm.size() - 1);
-21                pick[i] = false;
-22            }
-23        }
-24    }
-25}
+15
+16
+17        for (int i = 0; i < nums.length; i++) {
+18            if (!pick[i]) { // is this num is not used:
+19                perm.add(nums[i]); // 1. add it to perm
+20                pick[i] = true; //2.  marked as used
+21                backtrack(res, perm, nums, pick); // 3. backtrack: choose next num
+22
+23                // key point : remove and recover
+24                perm.remove(perm.size() - 1);
+25                pick[i] = false;
+26            }
+27        }
+28    }
+29}
