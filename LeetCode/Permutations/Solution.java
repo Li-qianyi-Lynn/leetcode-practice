0545527ae08b240d2c7400e43b1adc6b7aaee5b7
Qianyi Li <li.qianyi@northeastern.edu>
2025-12-26 12:43:28 -1000

    Add solution for Permutations

--- LeetCode/Permutations/Solution.java
+++ LeetCode/Permutations/Solution.java
@@ -1,29 +1,39 @@
-1public class Solution {
+1class Solution {
 2    public List<List<Integer>> permute(int[] nums) {
 3        List<List<Integer>> res = new ArrayList<>();
 4        List<Integer> perm = new ArrayList<>();
-5        backtrack(res, perm, nums, new boolean[nums.length]); // all false, which means not used
-6        return res;
-7    }
-8
-9    public void backtrack(List<List<Integer>> res, List<Integer> perm, int[] nums, boolean[] pick) {
-10        // when to end: perm's size == length of nums
-11        if (perm.size() == nums.length) {
-12            res.add(new ArrayList<>(perm));
-13            return;
-14        }
-15
+5        boolean[] used = new boolean[nums.length];
+6        backtrack (res, perm, nums, used);
+7        return res;
+8    }
+9
+10    private void backtrack(List<List<Integer>> res, List<Integer> perm, int[] nums, boolean[] used) {
+11        // base case
+12        if (perm.size() == nums.length) {
+13            res.add(new ArrayList<>(perm));
+14            return;
+15        }
 16
 17        for (int i = 0; i < nums.length; i++) {
-18            if (!pick[i]) { // is this num is not used:
-19                perm.add(nums[i]); // 1. add it to perm
-20                pick[i] = true; //2.  marked as used
-21                backtrack(res, perm, nums, pick); // 3. backtrack: choose next num
+18            if (used[i] == false) {
+19                perm.add(nums[i]);
+20                used[i] = true;
+21                backtrack(res, perm, nums, used);
 22
-23                // key point : remove and recover
-24                perm.remove(perm.size() - 1);
-25                pick[i] = false;
-26            }
-27        }
+23                perm.remove(perm.size() -1);
+24                used[i] = false;
+25            }
+26        }
+27
 28    }
 29}
+30
+31/**
+32backtrack:
+331. take nums[i] one by one to be one possible result(perm), boolean int[nums.length] = false
+342. if the num is used, marked as used (true), and then use backtrack to continue choosing next num
+353. end condition: size of perm == length of nums
+36
+374. recover: remove nums[i] from perm, marked as false for next use
+38
+39 */
