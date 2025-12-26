8164020727f9d7866b8015fa3577fa6422b2b1b0
Qianyi Li <li.qianyi@northeastern.edu>
2025-12-26 12:11:33 -1000

    Add solution for Permutations

--- LeetCode/Permutations/Solution.java
+++ LeetCode/Permutations/Solution.java
@@ -1,24 +1,25 @@
 1public class Solution {
-2    List<List<Integer>> res;
+2    // List<List<Integer>> res;
 3    public List<List<Integer>> permute(int[] nums) {
-4        res = new ArrayList<>();
-5        backtrack(new ArrayList<>(), nums, new boolean[nums.length]); // all false
+4        List<List<Integer>> res = new ArrayList<>();
+5        backtrack(res, new ArrayList<>(), nums, new boolean[nums.length]); // all false, which means not used
 6        return res;
 7    }
 8
-9    public void backtrack(List<Integer> perm, int[] nums, boolean[] pick) {
-10        if (perm.size() == nums.length) {
-11            res.add(new ArrayList<>(perm));
-12            return;
-13        }
-14        for (int i = 0; i < nums.length; i++) {
-15            if (!pick[i]) {
-16                perm.add(nums[i]);
-17                pick[i] = true;
-18                backtrack(perm, nums, pick);
-19                perm.remove(perm.size() - 1);
-20                pick[i] = false;
-21            }
-22        }
-23    }
-24}
+9    public void backtrack(List<List<Integer>> res, List<Integer> perm, int[] nums, boolean[] pick) {
+10        // when to end: perm's size == length of nums
+11        if (perm.size() == nums.length) {
+12            res.add(new ArrayList<>(perm));
+13            return;
+14        }
+15        for (int i = 0; i < nums.length; i++) {
+16            if (!pick[i]) {
+17                perm.add(nums[i]);
+18                pick[i] = true;
+19                backtrack(res, perm, nums, pick);
+20                perm.remove(perm.size() - 1);
+21                pick[i] = false;
+22            }
+23        }
+24    }
+25}
