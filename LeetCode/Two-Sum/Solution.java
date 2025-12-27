c5268f52430b9e44182b4b231fa5da8b3f4bd511
Qianyi Li <li.qianyi@northeastern.edu>
2025-12-26 15:05:16 -1000

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.java
+++ LeetCode/Two-Sum/Solution.java
@@ -1,23 +1,12 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        // use map to store index and nums[i] k:nums[i] v: index 
-        /**
-        when we get a num, check if the target-num is in map:
-        - if exist, return their index;
-        - if no, restore this num into map
-         */
-
-         Map<Integer, Integer> map = new HashMap<>();
-         for (int i = 0; i < nums.length; i++) {
-            int tar = target - nums[i];
-            if (map.containsKey(tar)) {
-                return new int[]{i, map.get(tar)};
-
-            }
-            map.put(nums[i], i);   
-
-        }
-        return new int[]{};
+ for(var i =0;i<nums.length;i++){
+    var item = nums[i];   
+    for(var j=0; j < nums.length;j++){
+        var item2 = nums[j];
+        var sum = item + item2;
         
+    if(sum === target ){
+      return [i,j];
+      
     }
-}
+  }
+ }
