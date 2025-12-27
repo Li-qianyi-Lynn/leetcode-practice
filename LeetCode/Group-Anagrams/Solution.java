4af83251d318517fe4248cf71dd31f1469c49652
Qianyi Li <li.qianyi@northeastern.edu>
2025-12-26 16:02:06 -1000

    Add solution for Group Anagrams

--- LeetCode/Group-Anagrams/Solution.java
+++ LeetCode/Group-Anagrams/Solution.java
@@ -1,50 +1,46 @@
-primes = {'a': 2, 
-                  'b': 3, 
-                  'c': 5, 
-                  'd': 7, 
-                  'e': 11, 
-                  'f': 13,
-                  'g': 17,
-                  'h': 19,
-                  'i': 23,
-                  'j': 29,
-                  'k': 31,
-                  'l': 37,
-                  'm': 41,
-                  'n': 43,
-                  'o': 47,
-                  'p': 53,
-                  'q': 59,
-                  'r': 61,
-                  's': 67, 
-                  't': 71,
-                  'u': 73,
-                  'v': 79,
-                  'w': 83,
-                  'x': 89,
-                  'y': 97,
-                  'z': 101
-                 }
-        
-        
-        subLists = {}
-        
-        for string in strs:
-            product = 1
-            
-            for character in string:
-                product = primes[character] * product
-            
-            if product in subLists.keys():
-                listA = subLists[product]
-                listA.append(string)
-                subLists[product] = listA
-            else:
-                subLists[product] = [string]
-                
-        listToReturn = []
-        
-        for value in subLists.keys():
-            listToReturn.append(subLists[value])
-            
-        return listToReturn
+1class Solution {
+2    public List<List<String>> groupAnagrams(String[] strs) {
+3        
+4        List<List<String>> res = new ArrayList<>();
+5        HashMap<String, List<String>> map = new HashMap<>();
+6
+7        // take each word 
+8        for (String s : strs) {
+9            int[] count = new int[26];
+10            for (char c : s.toCharArray()) {
+11                count[c-'a']++;
+12            }
+13
+14            String key = Arrays.toString(count);
+15            // if no keys, new arrayList<>()
+16            map.putIfAbsent(key, new ArrayList<>());
+17            map.get(key).add(s);
+18        }
+19
+20        res.addAll(map.values());
+21        return res;
+22  
+23    }
+24}
+25
+26/**
+27
+28use hashmap to store group anagrams
+29key: String - which means it has same char groups
+30-convert 26 english letters as int[26]
+31-index means a-z
+32-int[i] means the num of letters used
+33
+34values: List<String> - anagrams
+35
+36if map has the key, add the value;
+37or map. putifabsent (new key, new values )
+38
+39
+40return map. values as the res (collections)
+41or addAll
+42
+43
+44
+45
+46 */
