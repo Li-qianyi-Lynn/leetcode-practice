21cf5df4fa047653e7b1e6b5c60d28dae7541177
Qianyi Li <li.qianyi@northeastern.edu>
2025-12-26 16:28:08 -1000

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.java
+++ LeetCode/Valid-Anagram/Solution.java
@@ -1,16 +1,16 @@
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) { return false; }
-        Map<Character, Integer> mapS = new HashMap<>();
-        Map<Character, Integer> mapT = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i),0)+1);
-            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i),0)+1);
-
-        }
-
-        return mapS.equals(mapT);
-        
-    }
-}
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        if (s.length() != t.length()) { return false; }
+4        Map<Character, Integer> mapS = new HashMap<>();
+5        Map<Character, Integer> mapT = new HashMap<>();
+6
+7        for (int i = 0; i < s.length(); i++) {
+8            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i),0)+1);
+9            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i),0)+1);
+10
+11        }
+12
+13        return mapS.equals(mapT);
+14        
+15    }
+16}
