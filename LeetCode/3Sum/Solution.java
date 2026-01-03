76d99d0e460e81f2a57b3a8c2b36a26d708cf2e8
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-03 11:50:08 -0800

    Add solution for 3Sum

--- LeetCode/3Sum/Solution.java
+++ LeetCode/3Sum/Solution.java
@@ -1,60 +1,60 @@
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> results = new ArrayList<>();
-        /**
-        step1: sort, prepare for two pointers O(nlogn)
-        step2: iterate nums,use two pointers (l = i+1; r = length-1) to check 
-        if nums[i] + nums[l] +nums[r] == 0. O(n)
-            - it is possible to have duplicate res, we need to remove duplicates
-            - i == i+1 continue;
-            - l = l+1 continue;
-            - r = r -1 continue;
-
-
-        step3: add res to results;
-        
-     
-         */
-
-        Arrays.sort(nums);
-        int len = nums.length;
-
-        for (int i = 0; i < len; i++) {
-            if (nums[i] > 0) break;
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-
-            int l = i +1;
-            int r = len -1;
-
-            while (l < r) {
-                if (nums[i] + nums[l] + nums[r] < 0) {
-                    l ++;
-
-                } else if (nums[i] + nums[l] + nums[r] > 0) {
-                    r --;
-
-                } else {
-                    results.add(Arrays.asList(nums[i], nums[r],nums[l]));
-                    l++;
-                    r--;
-
-                    while (l < r && nums[l] == nums[l - 1]) {
-                        l++;
-
-                    }
-                    while (l < r && nums[r] == nums[r + 1]) {
-                        r--;
-
-                    }
-
-                    // when i found one solution, skip duplicated l!!
-                }
-
-            }
-
-        }
-
-        return results;
-        
-    }
-}
+1class Solution {
+2    public List<List<Integer>> threeSum(int[] nums) {
+3        List<List<Integer>> results = new ArrayList<>();
+4        /**
+5        step1: sort, prepare for two pointers O(nlogn)
+6        step2: iterate nums,use two pointers (l = i+1; r = length-1) to check 
+7        if nums[i] + nums[l] +nums[r] == 0. O(n)
+8            - it is possible to have duplicate res, we need to remove duplicates
+9            - i == i+1 continue;
+10            - l == l+1 continue;
+11            - r == r -1 continue;
+12
+13
+14        step3: add res to results;
+15        
+16     
+17         */
+18
+19        Arrays.sort(nums);
+20        int len = nums.length;
+21
+22        for (int i = 0; i < len; i++) {
+23            if (nums[i] > 0) break;
+24            if (i > 0 && nums[i] == nums[i-1]) continue;
+25
+26            int l = i +1;
+27            int r = len -1;
+28
+29            while (l < r) {
+30                if (nums[i] + nums[l] + nums[r] < 0) {
+31                    l ++;
+32
+33                } else if (nums[i] + nums[l] + nums[r] > 0) {
+34                    r --;
+35
+36                } else {
+37                    results.add(Arrays.asList(nums[i], nums[r],nums[l]));
+38                    l++;
+39                    r--;
+40
+41                    while (l < r && nums[l] == nums[l - 1]) {
+42                        l++;
+43
+44                    }
+45                    while (l < r && nums[r] == nums[r + 1]) {
+46                        r--;
+47
+48                    }
+49
+50                    // when i found one solution, skip duplicated l!!
+51                }
+52
+53            }
+54
+55        }
+56
+57        return results;
+58        
+59    }
+60}
