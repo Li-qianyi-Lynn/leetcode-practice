a774d0874de7a1e87cb91873b4a357080739b8a7
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-05 01:18:28 -0800

    Add solution for Meeting Rooms II

--- LeetCode/Meeting-Rooms-II/Solution.java
+++ LeetCode/Meeting-Rooms-II/Solution.java
@@ -1,46 +1,45 @@
-class Solution {
-    public int minMeetingRooms(int[][] intervals) {
-        // Map<Integer, Integer> map = new HashMap<>(); //time，-1/1
-        // for (int[] interval : intervals) {
-        //     map.put(interval[0],1);
-        //     map.put(interval[1],-1);
-        // }
-
-        // TreeMap<Integer, Integer> treemap = new TreeMap<>(map);
-        // int max = Integer.MIN_VALUE;
-        // int room = 0;
-
-        // for (int value : treemap.values()) {
-        //     room += value;
-        //     max = Math.max(room, max);
-        // }
-
-        // return max;
-        
-        List<int[]> events = new ArrayList<>();
-
-        for (int[] interval : intervals) {
-            events.add(new int[]{interval[0],1});
-            events.add(new int[]{interval[1],-1});
-        }
-
-        events.sort((a,b) -> {
-            if (a[0] != b[0]) return a[0] - b[0];
-            return a[1] - b[1];
-
-    });
-
-        int rooms = 0;
-        int max = 0;
-
-        for (int[] event : events) {
-            rooms += event[1];
-            max = Math.max(max, rooms);
-
-        }
-
-        return max;
-
-
-    }
-}
+1class Solution {
+2    public int minMeetingRooms(int[][] intervals) {
+3        int room = 0;
+4        int len = intervals.length;
+5        int[] start = new int[len];
+6        int[] end = new int[len];
+7
+8        for (int i = 0; i < len; i++) {
+9            start[i] = intervals[i][0];
+10            end[i] = intervals[i][1];
+11
+12        }
+13
+14        Arrays.sort(start);
+15        Arrays.sort(end);
+16
+17        int endidx = 0;
+18
+19        for (int i = 0; i < len; i++) {
+20            if (start[i] < end[endidx]) { // the event starts much early, need one moreroom
+21                room++;
+22            } else {
+23                endidx++;
+24            }
+25        }
+26        return room;   
+27    }
+28}
+29
+30/**
+31线性扫描
+32start int[]
+33end int[]
+34
+35把时间放进对应的event 里
+36sort
+37endindex
+38
+39判断if start < end time
+40-yes room++
+41-no endindex++
+42
+43
+44
+45 */
