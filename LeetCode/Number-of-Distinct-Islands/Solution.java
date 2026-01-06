4cc94ce978c991c3841aa1b12c6907f8a93491db
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-05 16:51:06 -0800

    Add solution for Number of Distinct Islands

--- LeetCode/Number-of-Distinct-Islands/Solution.java
+++ LeetCode/Number-of-Distinct-Islands/Solution.java
@@ -1,46 +1,41 @@
 1class Solution {
 2    public int numDistinctIslands(int[][] grid) {
-3        Set<String> set = new HashSet<>();
-4        int rows = grid.length;
-5        int cols = grid[0].length;
+3      Set<String> shapes = new HashSet<>();
+4      int row = grid.length;
+5      int col = grid[0].length;
 6
-7        for (int row = 0; row < rows; row++) {
-8            for (int col = 0; col < cols; col++) {
-9                StringBuilder sb = new StringBuilder(); // cur shape
-10                if (grid[row][col] == 1) {
-11                    dfs(grid, row, col, sb, 's'); // start
-12                    set.add(sb.toString());
-13                }
-14            }
-15        }
-16        return set.size();
+7      for (int i = 0; i < row; i++) {
+8        for (int j = 0; j < col; j++) {
+9            if (grid[i][j] == 1) {
+10                StringBuilder currentShape = new StringBuilder();
+11                dfs(grid, i, j, currentShape, 's');
+12                shapes.add(currentShape.toString());
+13            }
+14        }
+15      }  
+16      return shapes.size();
 17    }
 18
-19    private void dfs(int[][] grid, int row, int col, StringBuilder shape, char dir) {
-20        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == 0) {
-21            return;
-22        }
-23        grid[row][col] = 0; // visited
-24        shape.append(dir);
-25
-26        dfs(grid, row+1, col, shape, 'd'); 
-27        dfs(grid, row-1, col, shape, 'u');
-28        dfs(grid, row, col+1, shape, 'r');
-29        dfs(grid, row, col-1, shape, 'l');
-30        shape.append('b');
-31
-32    }
+19    private void dfs(int[][] grid, int i, int j, StringBuilder currentShape, char dir) {
+20        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) return;
+21
+22        grid[i][j] = 2;
+23        currentShape.append(dir);
+24
+25        dfs(grid, i, j - 1, currentShape, 'l');
+26        dfs(grid, i, j + 1, currentShape, 'r');
+27        dfs(grid, i + 1, j, currentShape, 'd');
+28        dfs(grid, i - 1, j, currentShape, 'u');
+29
+30        currentShape.append('b');
+31    }
+32}
 33
-34}
+34/**
 35
-36/**
-371) 如何判断有多少个distinct islands： set.size()
-382）如何判断岛屿一致：stringbuilder 
-39    rules：
-40    1）路径一样
-41    2）开始，回溯的位置一样
-42    
-43
-44
-45
-46 */
+36rules：
+371）路径一样
+382）结束的位置一样
+39
+40
+41 */
