c3af5e347544b91a1a95dbf2a9ad51f0bb62f5a7
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-05 16:38:55 -0800

    Add solution for Minimum Operations to Write the Letter Y on a Grid

--- LeetCode/Minimum-Operations-to-Write-the-Letter-Y-on-a-Grid/Solution.java
+++ LeetCode/Minimum-Operations-to-Write-the-Letter-Y-on-a-Grid/Solution.java
@@ -1,79 +1,70 @@
 1class Solution {
 2    public int minimumOperationsToWriteY(int[][] grid) {
-3        int res = Integer.MAX_VALUE;
-4        int[] countY = new int[3];
+3        // 算符合y letter 的数字有哪些， not y letter 有哪些
+4        int[] county = new int[3];
 5        int[] countNy = new int[3];
-6        int n = grid.length;
-7
-8        for (int r = 0; r < n; r++) {
-9            for (int c =0; c < n; c++) {
-10                if (isY(r,c, n)) {
-11                    countY[grid[r][c]]++;
-12                } else {
-13                    countNy[grid[r][c]]++;
-14                }
-15            }
-16        }
-17
-18        int totalY = countY[0] + countY[1] + countY[2];
-19        int totalNy = countNy[0] + countNy[1] + countNy[2];
-20
-21        for (int i = 0; i < 3; i++) { // y color 这里是3！不是n
-22            for (int j = 0; j < 3; j++) { // non y color
-23                if (j == i) {
-24                    continue;
-25                }
-26                
-27                int ops = (totalY - countY[i]) + (totalNy - countNy[j]);
-28                res = Math.min(ops, res);
-29
-30            }
+6        int res = Integer.MAX_VALUE;
+7        int n = grid.length;
+8
+9        for (int r = 0; r < n; r++) {
+10            for (int c = 0; c < n; c++) {
+11                if (isY(r, c, n)) { 
+12                    county[grid[r][c]]++;
+13                } else {
+14                    countNy[grid[r][c]]++;
+15                }
+16            }
+17        }
+18
+19        int totalY = county[0] + county[1] + county[2];
+20        int totalNy = countNy[0] + countNy[1] + countNy[2];
+21
+22        // 算各个数字出现的次数 for loop 存起来
+23
+24        //res =（totaly - 需要改动的cell num） + （totalny - 需要改动的cell num） ;math.min 
+25        // 如果两两配对，哪一种op 最少
+26        for (int i = 0; i < 3; i++) { // y 有可能的数字
+27            for (int j =0; j < 3; j++) { // non y
+28                if (i == j) {
+29                    continue;
+30                }
 31
-32        }
-33        return res;    
-34    }
+32                int cur = (totalY - county[i]) + (totalNy - countNy[j]);
+33                res = Math.min(cur, res);
+34            }
 35
-36    private boolean isY(int row, int col, int n) {
-37        int mid = n/2;
-38        if (row == col && row <= mid) {
-39            return true;
-40        } 
-41        
-42        if (row + col == n-1 && row <= mid) {
-43            return true;
-44
-45        } 
-46        if (row > mid && col == mid) {
-47            return true;
-48
-49        }
-50        return false;
-51
-52    }
-53}
-54
-55
-56/***
-57
-581. 统计y/ non y 各数字出现的次数, for loop
-59int[] countY = new int[3];  0/1/2
-60int[] countNotY = new int[3];
-61
-622. 如何判断 y/ non y （r,c）
-63mid = n/2 (m,m)
-64- r == c r <= mid
-65- r+c== n-1 r<= mid
-66- c== mid, r > mid
+36        }
+37        
+38        return res;
+39        
+40    }
+41
+42    private boolean isY(int row, int col, int n) {
+43        int mid = n / 2;
+44         // top left: r== c && row <= mid
+45        if (row == col && row <= mid) {
+46            return true;
+47        }
+48        
+49        // top right: r+c = n-1 && row <=mid
+50        if (row + col == n-1 && row <= mid) {
+51            return true;
+52        }
+53
+54         //vertical line: row > mid && col == mid
+55        if (row > mid && col == mid) {
+56            return true;
+57        }
+58
+59        return false;
+60        
+61    }
+62}
+63
+64/**
+65
+66
 67
-683. 把所有颜色统一， y 一种，non y 一种
-69totaly = count
-70totalnoty
-71minres = Integer.MAX_VALUE
-72
-73
-74for 循环遍历
-75要求颜色不一样，所以i j 一致时continue
-76求修改个数 ：totaly-已经是的 + totalnony - 已经是的
-77
-78
-79 */
+68
+69
+70 */
