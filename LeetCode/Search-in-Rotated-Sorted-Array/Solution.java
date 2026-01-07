a7f2aef6c16d5e36e6ec18647c7ef6788c0969a5
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-06 17:30:50 -0800

    Add solution for Search in Rotated Sorted Array

--- LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
+++ LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
@@ -1,32 +1,31 @@
-class Solution {
-    public int search(int[] nums, int target) {
-        int l = 0;
-        int r = nums.length - 1;
-
-        while(l <= r) {
-
-            int mid = (l + r) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            }
-
-            if (nums[l] <= nums[mid]) {
-                if (target > nums[mid] || target < nums[l]) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
-            } else {
-                if (target < nums[mid] || target > nums [r]) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
-            }
-
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # 创建一个哈希表/字典，存储右括号到左括号的映射
+        # 这有助于快速查找匹配的左括号
+        mappings = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
         }
 
-        return -1;
-    }
-}
+        # 使用列表作为栈
+        stack = []
+
+        # 遍历输入字符串中的每个字符
+        for char in s:
+            # 如果当前字符是右括号
+            if char in mappings:
+                # 检查栈是否为空，如果为空，则 pop() 会报错
+                # 如果不为空，则弹出栈顶元素，否则使用一个哑元值'#'，确保不匹配
+                top_element = stack.pop() if stack else '#'
+                
+                # 检查弹出的左括号是否与当前右括号匹配
+                if mappings[char] != top_element:
+                    return False
+            else:
+                # 如果当前字符是左括号，直接压入栈中
+                stack.append(char)
+        
+        # 循环结束后，如果栈为空，说明所有括号都匹配了
+        # 如果栈不为空，说明有未匹配的左括号
+        return not stack
