b8216771ef0aa75901ec1dc695e9e05f666edc32
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-08 22:59:00 -0800

    Add solution for Plus One

--- LeetCode/Plus-One/Solution.java
+++ LeetCode/Plus-One/Solution.java
@@ -1,15 +1,26 @@
-public int[] plusOne(int[] digits) {
-    
-    for (int i=digits.length-1; i>=0; i--){
-        if (digits[i]<9){
-            digits[i]+=1;
-            return digits;
-        }
-        digits[i]=0;
-    }
-    
-    int[] result = new int[digits.length+1];
-    result[0] = 1;
-    return result;
-    
-}
+1class Solution {
+2    public int[] plusOne(int[] digits) {
+3        int len = digits.length;
+4        for (int i = len-1; i >=0; i--) {
+5            if (digits[i] != 9) {
+6                digits[i] = digits[i]+1;
+7                return digits;
+8
+9            } else {
+10                digits[i] = 0; //[1,2,9]
+11            }
+12
+13        }
+14        // 每一位都是0 
+15        digits = new int[len+1];
+16        digits[0] = 1;
+17        return digits;
+18        
+19    }
+20}
+21
+22/**
+23如果末尾！= 9， +1
+24如果末尾 = 9， digits[i] = 0, 向前进一位
+25
+26 */
