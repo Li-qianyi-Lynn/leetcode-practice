a0fbdb27293244d7ac4273fd1e8d6ae65cd6396e
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-10 17:31:12 -0800

    Add solution for Word Break

--- LeetCode/Word-Break/Solution.java
+++ LeetCode/Word-Break/Solution.java
@@ -1,28 +1,20 @@
-1class Solution {
-2    public boolean wordBreak(String s, List<String> wordDict) {
-3        Set<String> words = new HashSet<>(wordDict);
-4        Deque<Integer> dq = new ArrayDeque<>();
-5        boolean[] seen = new boolean[s.length() + 1];
-6        dq.add(0);
-7
-8        while (!dq.isEmpty()) {
-9            int start = dq.pollFirst();
-10            if (start == s.length()) {
-11                return true;
-12            }
-13
-14            for (int end = start + 1; end <= s.length(); end++) {
-15                if (seen[end]) {
-16                    continue;
-17                }
-18
-19                if (words.contains(s.substring(start, end))) {
-20                    dq.offerLast(end);
-21                    seen[end] = true;
-22                }
-23            }
-24        }
-25
-26        return false;
-27    }
-28}
+    # Graph
+    #   * node: s position
+    #   * edge: word
+    #   * if we can reach target node (last letter), return True
+    def wordBreakBFS(self, s: str, wordDict: List[str]) -> bool:
+        queue = deque([0])  # queue represents positions we can cover up to
+        visited = set()
+
+        while queue:
+            cur = queue.popleft()
+            for word in wordDict:
+                new = cur+len(word)
+                if new not in visited and word == s[cur:new]:
+                    if new == len(s):
+                        return True
+                    else:
+                        queue.append(new)
+                        visited.add(new)
+
+        return False
