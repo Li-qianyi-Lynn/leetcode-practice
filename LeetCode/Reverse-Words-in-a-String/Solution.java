0de3aecad63c2422029def33d098893c791e9608
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-12 12:43:43 -0800

    Add solution for Reverse Words in a String

--- LeetCode/Reverse-Words-in-a-String/Solution.java
+++ LeetCode/Reverse-Words-in-a-String/Solution.java
@@ -1,11 +1,27 @@
-class Solution {
-    public String reverseWords(String s) {
-        String[] words = s.trim().split(" ");
-        StringBuilder result = new StringBuilder();
-        for (int i=words.length-1; i>=0; i--) {
-            if (!words[i].isEmpty())
-                result.append(words[i]).append(" ");
-        }
-        return result.toString().trim();
-    }
-}
+1class Solution {
+2    public String reverseWords(String s) {
+3        String[] words = s.trim().split("\\s+");
+4        StringBuilder sb = new StringBuilder();
+5        for (int i = words.length-1; i >= 0; i--) {
+6            if (i == 0) {
+7                sb.append(words[i]);
+8
+9            } else {
+10                sb.append(words[i]).append(" ");
+11
+12            }
+13           
+14        }
+15        return sb.toString();
+16        
+17
+18    }
+19}
+20
+21/***
+221. split space
+232. for loop：把string + space 加到stringbuilder 里，删去最后一个space
+24
+25
+26
+27 */
