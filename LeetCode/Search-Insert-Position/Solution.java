5126cab4f991dca5fbae361663d1e2a8e7b5926d
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-14 22:09:20 -0800

    Add solution for Search Insert Position

--- LeetCode/Search-Insert-Position/Solution.java
+++ LeetCode/Search-Insert-Position/Solution.java
@@ -1,24 +1,23 @@
 1class Solution {
 2    public int searchInsert(int[] nums, int target) {
-3        int r = nums.length-1;
+3        int r = nums.length;
 4        int l = 0;
 5
-6        while (l <= r) {
+6        while (l < r) {
 7            int mid = l + (r-l) / 2;
-8            if (nums[mid] == target) {
-9                return mid;
-10
-11            } else if (nums[mid] < target) {
-12                l = mid+1;
-13
-14            } else {
-15                r = mid -1;
-16            }
-17
-18        }
-19
-20        return l;
-21
-22        
-23    }
-24}
+8            
+9            if (nums[mid] >= target) {
+10                
+11                r = mid;
+12
+13            } else {
+14                l = mid+1;
+15            }
+16
+17        }
+18
+19        return l;
+20
+21        
+22    }
+23}
