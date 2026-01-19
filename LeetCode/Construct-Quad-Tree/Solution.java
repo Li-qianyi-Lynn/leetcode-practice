2ee312aec95c839e749125bdf43dbd18344a5d1d
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-18 19:14:17 -0800

    Add solution for Construct Quad Tree

--- LeetCode/Construct-Quad-Tree/Solution.java
+++ LeetCode/Construct-Quad-Tree/Solution.java
@@ -40,38 +40,61 @@
 40
 41class Solution {
 42    public Node construct(int[][] grid) {
-43        return helper(grid, 0, 0, grid.length);
-44    }
-45
-46    private Node helper(int[][] grid, int r, int c, int size) {
-47        // 1. 检查当前区域是否所有值都相同
-48        if (isAllSame(grid, r, c, size)) {
-49            // 如果相同，创建一个叶子节点
-50            // grid[r][c] == 1 表示 true，0 表示 false
-51            return new Node(grid[r][c] == 1, true);
-52        }
-53
-54        // 2. 如果不相同，创建一个非叶子节点，并递归拆分
-55        Node node = new Node(true, false); // val 可以随便设，isLeaf 设为 false
-56        int half = size / 2;
-57
-58        node.topLeft = helper(grid, r, c, half);
-59        node.topRight = helper(grid, r, c + half, half);
-60        node.bottomLeft = helper(grid, r + half, c, half);
-61        node.bottomRight = helper(grid, r + half, c + half, half);
-62
-63        return node;
-64    }
+43        //puring?
+44        return rec(grid,grid.length,0,0);
+45        
+46    }
+47    private Node rec(int[][] grid, int size, int r, int c) {
+48        if (isSame(grid,r,c,size)) {
+49            if (grid[r][c] == 0) {
+50                return new Node (false, true);
+51
+52            } else {
+53                return new Node (true, true);
+54            }
+55
+56        }
+57        Node leaf = new Node (false, false);
+58        size = size / 2;
+59        leaf.topLeft = rec(grid, size, r, c);
+60        leaf.topRight = rec(grid, size, r, c+size);
+61        leaf.bottomLeft = rec(grid, size, r+size, c);
+62        leaf.bottomRight = rec(grid, size, r+size, c+size);
+63
+64        return leaf;
 65
-66    private boolean isAllSame(int[][] grid, int r, int c, int size) {
-67        int val = grid[r][c];
-68        for (int i = r; i < r + size; i++) {
-69            for (int j = c; j < c + size; j++) {
-70                if (grid[i][j] != val) {
-71                    return false;
-72                }
-73            }
-74        }
-75        return true;
-76    }
-77}
+66    }
+67
+68    private boolean isSame(int[][] grid, int r, int c, int size) {
+69        int check = grid[r][c];
+70        for (int i = r; i < r+size; i++) {
+71            for (int j = c; j < c+size; j++ ) {
+72                if (grid[i][j] != check) {
+73                    return false;
+74                }
+75            }
+76        }
+77        return true;
+78
+79    }
+80}
+81
+82/**
+83helper function: 
+841. same or not? 2 for loops check grid[i][j] is equal to grid[base][base] or not?
+852. recursion: 划分4个区域进行递归
+86
+87- all same return new node ( grid[0][0] == 1 true; grid[0][0] == 0 false, true) c
+88- not same 
+89    - new node (true, false)
+90
+91    拆分！
+92
+93    node topLeft
+94    Node topRight
+95    Node bottomLeft
+96    Node bottomRight
+97
+98    return node
+99
+100 */
