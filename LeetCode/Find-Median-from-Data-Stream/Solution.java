47d8c8366c63d325f3a135c933607e28003a3088
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-19 00:25:02 -0800

    Add solution for Find Median from Data Stream

---
+++ LeetCode/Find-Median-from-Data-Stream/Solution.java
@@ -0,0 +1,29 @@
+Adding number 41
+MaxHeap lo: [41]           // MaxHeap stores the largest value at the top (index 0)
+MinHeap hi: []             // MinHeap stores the smallest value at the top (index 0)
+Median is 41
+=======================
+Adding number 35
+MaxHeap lo: [35]
+MinHeap hi: [41]
+Median is 38
+=======================
+Adding number 62
+MaxHeap lo: [41, 35]
+MinHeap hi: [62]
+Median is 41
+=======================
+Adding number 4
+MaxHeap lo: [35, 4]
+MinHeap hi: [41, 62]
+Median is 38
+=======================
+Adding number 97
+MaxHeap lo: [41, 35, 4]
+MinHeap hi: [62, 97]
+Median is 41
+=======================
+Adding number 108
+MaxHeap lo: [41, 35, 4]
+MinHeap hi: [62, 97, 108]
+Median is 51.5
