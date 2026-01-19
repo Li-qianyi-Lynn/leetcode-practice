a8cb449e517912a5993701b823b509593caa91ab
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-18 20:54:17 -0800

    Add solution for Convert Sorted List to Binary Search Tree

---
+++ LeetCode/Convert-Sorted-List-to-Binary-Search-Tree/Solution.java
@@ -0,0 +1,7 @@
+[findLengthFastAndSlowPtr] Time taken to find the size of the list: 0.343826
+[findLengthSinglePointer] Time taken to find the size of the list: 0.316219
+Length of linked list: 100000001
+
+[findMiddle2Pointers] Time taken to find the mid: 0.270065
+[findMiddleSinglePointer] Time taken to find the mid: 0.490117
+Middle Element of linked list: 50000000
