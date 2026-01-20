de89606595213742a5f7d524faff27b41d2ee49a
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-20 00:23:55 -0800

    Add solution for Remove Duplicates from Sorted Array II

--- LeetCode/Remove-Duplicates-from-Sorted-Array-II/Solution.java
+++ LeetCode/Remove-Duplicates-from-Sorted-Array-II/Solution.java
@@ -1,34 +1,41 @@
 1class Solution {
 2    public int removeDuplicates(int[] nums) {
-3        if (nums.length == 0) {
-4            return 0;
+3        if (nums.length <= 2) {
+4            return nums.length;    
 5
 6        }
-7
-8        int base = 1;
-9        int valid = 1; //  track position for valid elements
-10        int count =1;
-11
-12        while (base < nums.length) {
-13            if (nums[base] == nums[base-1]) {
-14                count++;
-15                if (count > 2) {
-16                    base++;
+7        int fast;
+8        int slow = 1;
+9        int times = 1;
+10        for (fast = 1; fast < nums.length; fast++) {
+11            
+12            if (nums[fast] == nums[fast-1]) {
+13                if (times < 2) {
+14                    times++;
+15
+16                } else {
 17                    continue;
 18                }
-19
-20            } else {
-21                count =1;
-22            }
-23            nums[valid] = nums[base];
-24            valid++;
-25            base++;
-26
-27        }
-28        return valid;
-29       
-30        
-31    }
-32}
-33
-34
+19            } else {
+20                times = 1;
+21            }
+22            nums[slow] = nums[fast];
+23            slow++;
+24
+25        }
+26        return slow;
+27        
+28        
+29    }
+30}
+31
+32/**
+33two pointers
+34fast: iterate nums, calculate nums[i] appeared how many times
+35if times >= 2
+36nums[slow] = nums[fast+1]; times = 1; slow++
+37
+38
+39
+40
+41 */
