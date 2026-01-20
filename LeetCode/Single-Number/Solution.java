a3136683e477bd127d030b272f088b34b350172f
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-20 15:39:57 -0800

    Add solution for Single Number

--- LeetCode/Single-Number/Solution.java
+++ LeetCode/Single-Number/Solution.java
@@ -1,29 +1,18 @@
 1class Solution {
 2    public int singleNumber(int[] nums) {
-3        if (nums.length == 1) {
-4            return nums[0];
-5
-6        }
-7        Set<Integer> set = new HashSet<>();
-8        int sum = 0;
-9        int duplicate = 0;
-10        for (int num : nums) {
-11            if (set.contains(num)) {
-12                duplicate += num;
-13
-14            }
-15            set.add(num);
-16            sum += num;
+3        int a = 0;
+4        for (int num : nums) {
+5            a ^= num;
+6
+7        }
+8        return a;
+9        
+10    }
+11}
+12
+13/**
+14
+15tc:O(n)
+16sc:O(1)
 17
-18        }
-19        return sum - 2* duplicate;
-20        
-21    }
-22}
-23
-24/**
-25
-26tc:O(n)
-27sc:O(1)
-28
-29 */
+18 */
