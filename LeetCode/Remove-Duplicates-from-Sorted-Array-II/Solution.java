81417266481d25e9687d4d90cbfd31d6a5cfade2
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-20 00:13:18 -0800

    Add solution for Remove Duplicates from Sorted Array II

--- LeetCode/Remove-Duplicates-from-Sorted-Array-II/Solution.java
+++ LeetCode/Remove-Duplicates-from-Sorted-Array-II/Solution.java
@@ -1,10 +1,34 @@
-  public int removeDuplicates(int[] nums) {
-    if (nums.length <= 2) return nums.length;
-    
-    int j = 2;
-    for(int i = 2; i < nums.length; i++) {
-      nums[j] = nums[i];
-      if (nums[j] != nums[j - 2]) j++;
-    }
-    return j;
-  }
+1class Solution {
+2    public int removeDuplicates(int[] nums) {
+3        if (nums.length == 0) {
+4            return 0;
+5
+6        }
+7
+8        int base = 1;
+9        int valid = 1; //  track position for valid elements
+10        int count =1;
+11
+12        while (base < nums.length) {
+13            if (nums[base] == nums[base-1]) {
+14                count++;
+15                if (count > 2) {
+16                    base++;
+17                    continue;
+18                }
+19
+20            } else {
+21                count =1;
+22            }
+23            nums[valid] = nums[base];
+24            valid++;
+25            base++;
+26
+27        }
+28        return valid;
+29       
+30        
+31    }
+32}
+33
+34
