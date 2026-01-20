3f8f62d70f4d6ce805d5cd6e42627585006cafc3
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-19 20:44:39 -0800

    Add solution for Letter Combinations of a Phone Number

--- LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.java
+++ LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.java
@@ -1,7 +1,7 @@
 1class Solution {
-2    
-3
-4    public List<String> letterCombinations(String digits) {
+2    public List<String> letterCombinations(String digits) {
+3        StringBuilder sb = new StringBuilder();
+4        List<String> res = new ArrayList<>();
 5        Map<Character, String> map = new HashMap<>();
 6        map.put('2', "abc");
 7        map.put('3', "def");
@@ -11,25 +11,25 @@
 11        map.put('7', "pqrs");
 12        map.put('8', "tuv");
 13        map.put('9', "wxyz");
-14        List<String> res = new ArrayList<>();
-15        StringBuilder sb = new StringBuilder();
-16        backtracking(map, sb, digits, res, 0); //todo
+14        backtracking(digits, sb, res, 0, map);
+15        return res;
+16
 17
-18        return res;     
+18        
 19    }
 20
-21    private void backtracking(Map<Character, String> map, StringBuilder cur, String digits,List<String>res, int index) {
-22        //base case
-23        if (cur.length() == digits.length()) {
-24            res.add(cur.toString());
+21    private void backtracking(String digits, StringBuilder sb, List<String> res, int index, Map<Character, String> map) {
+22        // base case
+23        if (sb.length() == digits.length()) {
+24            res.add(sb.toString());
 25            return;
-26        }
-27
-28        String temp = map.get(digits.charAt(index));
-29        for (int i = 0; i < temp.length(); i++) {
-30            cur.append(temp.charAt(i));
-31            backtracking(map, cur, digits, res, index+1);
-32            cur.deleteCharAt(cur.length()-1);
+26
+27        }
+28        String cur = map.get(digits.charAt(index));
+29        for (int i = 0; i < cur.length(); i++) { //abc
+30            sb.append(cur.charAt(i));
+31            backtracking(digits, sb, res, index+1, map); // digits 
+32            sb.deleteCharAt(sb.length()-1);
 33
 34        }
 35
@@ -37,16 +37,12 @@
 37}
 38
 39/**
-40map: Character -> String
-41iterate digits, for each digits, get the value
-42backtrack()
-431. base case: sb.size() == digits.length(); add cur sb to res;
-442. get mappings string
-453. iterate mapping string
-46    append charAt(i)
-47    backtrack() next num
-48    deleteCharAt(len-1);
-49
-50
-51
-52 */
+40
+41map: char(num) -> String
+42build String: pick num in digits, get the char, add the second char 
+43backtracking
+441. base case: if sb.size() == digits.length(), return 
+452. backtracking: iterate mapping string, cur.append ; index +1
+463. remove added value, come to next round
+47
+48 */
