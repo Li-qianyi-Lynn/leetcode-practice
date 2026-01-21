b5ef695a6a9bcb43870baacfe9b1c98e5bcb4d49
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-20 19:29:24 -0800

    Add solution for Remove Duplicates from Sorted Array II

--- LeetCode/Remove-Duplicates-from-Sorted-Array-II/Solution.java
+++ LeetCode/Remove-Duplicates-from-Sorted-Array-II/Solution.java
@@ -1,41 +1,42 @@
 1class Solution {
 2    public int removeDuplicates(int[] nums) {
-3        if (nums.length <= 2) {
-4            return nums.length;    
+3        if (nums.length == 1) {
+4            return 1;
 5
 6        }
-7        int fast;
-8        int slow = 1;
-9        int times = 1;
-10        for (fast = 1; fast < nums.length; fast++) {
-11            
-12            if (nums[fast] == nums[fast-1]) {
-13                if (times < 2) {
-14                    times++;
+7        int valid = 1;
+8        int times = 1;
+9        for (int i = 1; i < nums.length; i++) {
+10            if (nums[i] == nums[i-1]) {
+11                if (times <= 1) {
+12                    times ++;
+13                    // nums[valid] = nums[i];
+14                    // valid++;
 15
 16                } else {
 17                    continue;
 18                }
-19            } else {
-20                times = 1;
-21            }
-22            nums[slow] = nums[fast];
-23            slow++;
+19            
+20            } else {
+21                times = 1;
+22               
+23            }
 24
-25        }
-26        return slow;
-27        
-28        
-29    }
-30}
-31
+25            nums[valid] = nums[i];
+26            valid++;
+27        }
+28        return valid;
+29        
+30    }
+31}
 32/**
 33two pointers
-34fast: iterate nums, calculate nums[i] appeared how many times
-35if times >= 2
-36nums[slow] = nums[fast+1]; times = 1; slow++
+34
+35valid index
+36i regular pointers
 37
-38
-39
+38nums[i] == nums[i-1], times++
+39check times     
 40
-41 */
+41
+42 */
