f8753c86099513f51ff3d642e719e05c6e408bc7
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-22 23:18:47 -0800

    Add solution for Gas Station

--- LeetCode/Gas-Station/Solution.java
+++ LeetCode/Gas-Station/Solution.java
@@ -1,43 +1,34 @@
 1class Solution {
 2    public int canCompleteCircuit(int[] gas, int[] cost) {
-3        int curTank = 0;
-4        int totalDiff = 0; // must >0 gas - cost 
+3        int total = 0;
+4        int curTank = 0;
 5        int start = 0;
-6
-7        for (int i = 0; i < gas.length; i++) {
-8            int diff = gas[i] - cost[i];
-9            totalDiff += diff;
-10            curTank += diff; // if < 0 that means we cant reach the next station
-11
-12            if (curTank < 0) {
-13                start = i+1;
-14                curTank = 0; // start again
-15
-16            }
-17        }
-18        return totalDiff >= 0 ? start : -1;
-19   
-20    }
-21}
-22
-23/**
+6        for (int i = 0; i < gas.length; i++) {
+7            int diff = gas[i] - cost[i];
+8            total += diff;
+9            curTank += diff;
+10            if (curTank < 0) {
+11                start = i+1;
+12                curTank = 0;
+13            }
+14
+15        }
+16        return total >= 0 ? start : -1;    
+17    }
+18}
+19
+20/**
+21input :int[] gas, int[] cost
+22output: int: the starting gas station's index  or -1
+23
 24
-25规则 A：总油量必须大于总消耗
-26
-27算差值：先把 gas 和 cost 变成一个净增量的概念（gas[i] - cost[i]）。
-28
-29判总和：先看总账。如果总油量不够总消耗，直接判死刑（返回 -1）。
-30
-31找拐点：
-32
-33从 0 站出发，一边走一边累加。
-34
-35一旦累加和变成负数（这就叫“赤字”），说明之前那段路（从旧起点到现在这站）全都不行。
-36
-37清零，换下一站当起点重新出发。
-38
-39拿结果：当你走完最后一站，剩下的那个起点就是唯一答案（因为题目保证了如果有解则唯一）。
-40
-41
-42
-43 */
+25rule : all gas we obtained >= we cost
+26diff : gas[i] - cost[i]
+27
+28int total
+29
+30for loop to iterate gas[]
+31how to find we cant come the next station?
+32curTank < 0
+33we should update the index -> we start the new journey +1
+34 */
