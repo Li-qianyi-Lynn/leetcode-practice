b11101a96ffab6caeb09e43d8b5e2e4421098fdb
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-24 16:55:48 -0800

    Add solution for 3Sum

--- LeetCode/3Sum/Solution.java
+++ LeetCode/3Sum/Solution.java
@@ -12,47 +12,52 @@
 12
 13
 14        step3: add res to results;
-15        
-16     
-17         */
-18
-19        int len = nums.length;
-20        Arrays.sort(nums);
-21         
-22        for (int i = 0; i < len; i++) {
-23            if (nums[i] > 0) {
-24                break; // stop for-loop (no possibilities to get the answer)
-25            }
-26            
-27            if (i > 0 && nums[i] == nums[i-1]) { //duplicate i
-28                continue; // next i
-29            }
-30            
-31            int l = i+1;
-32            int r = len-1;
-33
-34            while (l < r) {
-35                if (nums[i] + nums[l] + nums[r] < 0) {
-36                    l++;
-37                } else if (nums[i] + nums[l] + nums[r] > 0) {
-38                    r--;
-39                } else {
-40                    
-41                    res.add(Arrays.asList(nums[i], nums[l], nums[r])); 
-42                    l++;
-43                    r--;
-44                    
+15
+16        tc:O(n* n)
+17        sc:O(1)
+18        
+19     
+20         */
+21
+22        int len = nums.length;
+23        Arrays.sort(nums);
+24        for (int i = 0; i < nums.length; i++) {
+25            if (nums[i] > 0) {
+26                break;
+27            }
+28            if (i > 0 && nums[i] == nums[i-1]) {
+29                continue;
+30            }
+31
+32            int l = i+1;
+33            int r = len-1;
+34
+35            while (l < r) {
+36                if ( nums[i] + nums[l] + nums[r] < 0) {
+37                    l++;
+38                } else if (nums[i] + nums[l] + nums[r] > 0) {
+39                    r--;
+40                } else {
+41                    
+42                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+43                    l++;
+44                    r--;
 45                    while (l < r && nums[l] == nums[l-1]) {
 46                        l++;
 47                    }
 48                    while (l < r && nums[r] == nums[r+1]) {
 49                        r--;
-50                    }
-51                   
-52                }
-53
-54            }
-55        }
-56        return res;   
-57    }
-58}
+50
+51                    }
+52
+53                   
+54                    
+55                }
+56
+57            }
+58        }
+59        return res;
+60
+61        
+62    }
+63}
