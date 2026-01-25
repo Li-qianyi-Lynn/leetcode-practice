6017534a9a283529964cce30084f25b3014c9bec
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-24 22:09:12 -0800

    Add solution for Sqrt(x)

--- LeetCode/Sqrt(x)/Solution.java
+++ LeetCode/Sqrt(x)/Solution.java
@@ -1,24 +1,25 @@
-class Solution {
-    public int mySqrt(int x) {
-    if (x == 0 || x == 1) {
-        return x;
-    }
-    
-    int left = 1, right = x / 2;
-    
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        long square = (long)mid * mid;
-        
-        if (square == x) {
-            return mid;
-        } else if (square < x) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    
-    return right; // right 是最后一个满足 mid*mid <= x 的值
-}
-}
+1class Solution {
+2    public int mySqrt(int x) {
+3    if (x == 0 || x == 1) {
+4        return x;
+5    }
+6    
+7    int left = 1;
+8    int right = x / 2;
+9    
+10    while (left <= right) {
+11        int mid = left + (right - left) / 2;
+12        long square = (long)mid * mid;
+13        
+14        if (square == x) {
+15            return mid;
+16        } else if (square < x) {
+17            left = mid + 1;
+18        } else {
+19            right = mid - 1;
+20        }
+21    }
+22    
+23    return right; // right 是最后一个满足 mid*mid <= x 的值
+24    }
+25}
