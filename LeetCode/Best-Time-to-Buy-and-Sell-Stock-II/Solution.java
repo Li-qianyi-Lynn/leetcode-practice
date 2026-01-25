a7bb7a7570b636127e569108609d88c45d1042a3
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-24 18:58:28 -0800

    Add solution for Best Time to Buy and Sell Stock II

--- LeetCode/Best-Time-to-Buy-and-Sell-Stock-II/Solution.java
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock-II/Solution.java
@@ -1,6 +1,33 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        for i in range(1, len(prices)):
-            profit += max(prices[i]-prices[i-1], 0)
-        return profit
+1class Solution {
+2    public int maxProfit(int[] prices) {
+3        if (prices.length == 1) {
+4            return 0;
+5
+6        }
+7        int total = 0;
+8        for (int i = 1; i < prices.length; i++) {
+9            if (prices[i]> prices[i-1]) {
+10                int diff = prices[i]- prices[i-1];
+11                total += diff;
+12
+13            }
+14
+15        }
+16        return total;
+17    
+18
+19    }
+20}
+21
+22/**
+23buy and sell multiple times
+24[7,1,5,3,6,4]
+25
+26[1,2,2,4,5]
+27total += diff 
+28
+29
+30
+31
+32
+33 */
