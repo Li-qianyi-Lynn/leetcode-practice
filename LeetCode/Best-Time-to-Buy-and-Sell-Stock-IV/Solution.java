cebd12f84c844381b7d0b2ea869ff328cedb4176
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-25 21:49:47 -0800

    Add solution for Best Time to Buy and Sell Stock IV

--- LeetCode/Best-Time-to-Buy-and-Sell-Stock-IV/Solution.java
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock-IV/Solution.java
@@ -1,36 +1,51 @@
 1class Solution {
 2    public int maxProfit(int k, int[] prices) {
-3        int n = prices.length;
-4        // days | times | status
-5        int[][][] dp= new int [n][k+1][2];
+3        int len = prices.length;
+4        
+5        int[][][] dp = new int[len][k+1][2]; //days | times | status
 6
-7        if (k > n/2) { //no limited use greedy
-8            int total = 0;
-9            for (int i = 1; i < n; i++) {
-10                if (prices[i] > prices[i-1]) {
-11                    total += prices[i] - prices[i-1];
-12                }
-13
+7        //pruning
+8        if (k > len/2) { //limited times, use greedy
+9            int total = 0;
+10            for (int i = 1; i < len; i++) {
+11                if (prices[i] > prices[i-1]) { // can make a profit
+12                    total += prices[i] - prices[i-1];
+13                }
 14            }
 15            return total;
-16        }
-17
-18        //day0
-19        for (int i = 0; i < k+1; i++) {
-20            dp[0][i][0] = -prices[0]; //buy
-21            dp[0][i][1] = 0; // sell
-22
-23        }
-24
-25        for (int i = 1; i < n; i++) {
-26            for (int j = 1; j < k+1;j++) {
-27                dp[i][j][0] = Math.max(dp[i-1][j][0], dp[i-1][j-1][1]-prices[i]); // buy: 上一次卖完剩下的钱再买
-28                dp[i][j][1] = Math.max(dp[i-1][j][1],dp[i][j][0] + prices[i]);  // sell
-29
-30            }
-31
-32        }
-33        return dp[n-1][k][1];
-34        
-35    }
-36}
+16
+17        }
+18        
+19        
+20        // day0 
+21
+22        for (int i = 0; i < k+1; i++) {
+23            dp[0][i][0] = -prices[0]; // after bought what i have
+24            dp[0][i][1] = 0;  // sold
+25        }
+26
+27        for (int i = 1; i< len; i++) { // for days
+28            for (int j = 1; j < k+1; j++) { // for transactions
+29                dp[i][j][0] = Math.max(dp[i-1][j][0], dp[i-1][j-1][1]-prices[i]);
+30                dp[i][j][1] = Math.max(dp[i-1][j][1], dp[i][j][0]+prices[i]);
+31            }
+32
+33        }
+34        return dp[len-1][k][1];
+35
+36        
+37    }
+38}
+39
+40/**
+41input:
+42-k: only k transactions
+43-int[] prices
+44
+45output: 
+46int: max profit
+47
+48dp: status change, profit is only depends on the previous status
+49
+50
+51 */
