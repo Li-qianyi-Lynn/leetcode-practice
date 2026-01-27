b96fc866e4e198d24a3ddf973e9687f6f52d955a
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-27 13:09:54 -0800

    Add solution for Linked List Cycle

--- LeetCode/Linked-List-Cycle/Solution.java
+++ LeetCode/Linked-List-Cycle/Solution.java
@@ -15,16 +15,17 @@
 15
 16        ListNode fast = head;
 17        ListNode slow = head;
-18
-19        while (fast != null && fast.next != null) {
-20            slow = slow.next;
-21            fast = fast.next.next;
-22
-23            if (slow == fast) return true;
-24
+18        while (fast!= null && fast.next != null) {
+19            slow = slow.next;
+20            fast = fast.next.next;
+21
+22            if (fast == slow) {
+23                return true;
+24            }
 25
 26        }
 27        return false;
-28        
-29    }
-30}
+28
+29       
+30    }
+31}
