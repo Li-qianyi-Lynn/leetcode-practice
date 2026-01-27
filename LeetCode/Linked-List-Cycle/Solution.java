8d86e6437c4719aba3c4c8aa7a7a4d4a13615d4e
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-27 13:07:23 -0800

    Add solution for Linked List Cycle

--- LeetCode/Linked-List-Cycle/Solution.java
+++ LeetCode/Linked-List-Cycle/Solution.java
@@ -1,30 +1,30 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
-    public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) return false;
-
-        ListNode fast = head;
-        ListNode slow = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-
-            if (slow == fast) return true;
-
-
-        }
-        return false;
-        
-    }
-}
+1/**
+2 * Definition for singly-linked list.
+3 * class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public boolean hasCycle(ListNode head) {
+14        if (head == null || head.next == null) return false;
+15
+16        ListNode fast = head;
+17        ListNode slow = head;
+18
+19        while (fast != null && fast.next != null) {
+20            slow = slow.next;
+21            fast = fast.next.next;
+22
+23            if (slow == fast) return true;
+24
+25
+26        }
+27        return false;
+28        
+29    }
+30}
