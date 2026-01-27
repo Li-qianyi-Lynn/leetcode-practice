6a12490e909ef873d43fa574d3f0dc6e3d9d69d5
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-26 16:57:38 -0800

    Add solution for Binary Tree Zigzag Level Order Traversal

--- LeetCode/Binary-Tree-Zigzag-Level-Order-Traversal/Solution.java
+++ LeetCode/Binary-Tree-Zigzag-Level-Order-Traversal/Solution.java
@@ -1,54 +1,36 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (root == null) return res;
-        bfs(root, res);
-        return res;   
-    }
-
-    private void bfs(TreeNode root, List<List<Integer>>res) {
-        if (root == null) return;
-        Deque<TreeNode> deque = new LinkedList<>();
-        boolean leftToRight = true;
-
-        deque.offerLast(root);
-        while(!deque.isEmpty()) {
-            int size = deque.size();
-            List<Integer> levelNode= new LinkedList<>();
-
-            for (int i = 1; i <= size; i++) {
-                TreeNode poll = deque.pollFirst();
-                levelNode.add(poll.val);
-                if (poll.left != null) {
-                        deque.offerLast(poll.left);
-                    } 
-                    if (poll.right != null) {
-                        deque.offerLast(poll.right);
-                    }
-                
-            }
-            if (!leftToRight) {
-                Collections.reverse(levelNode);
-            }
-            res.add(levelNode);
-            leftToRight = !leftToRight;
+        List<List<Integer>> list = new LinkedList<>();
+        if(root == null){
+            return list;
         }
         
-    
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        boolean even = true;
+        
+        while(!q.isEmpty()){
+            int n = q.size();
+            LinkedList<Integer> subList = new LinkedList<>();
+            
+            for(int i = 0; i < n; i++){
+                TreeNode top = q.poll();
+                
+                if(top.left != null){
+                    q.add(top.left);
+                }
+                if(top.right != null){
+                    q.add(top.right);
+                }
+                if(even)
+                    subList.add(top.val);
+                else
+                    subList.addFirst(top.val);
+            }
+            list.add(subList);
+            even = !even;
+        }
+        
+        return list;
     }
 }
