39a4e50245cfc8367ae71ec380250a7aeb9faae2
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-27 12:45:06 -0800

    Add solution for Merge k Sorted Lists

--- LeetCode/Merge-k-Sorted-Lists/Solution.java
+++ LeetCode/Merge-k-Sorted-Lists/Solution.java
@@ -1,22 +1,46 @@
-public ListNode mergeKLists(ListNode[] lists) {
-    List<Integer> l = new ArrayList<Integer>();
-   
-    for (ListNode ln : lists) {
-        while (ln != null) {
-            l.add(ln.val);
-            ln = ln.next;
-        }
-    }
-   
-    Collections.sort(l);
- 
-    ListNode head = new ListNode(0);
-    ListNode h = head;
-    for (int i : l) {
-        ListNode t = new ListNode(i);
-        h.next = t;
-        h = h.next;
-    }
-    h.next = null;
-    return head.next;
-}
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode mergeKLists(ListNode[] lists) {
+13        if (lists.length == 0) {
+14            return null;
+15
+16        }
+17
+18        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+19        for (ListNode list : lists) {
+20            ListNode cur = list;
+21            while (cur != null) {
+22                pq.offer(cur);
+23                cur = cur.next;
+24            }
+25
+26        }
+27
+28        ListNode dummy = new ListNode(-1);
+29        ListNode tail = dummy;
+30        while (!pq.isEmpty()) {
+31            ListNode poll = pq.poll();
+32            tail.next = poll;
+33            tail = tail.next;
+34
+35        }
+36        tail.next = null;
+37        return dummy.next;
+38    }
+39}
+40
+41/**
+42iterate list，put all node into pq;
+43build result
+44
+45
+46 */
