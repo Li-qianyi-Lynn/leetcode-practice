db5f1b279de7898a0a620366e8baac41e7a20859
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-28 09:56:32 -0800

    Add solution for Make Array Empty

--- LeetCode/Make-Array-Empty/Solution.java
+++ LeetCode/Make-Array-Empty/Solution.java
@@ -1,11 +1,14 @@
-    public long countOperationsToEmptyArray(int[] A) {
-        Map<Integer, Integer> pos = new HashMap<>();
-        long n = A.length, res = n;
-        for (int i = 0; i < n; ++i)
-            pos.put(A[i], i);
-        Arrays.sort(A);
-        for (int i = 1; i < n; ++i)
-            if (pos.get(A[i]) < pos.get(A[i - 1]))
-                res += n - i;
-        return res;
-    }
+1class Solution {
+2    public long countOperationsToEmptyArray(int[] nums) {
+3        Map<Integer, Integer> pos = new HashMap<>();
+4        long n = nums.length, res = n;
+5        for (int i = 0; i < n; ++i)
+6            pos.put(nums[i], i);
+7        Arrays.sort(nums);
+8        for (int i = 1; i < n; ++i)
+9            if (pos.get(nums[i]) < pos.get(nums[i - 1]))
+10                res += n - i;
+11        return res;
+12        
+13    }
+14}
