f0128d864344c70cf7af3c5e741f4c2c981e6dca
Qianyi Li <li.qianyi@northeastern.edu>
2026-01-28 09:36:48 -0800

    Add solution for House Robber

---
+++ LeetCode/House-Robber/Solution.java
@@ -0,0 +1,13 @@
+    def rob(self, nums: List[int]) -> int:
+        
+        if len(nums) == 1:
+            return nums[0]
+        
+        bounty = [None] * len(nums)
+        bounty[0] = nums[0]
+        bounty[1] = max(nums[0],nums[1])
+        
+        for i in range(2,len(nums)):
+            bounty[i] = max(bounty[i-2]+nums[i], bounty[i-1])
+        
+        return max(bounty)
