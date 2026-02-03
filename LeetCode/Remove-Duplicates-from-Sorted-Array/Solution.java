1765501cfbab89e4f045ed48ac74b733b129764c
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-03 15:16:07 -0800

    Add solution for Remove Duplicates from Sorted Array

--- LeetCode/Remove-Duplicates-from-Sorted-Array/Solution.java
+++ LeetCode/Remove-Duplicates-from-Sorted-Array/Solution.java
@@ -1,14 +1,21 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        l = r = 0
-        
-        while r < len(nums):
-            
-            while r + 1 < len(nums) and nums[r+1] == nums[r]:
-                r += 1
-                
-            nums[l] = nums[r]
-            l += 1
-            r += 1
-            
-        return l
+1class Solution {
+2    public int removeDuplicates(int[] nums) {
+3        if (nums.length == 1) {
+4            return 1;
+5
+6        }
+7
+8        int valid = 1;
+9        int cur;
+10
+11        for (cur = 1; cur < nums.length; cur++) {
+12            if (nums[cur] != nums[cur-1]) {
+13                nums[valid] = nums[cur];
+14                valid ++;
+15            }
+16
+17        }
+18        return valid;
+19        
+20    }
+21}
