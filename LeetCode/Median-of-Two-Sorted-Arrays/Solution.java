901b9fa6a152bd03091b74dc8292112bb4036bfa
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-03 22:41:18 -0800

    Add solution for Median of Two Sorted Arrays

--- LeetCode/Median-of-Two-Sorted-Arrays/Solution.java
+++ LeetCode/Median-of-Two-Sorted-Arrays/Solution.java
@@ -15,78 +15,100 @@
 15            int i = l + (r - l)/2;
 16            int j = totalLeft - i;
 17
-18    
-19            if (i >= 1 && nums1[i-1] > nums2[j]) {
-20                r = i-1;
-21
-22            } else if ( i < m && nums2[j-1] > nums1[i]) {
-23                l = i+1;
-24            } else { // find the break point
-25                // leftmax rightmin
-26                double leftMax = Integer.MIN_VALUE;
-27                if (i == 0) {
-28                    leftMax = nums2[j-1];
-29                } else if (j == 0) {
-30                    leftMax = nums1[i-1];
-31
-32                } else {
-33                    leftMax = Math.max(nums1[i-1], nums2[j-1]);
-34                }
-35                if ((m+n) % 2 == 1) {
-36                    return leftMax;
-37                }
-38
-39                double rightMin = Integer.MAX_VALUE;
-40                if (i == m) {
-41                    rightMin = nums2[j];
-42                } else if (j == n) {
-43                    rightMin = nums1[i];
-44                } else {
-45                    rightMin = Math.min(nums1[i], nums2[j]);
-46                }
-47
-48                return (rightMin + leftMax) / 2.0;
-49            }
-50
-51        }
-52        return 0.0;    
-53    }
-54}
-55
-56/**
-57
-58input：nums1[] nums2[] sorted ascend
-59output: double
-60
-61binary search
-62|1  3  4.     2. | 5
-63  i-1. i      j-1.  j
-64left: 123
-65right: 56
-66
-671. break point 
-68left = 0;
-69right = m;
-70
-71nums1: i 取了 i 个数字
-72nums2: j 
-73nums1[i-1] <= nums2[j]
-74nums2[j-1] <= nums1[i]
-75
-76以nums1 为定位
+18            int left1;
+19            int right1;
+20            int left2;
+21            int right2;
+22            if (i == 0) {
+23                left1 = Integer.MIN_VALUE;
+24
+25            } else {
+26                left1 = nums1[i-1];
+27            }
+28            if (i == m) {
+29                right1 = Integer.MAX_VALUE;
+30
+31            } else {
+32                right1 = nums1[i];
+33            }
+34
+35            if (j == 0) {
+36                left2 = Integer.MIN_VALUE;
+37
+38            } else {
+39                left2 = nums2[j-1];
+40            }
+41
+42            if (j == n) {
+43                right2 = Integer.MAX_VALUE;
+44            } else {
+45                right2 = nums2[j];
+46            }
+47           
+48
+49
+50            if (left1 > right2) {
+51                r = i-1;
+52
+53            } else if ( left2 > right1) {
+54                l = i+1;
+55            } else { // find the break point
+56                // leftmax rightmin
+57               
+58                double leftMax = Integer.MIN_VALUE;
+59                leftMax = Math.max(left1,left2);
+60               
+61                if ((m+n) % 2 == 1) {
+62                    return leftMax;
+63                }
+64
+65                double rightMin = Integer.MAX_VALUE;
+66               
+67                rightMin = Math.min(right1, right2);
+68            
+69
+70                return (rightMin + leftMax) / 2.0;
+71            }
+72
+73        }
+74        return 0.0;    
+75    }
+76}
 77
-78left = 0;
-79right = m;
-80
-81i = left + (right - left)/2
-82j = totalLeft - i；
-83
-84while (left <= right) 
-85condition1 nums1[i-1] > nums2[j] i-1>= 0 ;right = i-1;
-86condition2 nums2[j-1] > nums1[i] j-1 >= 0; left = i+1;
-87
+78/**
+79
+80input：nums1[] nums2[] sorted ascend
+81output: double
+82
+83binary search
+84|1  3  4.     2. | 5
+85  i-1. i      j-1.  j
+86left: 123
+87right: 56
 88
-892. maxLeft. minRight -> median
-90edge cases
-91
-92 */
+891. break point 
+90left = 0;
+91right = m;
+92
+93nums1: i 取了 i 个数字
+94nums2: j 
+95nums1[i-1] <= nums2[j]
+96nums2[j-1] <= nums1[i]
+97
+98以nums1 为定位
+99
+100left = 0;
+101right = m;
+102
+103i = left + (right - left)/2
+104j = totalLeft - i；
+105
+106while (left <= right) 
+107condition1 nums1[i-1] > nums2[j] i-1>= 0 ;right = i-1;
+108condition2 nums2[j-1] > nums1[i] j-1 >= 0; left = i+1;
+109
+110
+1112. maxLeft. minRight -> median
+112edge cases
+113
+114 */
