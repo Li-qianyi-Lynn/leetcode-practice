3919b16419c355455e418b9f0f5e98fcaab7fc84
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-04 09:41:22 -0800

    Add solution for Linked List Cycle II

--- LeetCode/Linked-List-Cycle-II/Solution.java
+++ LeetCode/Linked-List-Cycle-II/Solution.java
@@ -1,35 +1,34 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        if (head == null || head.next == null) return null;
-
-        ListNode fast = head;
-        ListNode slow = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-
-            if (slow == fast) {
-                slow = head;
-                while (slow != fast) {
-                    slow = slow.next;
-                    fast = fast.next;
-
-                }
-                return slow;
-            }
-        }
-        return null;
-    }
-}
+1/**
+2 * Definition for singly-linked list.
+3 * class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public ListNode detectCycle(ListNode head) {
+14        if (head == null || head.next == null) return null;
+15
+16        ListNode slow = head;
+17        ListNode fast = head;
+18
+19        while (fast!= null && fast.next != null ) {
+20            slow = slow.next;
+21            fast = fast.next.next;
+22
+23            if (slow == fast) {
+24                slow = head;
+25                while (slow != fast) {
+26                    slow = slow.next;
+27                    fast = fast.next;
+28                }
+29                return slow;
+30            }
+31        }
+32        return null;  
+33    }
+34}
