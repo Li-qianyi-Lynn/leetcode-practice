6718d46f9e0df37141f77cae6f0dfb9c207e8a4d
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-04 17:11:52 -0800

    Add solution for IPO

--- LeetCode/IPO/Solution.java
+++ LeetCode/IPO/Solution.java
@@ -3,46 +3,45 @@
 3class Solution {
 4    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
 5        int n = profits.length;
-6        int[][] match = new int[n][2]; // n projects, 2 (profit + capital)
+6        int[][] projects = new int[n][2]; // n projects, 2 (profit + capital)
 7        for (int i = 0; i < n; i++) {
-8            match[i][0] = capital[i];
-9            match[i][1] = profits[i];
+8            projects[i][0] = capital[i];
+9            projects[i][1] = profits[i];
 10
 11        }
-12
-13        // sorted capital
-14        Arrays.sort(match, (a,b)-> a[0] - b[0]);
-15        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> b-a); // profit
-16
-17        int cur = 0;
-18        for (int i = 0; i < k; i++) {
-19            while (cur < n && match[cur][0] <= w) {
-20                maxHeap.add(match[cur][1]);
-21                cur++;
-22            }
-23            if (maxHeap.isEmpty()) {
-24                break;
-25
-26            }
-27
-28            w += maxHeap.poll();
-29
-30        }
-31        return w;
-32        
-33    }
-34}
-35/**
-361. match capital and profit
-372. sorted capital , use less capital, get max profit
-383. k limitations, process max profit: maxHeap
-394. for (i-k) {
-40    projectid < n, capital <= w : add it to the heap,
-41    w +=maxProfitHeap.poll();
+12        // sorted capital
+13        Arrays.sort(projects, (a,b)-> a[0] - b[0]);
+14        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> b-a); // profit
+15
+16        int cur = 0;
+17        for (int i = 0; i < k; i++) {
+18            while (cur < n && projects[cur][0] <= w) {
+19                maxHeap.add(projects[cur][1]);
+20                cur++;
+21            }
+22            if (maxHeap.isEmpty()) {
+23                break;
+24
+25            }
+26
+27            w += maxHeap.poll();
+28
+29        }
+30        return w;
+31        
+32    }
+33}
+34/**
+351. match capital and profit
+362. sorted capital , use less capital, get max profit
+373. k limitations, process max profit: maxHeap
+384. for (i-k) {
+39    projectid < n, capital <= w : add it to the heap,
+40    w +=maxProfitHeap.poll();
+41
 42
-43
-44}
+43}
+44
 45
 46
-47
-48 */
+47 */
