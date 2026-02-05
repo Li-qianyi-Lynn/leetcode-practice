f86e9f22a9e50d45731a1f3e5e4b5cf8d186c7f0
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-04 21:11:14 -0800

    Add solution for Max Points on a Line

--- LeetCode/Max-Points-on-a-Line/Solution.java
+++ LeetCode/Max-Points-on-a-Line/Solution.java
@@ -5,41 +5,33 @@
 5
 6        }
 7        int n = points.length;
-8        int maxP = 0;
-9
-10        for (int i = 0; i < n; i++) {
-11            Map<String, Integer> map = new HashMap<>();
-12
-13            for (int j = i+1; j < n; j++) {
-14                int dy = points[j][1] - points[i][1];
-15                int dx = points[j][0] - points[i][0];
-16                int common = gcd(dx, dy);
-17                dx /= common;
-18                dy /= common;
-19                
-20                String key = dy + "/" + dx;
-21                map.put(key, map.getOrDefault(key, 0) + 1);
-22                maxP = Math.max(maxP, map.get(key));
-23            }   
-24        }
-25        return maxP + 1;
-26        
-27    }
-28
-29    private int gcd(int a, int b) {
-30        while (b!= 0) {
-31            int temp = a % b;
-32            a = b;
-33            b = temp;
-34
-35        }
-36        return a;
-37       
-38    }
-39}
-40/**
-41y = kx
-42k = y/x
-43
-44
-45 */
+8        int res = 0;
+9        for (int i = 0; i < n; i++) {
+10            Map<String, Integer> map = new HashMap<>();
+11            for (int j = i+1; j < n; j++) {
+12                int dy = points[j][1]-points[i][1];
+13                int dx = points[j][0]-points[i][0];
+14
+15                int common = helper(dy, dx);
+16                dy = dy / common;
+17                dx = dx / common;
+18                String k = dy + "/" + dx;
+19                map.put(k, map.getOrDefault(k,0)+1);
+20                res = Math.max(res, map.get(k));
+21
+22            }
+23        }
+24        return res+1;
+25        
+26    }
+27
+28    private int helper(int a,int b) {
+29        while (b != 0) {
+30            int temp = a % b;
+31            a = b;
+32            b = temp;
+33
+34        }
+35        return a;
+36    }
+37}
