106cf78ddc4e55d0b361631567b56b628bafc234
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-04 17:19:56 -0800

    Add solution for Majority Element

--- LeetCode/Majority-Element/Solution.java
+++ LeetCode/Majority-Element/Solution.java
@@ -1,12 +1,22 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> hash;
-        auto size = nums.size() / 2;
-        for (auto i: nums){
-            ++hash[i];
-            if (hash[i] > size) return i; // Here I thought I would exit way before reaching N
-        }
-        throw;
-    }
-};
+1class Solution {
+2    public int majorityElement(int[] nums) {
+3        Map<Integer, Integer> map = new HashMap<>();
+4        for (int num : nums) {
+5            map.put(num, map.getOrDefault(num,0)+1);
+6        }
+7
+8        int target = nums.length / 2;
+9        for (var entry : map.entrySet()) {
+10            if (entry.getValue() > target) {
+11                return entry.getKey();
+12
+13            }
+14
+15        }
+16        return 0;
+17        
+18
+19
+20        
+21    }
+22}
