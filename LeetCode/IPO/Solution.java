cc3044c52f982744f55c381ef2f710cecf6b5c6d
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-04 17:10:48 -0800

    Add solution for IPO

--- LeetCode/IPO/Solution.java
+++ LeetCode/IPO/Solution.java
@@ -3,39 +3,46 @@
 3class Solution {
 4    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
 5        int n = profits.length;
-6        
-7        // 1. 将项目按所需资本从小到大排序，创建一个二维数组或者自定义对象来存储 capital 和 profits 的对应关系
-8        int[][] projects = new int[n][2];
-9        for (int i = 0; i < n; i++) {
-10            projects[i][0] = capital[i];
-11            projects[i][1] = profits[i];
-12        }
-13        
-14        // 按 capital 升序排列
-15        Arrays.sort(projects, (a, b) -> a[0] - b[0]);
+6        int[][] match = new int[n][2]; // n projects, 2 (profit + capital)
+7        for (int i = 0; i < n; i++) {
+8            match[i][0] = capital[i];
+9            match[i][1] = profits[i];
+10
+11        }
+12
+13        // sorted capital
+14        Arrays.sort(match, (a,b)-> a[0] - b[0]);
+15        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> b-a); // profit
 16
-17        // 2. 最大堆：用于存储当前资本可以启动的所有项目的利润（利润大的在前）
-18        PriorityQueue<Integer> maxProfitHeap = new PriorityQueue<>((a, b) -> b - a);
-19
-20        int projectIdx = 0;
-21        
-22        // 3. 进行 k 轮选择
-23        for (int i = 0; i < k; i++) {
-24            // 将所有当前资本 w 能够负担的项目放入最大堆
-25            while (projectIdx < n && projects[projectIdx][0] <= w) {
-26                maxProfitHeap.add(projects[projectIdx][1]);
-27                projectIdx++;
-28            }
+17        int cur = 0;
+18        for (int i = 0; i < k; i++) {
+19            while (cur < n && match[cur][0] <= w) {
+20                maxHeap.add(match[cur][1]);
+21                cur++;
+22            }
+23            if (maxHeap.isEmpty()) {
+24                break;
+25
+26            }
+27
+28            w += maxHeap.poll();
 29
-30            // 如果最大堆为空，说明剩下的项目都买不起，直接跳出
-31            if (maxProfitHeap.isEmpty()) {
-32                break;
-33            }
-34
-35            // 贪心选择：选利润最大的项目，更新当前资本
-36            w += maxProfitHeap.poll();
-37        }
-38
-39        return w;
-40    }
-41}
+30        }
+31        return w;
+32        
+33    }
+34}
+35/**
+361. match capital and profit
+372. sorted capital , use less capital, get max profit
+383. k limitations, process max profit: maxHeap
+394. for (i-k) {
+40    projectid < n, capital <= w : add it to the heap,
+41    w +=maxProfitHeap.poll();
+42
+43
+44}
+45
+46
+47
+48 */
