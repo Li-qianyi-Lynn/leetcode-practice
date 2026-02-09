9ec090379f5d648ee699b03405c37971fd0dfb48
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-09 01:31:10 -0800

    Add solution for Binary Tree Right Side View

--- LeetCode/Binary-Tree-Right-Side-View/Solution.java
+++ LeetCode/Binary-Tree-Right-Side-View/Solution.java
@@ -1,51 +1,52 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        if (root == null) {
-            return res;
-        }
-        bfs(root,res);
-        return res;
-        
-    }
-    private void bfs(TreeNode root, List<Integer> res) {
-        Deque<TreeNode> deque = new LinkedList<>();
-        deque.offerLast(root);
-        while (!deque.isEmpty()) {
-            int size = deque.size();
-            for (int i = 1; i <= size; i++) {
-                TreeNode poll = deque.pollFirst();
-                if (poll.left != null) {
-                    deque.offerLast(poll.left);
-
-                }
-                if (poll.right != null) {
-                    deque.offerLast(poll.right);
-
-                }
-                if (i == size) {
-                    res.add(poll.val);
-
-                }
-
-            }
-
-        }
-
-    }
-}
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public List<Integer> rightSideView(TreeNode root) {
+18        List<Integer> res = new ArrayList<>();
+19        Deque<TreeNode> deque = new LinkedList<>();
+20
+21        if (root == null) {
+22            return res;
+23
+24        }
+25        deque.addLast(root);
+26
+27        while (!deque.isEmpty()) {
+28            int levelNodes = deque.size();
+29            for (int i = 1;i <= levelNodes;i++) {
+30                TreeNode poll = deque.pollFirst();
+31                if (poll.left != null) {
+32                    deque.addLast(poll.left);
+33
+34                }
+35                if (poll.right != null) {
+36                    deque.addLast(poll.right);
+37
+38                }
+39
+40                if (i == levelNodes) {
+41                    res.add(poll.val);
+42
+43                }
+44
+45            }   
+46
+47        }
+48        return res;
+49
+50        
+51    }
+52}
