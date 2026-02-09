295b0206aadc0a915b8f1827ef0b53a3722c00ce
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-09 00:19:30 -0800

    Add solution for Merge Sorted Array

--- LeetCode/Merge-Sorted-Array/Solution.java
+++ LeetCode/Merge-Sorted-Array/Solution.java
@@ -1,12 +1,50 @@
-public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m - 1, p2 = n - 1;
-        for(int i = nums1.length - 1; i >= 0; i--) {
-            if(p1 >= 0 && p2 >= 0) {
-                nums1[i] = nums1[p1] > nums2[p2] ? nums1[p1--] : nums2[p2--];
-            } else if (p1 >= 0) {
-                nums1[i] = nums1[p1--];
-            } else {
-                nums1[i] = nums2[p2--];
-            }
-        }
-    }
+1class Solution {
+2    public void merge(int[] nums1, int m, int[] nums2, int n) {
+3        //pruning
+4        if (m == 0) {
+5            nums1[0] = nums2[0];
+6
+7        }
+8        if (n == 0) {
+9            return;
+10
+11        }
+12        int arr1 = m-1;
+13        int arr2 = n-1;
+14        int valid = m+n-1; 
+15        while (arr1>= 0 && arr2 >= 0 ) {
+16            if (nums1[arr1] > nums2[arr2]) {
+17                nums1[valid] = nums1[arr1];
+18                
+19                arr1--;
+20
+21            } else {
+22                nums1[valid] = nums2[arr2];
+23                arr2--;
+24
+25            }
+26            valid--;
+27        }
+28        // nums1 已经排好了所以不用换，要换nums2
+29        while (arr2 >= 0) {
+30            nums1[valid] = nums2[arr2];
+31            arr2--;
+32            valid--;
+33
+34        }    
+35    }
+36}
+37
+38/**
+39nums1 = [1,2, 3, 0, 0 ,0] m
+40 
+41             m-1      m+n-1
+42nums2 = [2,5,6] n
+43             n-1
+44
+45//edge case
+46m == 0  nums1 = nums2 ; == 1, return 
+47
+48
+49
+50 */
