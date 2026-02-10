ab149b1af0ae30431f0574163321cb1d2b2b73a0
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-10 15:01:27 -0800

    Add solution for Best Time to Buy and Sell Stock

--- LeetCode/Best-Time-to-Buy-and-Sell-Stock/Solution.java
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock/Solution.java
@@ -1,46 +1,30 @@
 1class Solution {
 2    public int maxProfit(int[] prices) {
-3        // edge case
-4        if (prices.length == 1) {
-5            return 0;
-6        }
-7
-8        int profit = Integer.MIN_VALUE;
-9        int buy = 0;
-10        int sell = 1;
-11        while (sell < prices.length) { // diff day
-12            if (prices[buy] >= prices[sell]) {
-13                buy = sell;
+3        if (prices.length == 1) {
+4            return 0;
+5        }
+6        int profit = 0;
+7        int buy = prices[0];
+8        for (int i = 1; i < prices.length; i++) {
+9            if (prices[i] > buy) {
+10                profit = Math.max(profit, prices[i]- buy);
+11            } else {
+12                buy = prices[i];
+13            }
 14
-15            } else {
-16                profit = Math.max(profit, prices[sell]-prices[buy]);
-17            }
-18            sell++;
+15        }
+16        return profit;    
+17    }
+18}
 19
-20
-21        }
-22        return profit == Integer.MIN_VALUE ? 0 : profit;
-23        
-24        
-25    }
-26}
-27
-28/**
+20/**
+21profit = 0;
+22cur = num[0];
+23
+24nums[i] > cur
+25update profit max 4
+26
+27else 
+28cur = nums[i]
 29
-30input: int[] prices 
-31output: int  maximum profit  or 0
-32
-33two pointer
-34[7,6,4,3,1]
-35l
-36          r
-37if prices[l] >= prices[r];
-38move left pointer
-39else update profit, move right pointer
-40
-41[2,1,4]
-42l
-43     r
-44
-452
-46 */
+30 */
