082f6bc10090cb415cdfbee9f47bf419b163a972
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-09 17:43:13 -0800

    Add solution for Remove Duplicates from Sorted Array II

--- LeetCode/Remove-Duplicates-from-Sorted-Array-II/Solution.java
+++ LeetCode/Remove-Duplicates-from-Sorted-Array-II/Solution.java
@@ -1,42 +1,40 @@
 1class Solution {
 2    public int removeDuplicates(int[] nums) {
-3        if (nums.length == 1) {
-4            return 1;
-5
-6        }
-7        int valid = 1;
-8        int times = 1;
-9        for (int i = 1; i < nums.length; i++) {
-10            if (nums[i] == nums[i-1]) {
-11                if (times <= 1) {
-12                    times ++;
-13                    // nums[valid] = nums[i];
-14                    // valid++;
+3        if (nums.length <= 2) {
+4            return nums.length;
+5        }
+6        int valid = 1;
+7        int times = 1;
+8        for (int i = 1; i < nums.length; i++) {
+9            if (nums[i] == nums[i-1]) {
+10                times++;
+11
+12            } else {
+13                times = 1;
+14            }
 15
-16                } else {
-17                    continue;
-18                }
-19            
-20            } else {
-21                times = 1;
-22               
-23            }
-24
-25            nums[valid] = nums[i];
-26            valid++;
-27        }
-28        return valid;
-29        
-30    }
-31}
-32/**
-33two pointers
-34
-35valid index
-36i regular pointers
-37
-38nums[i] == nums[i-1], times++
-39check times     
-40
-41
-42 */
+16            if (times <= 2) {
+17                nums[valid] = nums[i];
+18                valid ++;
+19            }
+20        }
+21        return valid;    
+22    }
+23}
+24/**
+25K: the num of nums[i] appeared one or two times
+26
+27valid
+28for loop: 1- len-1
+29times = 1
+30nums[i]== nums[i-1]
+31times++;
+32
+33else:
+34times = 1
+35
+36if times <= 2
+37nums[valid] = nums[i]
+38
+39return valid
+40 */
