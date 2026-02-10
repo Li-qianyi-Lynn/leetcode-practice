b9ca13a5ef8d53e878aef7f07afb6a213ae217e5
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-09 21:49:12 -0800

    Add solution for Remove Element

--- LeetCode/Remove-Element/Solution.java
+++ LeetCode/Remove-Element/Solution.java
@@ -1,17 +1,20 @@
 1class Solution {
 2    public int removeElement(int[] nums, int val) {
-3        int slow = 0;
-4        for (int fast = 0; fast < nums.length; fast++) {
-5            if (nums[fast] != val) {
-6                nums[slow] = nums[fast]; 
-7                slow++;
-8
-9            }
-10
-11        }
-12        return slow;
-13    
-14
-15        
-16    }
-17}
+3        if (nums.length == 0) {
+4            return 0;
+5        }
+6        int r = nums.length-1;
+7        int l = 0;
+8        while (l <= r) {
+9            if (nums[l] == val) {
+10                nums[l] = nums[r];
+11                r--;
+12
+13            } else {
+14                l++;
+15            }
+16        }
+17        return l;
+18        
+19    }
+20}
