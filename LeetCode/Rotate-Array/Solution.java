ac3de950f6adec63e004d06f59fd80575d25ac9b
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-10 12:24:51 -0800

    Add solution for Rotate Array

--- LeetCode/Rotate-Array/Solution.java
+++ LeetCode/Rotate-Array/Solution.java
@@ -2,47 +2,38 @@
 2
 3
 4    public void rotate(int[] nums, int k) {
-5        int len = nums.length;
-6        k = k % len;  // 处理 k > len 的情况
+5        int[] copy = nums.clone();
+6        int len = nums.length;
 7        
-8        if (k == 0) return;  // 不需要旋转
-9        
-10        int[] copy = nums.clone();
-11        for (int i = 0; i < len; i++) {
-12            nums[(i + k) % len] = copy[i];
-13        }
-14    //     int[] copy = nums.clone();
-15    //     int len = nums.length;
-16        
-17    //     if (len == 1 || len == k) {
-18    //         return;
-19    //     }
+8        if (len == 1 || len == k) {
+9            return;
+10        }
+11
+12        if (len > k) {
+13            changeInt(k, nums, copy);
+14            
+15
+16        } else if (len < k) {
+17            int times = k % len; 
+18            changeInt(times, nums, copy);
+19
 20
-21    //     if (len > k) {
-22    //         changeInt(k, nums, copy);
-23            
-24
-25    //     } else if (len < k) {
-26    //         int times = k % len; 
-27    //         changeInt(times, nums, copy);
-28
-29
-30    //     }
-31        
-32        
-33    // }
-34
-35    // private void changeInt(int times, int[] nums, int[] copy) {
-36    //     for (int i = 0; i < nums.length; i++) {
-37    //             if ( i < times) {
-38    //                 nums[i] = copy[i+ nums.length - times];
-39
-40    //             } else {
-41    //                 nums[i] = copy[i - times];
-42
-43    //             }   
-44
-45    //         }
-46
-47    }
-48}
+21        }
+22        
+23        
+24    }
+25
+26    private void changeInt(int times, int[] nums, int[] copy) {
+27        for (int i = 0; i < nums.length; i++) {
+28                if ( i < times) {
+29                    nums[i] = copy[i+ nums.length - times];
+30
+31                } else {
+32                    nums[i] = copy[i - times];
+33
+34                }   
+35
+36            }
+37
+38    }
+39}
