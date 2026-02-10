98b31949328cfab0d42cba4249058954e4cd7ff1
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-09 17:19:27 -0800

    Add solution for Merge Sorted Array

--- LeetCode/Merge-Sorted-Array/Solution.java
+++ LeetCode/Merge-Sorted-Array/Solution.java
@@ -1,50 +1,44 @@
 1class Solution {
 2    public void merge(int[] nums1, int m, int[] nums2, int n) {
-3        //pruning
-4        if (m == 0) {
-5            nums1[0] = nums2[0];
-6
-7        }
-8        if (n == 0) {
-9            return;
-10
+3        // edge case: m == 1 n ==0
+4        if (m == 1 && n== 0) {
+5            return;
+6        }
+7
+8        if (m == 0 && n == 1) {
+9            nums1[0] = nums2[0];
+10            return;
 11        }
-12        int arr1 = m-1;
-13        int arr2 = n-1;
-14        int valid = m+n-1; 
-15        while (arr1>= 0 && arr2 >= 0 ) {
-16            if (nums1[arr1] > nums2[arr2]) {
-17                nums1[valid] = nums1[arr1];
-18                
-19                arr1--;
-20
+12        // two pointers
+13        int i = m-1;
+14        int j = n-1;
+15        int k = m+n-1;
+16
+17        while (i >= 0 && j >= 0) {
+18            if (nums1[i] >= nums2[j]) {
+19                nums1[k] = nums1[i];
+20                i--;
 21            } else {
-22                nums1[valid] = nums2[arr2];
-23                arr2--;
+22                nums1[k] = nums2[j];
+23                j--;
 24
 25            }
-26            valid--;
+26            k--;
 27        }
-28        // nums1 已经排好了所以不用换，要换nums2
-29        while (arr2 >= 0) {
-30            nums1[valid] = nums2[arr2];
-31            arr2--;
-32            valid--;
-33
-34        }    
-35    }
-36}
-37
-38/**
-39nums1 = [1,2, 3, 0, 0 ,0] m
-40 
-41             m-1      m+n-1
-42nums2 = [2,5,6] n
-43             n-1
-44
-45//edge case
-46m == 0  nums1 = nums2 ; == 1, return 
-47
-48
-49
-50 */
+28
+29        while (j >= 0) {
+30            nums1[k] = nums2[j];
+31            j--;
+32            k--;
+33           
+34        }
+35
+36        
+37    }
+38}
+39/**
+40
+41
+42
+43
+44 */
