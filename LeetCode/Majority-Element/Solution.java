4f1cdbf792ca7ac3d66f9a71249b235059f06f1f
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-10 11:54:28 -0800

    Add solution for Majority Element

--- LeetCode/Majority-Element/Solution.java
+++ LeetCode/Majority-Element/Solution.java
@@ -1,22 +1,24 @@
 1class Solution {
 2    public int majorityElement(int[] nums) {
-3        Map<Integer, Integer> map = new HashMap<>();
-4        for (int num : nums) {
-5            map.put(num, map.getOrDefault(num,0)+1);
-6        }
-7
-8        int target = nums.length / 2;
-9        for (var entry : map.entrySet()) {
-10            if (entry.getValue() > target) {
-11                return entry.getKey();
-12
-13            }
-14
-15        }
-16        return 0;
-17        
-18
-19
-20        
-21    }
-22}
+3        int canidate = nums[0];
+4        int count = 1; // more than n/2
+5
+6        for (int i = 1; i < nums.length; i++) {
+7            if (count == 0) {  
+8                canidate = nums[i];
+9                count = 1;
+10
+11            } else if (nums[i] == canidate) {
+12                count++;
+13
+14            } else if (nums[i] != canidate) {
+15                count--;
+16            }
+17
+18        }
+19        return canidate;
+20
+21
+22        
+23    }
+24}
