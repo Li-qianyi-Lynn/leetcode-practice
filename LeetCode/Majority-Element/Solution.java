6d466b327846ca884fb9a4832696e5cd667f6a96
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-10 11:56:10 -0800

    Add solution for Majority Element

--- LeetCode/Majority-Element/Solution.java
+++ LeetCode/Majority-Element/Solution.java
@@ -1,24 +1,34 @@
 1class Solution {
 2    public int majorityElement(int[] nums) {
-3        int canidate = nums[0];
-4        int count = 1; // more than n/2
+3        if (nums.length == 1) {
+4            return nums[0];
 5
-6        for (int i = 1; i < nums.length; i++) {
-7            if (count == 0) {  
-8                canidate = nums[i];
-9                count = 1;
-10
-11            } else if (nums[i] == canidate) {
-12                count++;
-13
-14            } else if (nums[i] != canidate) {
-15                count--;
-16            }
-17
-18        }
-19        return canidate;
-20
-21
-22        
-23    }
-24}
+6        }
+7        int count = 1;
+8        int res = nums[0];
+9        for (int i = 1; i < nums.length; i++) {
+10            if (count == 0) {
+11                res = nums[i];
+12                count = 1;
+13            } else if (nums[i] == res) {
+14                count++;
+15
+16            } else {
+17                count --;
+18            }
+19
+20            if (count > nums.length / 2) {
+21                break;
+22
+23            }
+24        }
+25        return res;
+26
+27        
+28    }
+29}
+30/**
+31
+32
+33
+34 */
