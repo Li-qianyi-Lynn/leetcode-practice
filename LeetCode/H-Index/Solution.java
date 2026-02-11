704d2edf88deadd03fefb4255bac1ee88de6419e
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-10 20:28:59 -0800

    Add solution for H-Index

--- LeetCode/H-Index/Solution.java
+++ LeetCode/H-Index/Solution.java
@@ -1,53 +1,43 @@
 1class Solution {
 2    public int hIndex(int[] citations) {
-3        int l = 0;
-4        int r = citations.length;
-5        int res= 0;
-6
-7        while (l <= r) {
-8            int mid = l + (r-l) / 2;
-9
+3        
+4
+5        int l = 0;
+6        int r = citations.length;
+7        int res = 0;
+8        while (l <= r) {
+9            int mid = l + (r-l)/2;
 10            int count = 0;
-11            for (int c : citations) {
-12                if (c >= mid) {
+11            for (int i = 0; i < citations.length; i++) {
+12                if (citations[i] >= mid) {
 13                    count++;
-14
-15                }
+14                }
+15
 16            }
 17            if (count >= mid) {
-18                res = mid;
-19                l = mid +1;   
+18                res = Math.max(res,mid);
+19                l = mid+1;
 20
 21            } else {
 22                r = mid-1;
 23            }
 24        }
-25        return res;     
-26    }
-27}
-28/**
-29input int[] 
-30index: ith paper ;
-31citation[i]:  the number of citations a researcher received for their ith paper (likes for the paper i)
-32
-33
-34output: int h: 
-351) must have h paper
-362) each paper has citations larger than k
-37
-38 0<= h <= citations.length
-39 binary search
-40
-41citations[m]
-420 1 2 3 4 5 6 
-43      |
-44 check if count of citations[i] >= 3 find three update res ,l = mid+1, 
-45 return res
-46
-47
-48 
-49
-50
-51
-52
-53 */
+25        return res;
+26        
+27    }
+28}
+29/**
+30
+31h: max
+321. h: 0-len
+332. each paper has been cited at least h times
+34
+35binary search
+360. 5
+37check if nums[i] >= mid
+38if yes , count ++
+39
+40check if count >= mid
+41if yes, answer = max mid, l = mid +1
+42if no, r = mid -1
+43 */
