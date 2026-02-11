e236fcff01bffff4aabf921bf1cb960f2570ff18
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-10 20:05:24 -0800

    Add solution for Jump Game

--- LeetCode/Jump-Game/Solution.java
+++ LeetCode/Jump-Game/Solution.java
@@ -1,14 +1,21 @@
 1class Solution {
 2    public boolean canJump(int[] nums) {
-3        int last = nums.length-1;
-4        for (int i = last; i >=0; i--) {
+3        int maxReach = 0; // index
+4        int target = nums.length - 1;
 5
-6            if (i+ nums[i] >= last) {
-7                last = i;
-8
+6        for (int i = 0; i < nums.length; i++) {
+7            if (i > maxReach) {
+8                return false;
 9            }
-10        }
-11        return last == 0;
-12        
-13    }
-14}
+10
+11            maxReach = Math.max(maxReach,i+ nums[i]);
+12            if (maxReach >= target) {
+13                return true;
+14
+15            }
+16
+17        }
+18        return maxReach >= target;
+19
+20    }
+21}
