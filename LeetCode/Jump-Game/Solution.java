4ca5abac5bfe2bce6ba9e3347063bb6ef0e127f3
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-10 20:04:07 -0800

    Add solution for Jump Game

--- LeetCode/Jump-Game/Solution.java
+++ LeetCode/Jump-Game/Solution.java
@@ -4,14 +4,20 @@
 4        int target = nums.length - 1;
 5
 6        for (int i = 0; i < nums.length; i++) {
-7            if (i > maxReach) {
-8                return false;
-9            }
-10
-11            maxReach = Math.max(maxReach,i+ nums[i]);
-12
-13        }
-14        return maxReach >= nums[target];
-15
-16    }
-17}
+7            // jump to zero
+8            if (i > maxReach) {
+9                return false;
+10            }
+11
+12            
+13            maxReach = Math.max(maxReach, i + nums[i]);
+14
+15            // pruning: 如果最远距离已经覆盖了最后一个下标，直接返回 true
+16            if (maxReach >= target) {
+17                return true;
+18            }
+19        }
+20
+21        return maxReach >= target;
+22    }
+23}
