f64b0868ed3d37e6e2d5400ee610d4705a145442
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-10 20:02:07 -0800

    Add solution for Jump Game

--- LeetCode/Jump-Game/Solution.java
+++ LeetCode/Jump-Game/Solution.java
@@ -1,4 +1,23 @@
-// Old
-for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++)
-// New
-for (int nextPosition = furthestJump; nextPosition > position; nextPosition--)
+1class Solution {
+2    public boolean canJump(int[] nums) {
+3        int maxReach = 0;
+4        int target = nums.length - 1;
+5
+6        for (int i = 0; i < nums.length; i++) {
+7            // jump to zero
+8            if (i > maxReach) {
+9                return false;
+10            }
+11
+12            
+13            maxReach = Math.max(maxReach, i + nums[i]);
+14
+15            // pruning: 如果最远距离已经覆盖了最后一个下标，直接返回 true
+16            if (maxReach >= target) {
+17                return true;
+18            }
+19        }
+20
+21        return maxReach >= target;
+22    }
+23}
