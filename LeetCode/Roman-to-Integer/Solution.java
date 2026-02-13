cd8a28ee64d86e4e0388c63535ca9fc721a67822
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-12 16:23:26 -0800

    Add solution for Roman to Integer

--- LeetCode/Roman-to-Integer/Solution.java
+++ LeetCode/Roman-to-Integer/Solution.java
@@ -1,25 +1,32 @@
 1class Solution {
 2    public int romanToInt(String s) {
-3        int num = 0;
-4        Map<Character, Integer> map = new HashMap<>();
-5        map.put('I',1);
-6        map.put('V',5);
-7        map.put('X',10);        
-8        map.put('L',50);
-9        map.put('C',100);
-10        map.put('D',500);
-11        map.put('M',1000);
-12
-13        for (int i = 0; i < s.length(); i++) {
-14  
+3        Map<Character, Integer> map = new HashMap<>();
+4        int res = 0;
+5         
+6        map.put('I',1);
+7        map.put('V',5);
+8        map.put('X',10);
+9        map.put('L',50);
+10        map.put('C',100);
+11        map.put('D',500);
+12        map.put('M',1000);
+13
+14        for (int i = 0; i < s.length(); i++) {
 15            if (i < s.length()-1 && map.get(s.charAt(i)) < map.get(s.charAt(i+1))) {
-16                num -= map.get(s.charAt(i));
-17            } else {
-18                num += map.get(s.charAt(i));
-19            }
-20
-21        }
-22        return num;
-23        
-24    }
-25}
+16                res -= map.get(s.charAt(i));
+17
+18            } else {
+19                res += map.get(s.charAt(i));
+20            }
+21
+22        }
+23        return res;
+24
+25    }
+26}
+27/**
+28
+29map.get(charAt(i) < charAt(i+1)) 
+30res - i
+31
+32 */
