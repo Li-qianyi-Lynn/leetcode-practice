9d75e698711eed4f48daa1c098b37fa173ef62c7
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-18 12:14:27 -0800

    Add solution for Basic Calculator

--- LeetCode/Basic-Calculator/Solution.java
+++ LeetCode/Basic-Calculator/Solution.java
@@ -1,34 +1,75 @@
-class Solution {
-    int i;
-
-    public int calculate(String s) {
-
-        int operand = 0;
-        int result = 0; // For the on-going result
-        int nextOperandSign = 1;  // 1 means positive, -1 means negative
-
-        while(i < s.length()) {
-
-            char ch = s.charAt(i++);
-
-            if (ch == ' ' || Character.isDigit(ch)) {
-                operand = (ch == ' ') ? operand : 10 * operand + (ch - '0');
-                
-            } else if (ch == '(') {
-                operand = calculate(s);
-                
-            } else if (ch == ')') {
-                break; // Sub-expression we were evaluating has ended. Exit now...
-                
-            } else { 
-                // If we're here, we just read the operator for the next operand/expression.
-                // Evaluate the existing expression already read, reset operand, set sign for next incoming operand.
-                result += nextOperandSign * operand;
-                nextOperandSign = ch == '+' ? 1 : -1;
-                operand = 0;
-            }
-        }
-
-        return result + (nextOperandSign * operand);
-    }
-}
+1/**
+2(23+5) + 6
+3
+4 */
+5class Solution {
+6    public int calculate(String s) {
+7        int res = 0;
+8        Deque<Integer> dq = new ArrayDeque<>();
+9        int sign = 1;
+10        int len = s.length();
+11
+12        for (int i = 0; i < len; i++) {
+13            char c = s.charAt(i);
+14
+15            if (c == '(') {
+16                dq.offerLast(res);
+17                dq.offerLast(sign);
+18
+19                res = 0;
+20                sign = 1;
+21            } else if (c == ')') {
+22                int preSign = dq.pollLast();
+23                int preSum = dq.pollLast();
+24                
+25                res = preSign * res + preSum;
+26            } else if (c == '+') {
+27                sign = 1;
+28
+29            } else if (c == '-') {
+30                sign = -1;
+31
+32            } else if (Character.isDigit(c)) {
+33                int num = 0;
+34                while (i < len && Character.isDigit(s.charAt(i))) {
+35                    num = num * 10 + s.charAt(i)-'0';
+36                    i++;
+37                }
+38                res += num * sign;
+39                i--;
+40
+41            } else {
+42                continue;
+43            }
+44
+45        }
+46        return res;
+47        
+48    }
+49}
+50
+51/**
+52res 
+53sign +/- 1/-1
+54deque 
+55
+56(： 
+57把之前算的res 存进去
+58把sign 存进去
+59重置res and sign
+60
+61）：
+62获得res*sign poll
+63获得res += 上一个存进去的res
+64
+65+/-：
+66update sign
+67
+68num：
+69res = sign* num
+70multi digit:
+71int num = 0;
+72num * 10 + c-'0'
+73
+74
+75 */
