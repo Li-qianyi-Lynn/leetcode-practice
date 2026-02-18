12674955cee51bd96d5b5d72be78d8c0edce4785
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-18 12:14:39 -0800

    Add solution for Basic Calculator

--- LeetCode/Basic-Calculator/Solution.java
+++ LeetCode/Basic-Calculator/Solution.java
@@ -3,15 +3,15 @@
 3class Solution {
 4    public int calculate(String s) {
 5        // 使用 Deque 作为栈
-6        Deque<Integer> dq = new ArrayDeque<>();
+6        Deque<Integer> stack = new ArrayDeque<>();
 7        int res = 0;    // 当前层级的累加结果
 8        int sign = 1;   // 当前的符号：1 表示 +，-1 表示 -
 9        int n = s.length();
 10
 11        for (int i = 0; i < n; i++) {
-12            char c = s.charAt(i);
+12            char ch = s.charAt(i);
 13
-14            if (Character.isDigit(c)) {
+14            if (Character.isDigit(ch)) {
 15                // 1. 处理多位数字
 16                int num = 0;
 17                while (i < n && Character.isDigit(s.charAt(i))) {
@@ -22,27 +22,27 @@
 22                res += sign * num;
 23                i--; // 补偿 for 循环的 i++
 24            } 
-25            else if (c == '+') {
+25            else if (ch == '+') {
 26                sign = 1;
 27            } 
-28            else if (c == '-') {
+28            else if (ch == '-') {
 29                sign = -1;
 30            } 
-31            else if (c == '(') {
+31            else if (ch == '(') {
 32                // 2. 遇到左括号：保存现场
 33                // 先存当前算好的结果，再存括号前的符号
-34                dq.offerLast(res);
-35                dq.offerLast(sign);
+34                stack.push(res);
+35                stack.push(sign);
 36                // 重置现场，准备算括号里的新表达式
 37                res = 0;
 38                sign = 1;
 39            } 
-40            else if (c == ')') {
+40            else if (ch == ')') {
 41                // 3. 遇到右括号：恢复现场
 42                // 括号里的算完了，先乘上括号外的符号
-43                res *= dq.pollLast(); 
+43                res *= stack.pop(); 
 44                // 再加上括号前已经算好的结果
-45                res += dq.pollLast();
+45                res += stack.pop();
 46            }
 47            // 其余字符如空格直接忽略
 48        }
