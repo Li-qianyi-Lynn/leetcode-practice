db8764ce06c5a3de50c578100c507c1c966c2b5e
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-18 22:04:11 -0800

    Add solution for Basic Calculator

--- LeetCode/Basic-Calculator/Solution.java
+++ LeetCode/Basic-Calculator/Solution.java
@@ -1,51 +1,92 @@
-1
-2
-3class Solution {
-4    public int calculate(String s) {
-5        // 使用 Deque 作为栈
-6        Deque<Integer> stack = new ArrayDeque<>();
-7        int res = 0;    // 当前层级的累加结果
-8        int sign = 1;   // 当前的符号：1 表示 +，-1 表示 -
-9        int n = s.length();
+1class Solution {
+2    public int calculate(String s) {
+3        Deque<Integer> dq = new ArrayDeque<>();
+4        int res = 0; 
+5        int n = s.length();
+6        int mark = 1;
+7
+8        for (int i = 0; i < n; i++) {
+9            char c = s.charAt(i);
 10
-11        for (int i = 0; i < n; i++) {
-12            char ch = s.charAt(i);
-13
-14            if (Character.isDigit(ch)) {
-15                // 1. 处理多位数字
-16                int num = 0;
-17                while (i < n && Character.isDigit(s.charAt(i))) {
-18                    num = num * 10 + (s.charAt(i) - '0');
-19                    i++;
-20                }
-21                // 算出这个数后，立刻根据符号加到结果里
-22                res += sign * num;
-23                i--; // 补偿 for 循环的 i++
-24            } 
-25            else if (ch == '+') {
-26                sign = 1;
-27            } 
-28            else if (ch == '-') {
-29                sign = -1;
-30            } 
-31            else if (ch == '(') {
-32                // 2. 遇到左括号：保存现场
-33                // 先存当前算好的结果，再存括号前的符号
-34                stack.push(res);
-35                stack.push(sign);
-36                // 重置现场，准备算括号里的新表达式
-37                res = 0;
-38                sign = 1;
-39            } 
-40            else if (ch == ')') {
-41                // 3. 遇到右括号：恢复现场
-42                // 括号里的算完了，先乘上括号外的符号
-43                res *= stack.pop(); 
-44                // 再加上括号前已经算好的结果
-45                res += stack.pop();
-46            }
-47            // 其余字符如空格直接忽略
-48        }
-49        return res;
-50    }
-51}
+11            if (c == '(') {
+12                dq.offerLast(res);
+13                dq.offerLast(mark);
+14
+15                res = 0;
+16                mark = 1;
+17            } else if (c ==')') {
+18                int preMark = dq.pollLast();
+19                int preSum = dq.pollLast();
+20
+21                res = preMark * res + preSum;
+22            } else if (c == '+') {
+23                mark = 1;
+24
+25            } else if (c == '-') {
+26                mark = -1;
+27
+28            } else if (Character.isDigit(c)) {
+29                int num = 0;
+30                while (i < n && Character.isDigit(s.charAt(i))) {
+31                    num = num * 10 + s.charAt(i) - '0';
+32                    i++;
+33                }
+34                res += num * mark;
+35                i--;
+36                
+37            } else {
+38                continue;
+39            }
+40        }
+41        return res;
+42        
+43    }
+44}
+45/**
+46input: String 去做一下识别, 遇到 empty skip
+47output: int 
+48
+49iterate s, get the char
+50
+511. 知道 +/-
+522. 需要先放下目前在算的值，先计算括号里的sum
+53
+54stack:
+55(: 1. 把之前算的sum 保存下来，还要把mark 保存下来 (+/-)
+562. 开始这个括号里的计算
+57
+58cur: 0 
+59
+60）：
+611.需要拿到之前存进去的premark, premark * cur -》现在这一层（）的结果
+622. 拿到之前存的sum
+632. cur = 这一层的结果和之前存的sum 加起来 
+64
+65+:
+66mark = 1;
+67
+68
+69-:
+70mark = -1;
+71
+72digit:
+730-9 可能会重复出现: 11+1 111+1
+74
+75num = 0
+76c= charAt(i)
+77while (i < length of string && charAt(i) is digit) {
+78    num += num * 10 + digit of charAt(i)
+79    i++
+80}
+81i--
+82
+83
+84else:
+85continue
+86
+87
+88
+89
+90
+91
+92 */
