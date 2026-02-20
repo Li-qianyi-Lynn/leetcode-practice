26640573aeedf30dd62702acf8da7180370a3d79
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-19 20:59:57 -0800

    Add solution for Find the Index of the First Occurrence in a String

--- LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.java
+++ LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.java
@@ -1,10 +1,20 @@
-class Solution {
-    public int strStr(String haystack, String needle) {
-        for(int i = 0, j = needle.length(); j<=haystack.length(); i++,j++){
-            if(haystack.substring(i,j).equals(needle)){
-                return i;
-            }
-        }
-        return -1;
-    }
-}
+1class Solution {
+2    public int strStr(String haystack, String needle) {
+3        int len1 = haystack.length();
+4        int len2 = needle.length();
+5        if (len1 == 0 || len2 == 0) {
+6            return -1;
+7
+8        }
+9        for (int i = 0, j = len2; j <= len1; i++, j++) {
+10            if (haystack.substring(i,j).equals(needle)) {
+11                return i;
+12
+13            }
+14
+15        }
+16        return -1;
+17
+18        
+19    }
+20}
