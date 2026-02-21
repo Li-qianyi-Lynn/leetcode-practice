5c8af193e6d714d2ce0a825b21957cb9f469e6dc
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-20 17:03:04 -0800

    Add solution for Valid Palindrome

--- LeetCode/Valid-Palindrome/Solution.java
+++ LeetCode/Valid-Palindrome/Solution.java
@@ -2,44 +2,47 @@
 2    public boolean isPalindrome(String s) {
 3        if (s.length() == 1) {
 4            return true;
-5        
+5
 6        }
-7        
-8
+7
+8        int l = 0;
 9        int r = s.length()-1;
-10        int l = 0;
-11
-12        while (l < r) {
-13            if (l < r && alphaNum(s.charAt(l)) == false ) {
-14                l++;
-15                continue;
-16                }
-17            if (l < r && alphaNum(s.charAt(r)) == false ) {
-18                r--;
-19                continue;
-20                }
-21
-22            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
-23                return false;
-24
-25            }
-26
-27            l++;
-28            r--;
-29
-30        }
-31        return true;
-32       
-33        
-34    }
+10
+11        while (l < r) {
+12            if (l < r && alpha(s.charAt(l)) == false) {
+13                l++;
+14                continue;
+15            } else if (l < r && alpha(s.charAt(r)) == false) {
+16                r--;
+17                continue;
+18
+19            }
+20
+21            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+22                return false;
+23
+24            }
+25            l++;
+26            r--;
+27          
+28        }
+29        return true;
+30        
+31    }
+32    private boolean alpha(char c) {
+33        if (c >= '0' && c <= '9') {
+34            return true;
 35
-36    private boolean alphaNum(char c) {
-37        if (c >= '0' && c <= '9') { return true;}
-38        if (c >= 'A' && c <= 'Z') { return true;}
-39        if (c >= 'a' && c <= 'z') { return true;}
-40
-41        return false;
-42
-43        
-44    }
-45}
+36        }
+37        if (c >= 'a' && c <= 'z') {
+38            return true;
+39
+40        }
+41        if (c >= 'A' && c <= 'Z') {
+42            return true;
+43
+44        }
+45        return false;
+46
+47    }
+48}
