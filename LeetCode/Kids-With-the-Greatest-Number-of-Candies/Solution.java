0f8bcc9603d001770bfa7e644a1e61051cb75e8d
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-21 00:34:21 -0800

    Add solution for Kids With the Greatest Number of Candies

--- LeetCode/Kids-With-the-Greatest-Number-of-Candies/Solution.java
+++ LeetCode/Kids-With-the-Greatest-Number-of-Candies/Solution.java
@@ -1,21 +1,2 @@
-1class Solution {
-2    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-3        int maxCandies = 1;
-4        for (int candy : candies) {
-5            maxCandies = Math.max(candy, maxCandies);
-6        }
-7
-8        List<Boolean> res = new ArrayList<>();
-9        for (int candy : candies) {
-10            if (candy + extraCandies >=  maxCandies) {
-11                res.add(true);
-12
-13            } else {
-14                res.add(false);
-15            }
-16
-17        }
-18        return res;
-19        
-20    }
-21}
+max_value = max(candies) - extraCandies
+return (c >= max_value for c in candies)
