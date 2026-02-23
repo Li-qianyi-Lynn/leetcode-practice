0f50c44f654e490496d583cd4a0048937a03c67a
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-22 23:40:49 -0800

    Add solution for Minimum Size Subarray Sum

--- LeetCode/Minimum-Size-Subarray-Sum/Solution.java
+++ LeetCode/Minimum-Size-Subarray-Sum/Solution.java
@@ -1,30 +1,25 @@
-class Solution {
-    public int minSubArrayLen(int s, int[] nums) {
-        int n = nums.length;
-        if (n == 0) {
-            return 0;
-        }
-
-        int ans = Integer.MAX_VALUE;
-
-        int[] sums = new int[n + 1]; 
-        for (int i = 1; i <= n; i++) {
-            sums[i] = sums[i - 1] + nums[i - 1];
-        }
-
-        for (int i = 1; i <= n; i++) {
-            int target = s + sums[i - 1];
-            int bound = Arrays.binarySearch(sums, target);
-            
-            if (bound < 0) {
-                bound = -bound - 1;
-            }
-
-            if (bound <= n) {
-                ans = Math.min(ans, bound - (i - 1));
-            }
-        }
-
-        return ans == Integer.MAX_VALUE ? 0 : ans;
-    }
-}
+1class Solution {
+2    public int minSubArrayLen(int target, int[] nums) {
+3        int r;
+4        int l = 0;
+5        int minLen = Integer.MAX_VALUE;
+6        int curSum = 0;
+7
+8        for (r = 0; r < nums.length; r++) {
+9            if (nums[r] == target) {
+10                return 1;
+11            }
+12            curSum += nums[r];
+13
+14            while (curSum >= target) { // 一直收左边界
+15                int curLen = r -l +1;
+16                minLen = Math.min(curLen, minLen);
+17                curSum -= nums[l];
+18                l++;
+19            }
+20
+21        }
+22        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+23        
+24    }
+25}
