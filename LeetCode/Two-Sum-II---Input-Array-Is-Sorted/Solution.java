0702e256743b8d93c2ee2e13117c14837596d913
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-22 23:25:42 -0800

    Add solution for Two Sum II - Input Array Is Sorted

--- LeetCode/Two-Sum-II---Input-Array-Is-Sorted/Solution.java
+++ LeetCode/Two-Sum-II---Input-Array-Is-Sorted/Solution.java
@@ -1,39 +1,23 @@
-class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        // use two pointers (l & r) 
-        /**
-        if nums[l] + nums[r] < target
-        l ++;
-        if res > target
-        r --;
-        else return their index;
-
-        [2,7,11,15]
-        |   |     
-        
-         */
-
-
-        int l = 0;
-        int r = numbers.length -1;
-        int[] res = new int[2];
-
-        while (l < r) {
-            int sum = numbers[l] + numbers[r];
-            if (sum < target) {
-                l++;
-            } else if (sum > target) {
-                r--;
-            } else {
-                res[0] = l+1;
-                res[1] = r+1;
-                return res;
-
-            }
-            
-
-        } 
-        return res;
-        
-    }
-}
+1class Solution {
+2    public int[] twoSum(int[] numbers, int target) {
+3        int[] res = new int[2];
+4        int r = numbers.length-1;
+5        int l = 0;
+6
+7        while (l < r) {
+8            if (l < numbers.length && numbers[r] + numbers[l] < target) {
+9                l++;
+10            } else if (r >= 0 && numbers[r] + numbers[l] > target) {
+11                r--;
+12            } else {
+13                res[0] = l+1;
+14                res[1] = r+1;
+15                break;
+16            }
+17
+18        }
+19        return res;
+20
+21        
+22    }
+23}
