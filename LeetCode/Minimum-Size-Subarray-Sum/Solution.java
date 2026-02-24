b4a875710063501fbedd35270ea7e6b510f448dc
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-23 21:03:01 -0800

    Add solution for Minimum Size Subarray Sum

--- LeetCode/Minimum-Size-Subarray-Sum/Solution.java
+++ LeetCode/Minimum-Size-Subarray-Sum/Solution.java
@@ -1,40 +1,23 @@
 1class Solution {
 2    public int minSubArrayLen(int target, int[] nums) {
-3        int n = nums.length;
-4        if (n == 0) return 0;
-5        
-6        int res = Integer.MAX_VALUE;
-7        // prefixSum[i] stores the sum of nums[0...i-1]
-8        int[] prefixSum = new int[n + 1];
-9        for (int i = 1; i <= n; i++) {
-10            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
-11        }
-12
-13        for (int i = 0; i < n; i++) {
-14            // Find the smallest j such that prefixSum[j] - prefixSum[i] >= target
-15            // Which is prefixSum[j] >= target + prefixSum[i]
-16            int toFind = target + prefixSum[i];
-17            int bound = binarySearch(prefixSum, i + 1, n, toFind);
-18            
-19            if (bound != -1) {
-20                res = Math.min(res, bound - i);
-21            }
-22        }
-23
-24        return res == Integer.MAX_VALUE ? 0 : res;
-25    }
-26
-27    private int binarySearch(int[] arr, int left, int right, int target) {
-28        int res = -1;
-29        while (left <= right) {
-30            int mid = left + (right - left) / 2;
-31            if (arr[mid] >= target) {
-32                res = mid;
-33                right = mid - 1; // Try to find a smaller index
-34            } else {
-35                left = mid + 1;
-36            }
-37        }
-38        return res;
-39    }
-40}
+3        int res = Integer.MAX_VALUE;
+4        int curSum = 0;
+5        int slow = 0;
+6        int fast;
+7        for (fast =0; fast < nums.length; fast++) {
+8            if (nums[fast]== target) {
+9                return 1;
+10
+11            }
+12            curSum += nums[fast];
+13
+14            while (curSum >= target) {
+15                res = Math.min(res, fast-slow+1);
+16                curSum -= nums[slow];
+17                slow++;
+18            }
+19        }
+20        return res == Integer.MAX_VALUE ? 0 : res;
+21        
+22    }
+23}
