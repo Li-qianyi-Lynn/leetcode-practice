6359cb3e2de38f627083da6a75bd040fa886e381
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-24 20:57:19 -0800

    Add solution for Substring with Concatenation of All Words

--- LeetCode/Substring-with-Concatenation-of-All-Words/Solution.java
+++ LeetCode/Substring-with-Concatenation-of-All-Words/Solution.java
@@ -1,56 +1,64 @@
 1class Solution {
 2    public List<Integer> findSubstring(String s, String[] words) {
 3        List<Integer> res = new ArrayList<>();
-4
-5        int wordLen = words[0].length();
-6        int wordNum = words.length;
-7        int totalLen = wordLen * wordNum;
-8        
-9        // 1. 统计 words 中每个单词出现的次数
-10        Map<String, Integer> wordCount = new HashMap<>();
-11        for (String w : words) {
-12            wordCount.put(w, wordCount.getOrDefault(w, 0) + 1);
-13        }
-14
-15        // 2. 只需要移动 wordLen 次，覆盖所有可能的起始对齐位置
-16        for (int i = 0; i < wordLen; i++) {
-17            int left = i, right = i;
-18            int count = 0; // 当前窗口内有效单词的总数
-19            Map<String, Integer> curMap = new HashMap<>();
-20
-21            // 3. 右指针滑动，每次跨越一个 wordLen
-22            while (right + wordLen <= s.length()) {
-23                String w = s.substring(right, right + wordLen);
-24                right += wordLen;
-25
-26                if (wordCount.containsKey(w)) {
-27                    curMap.put(w, curMap.getOrDefault(w, 0) + 1);
-28                    count++;
-29
-30                    // 如果当前单词多了，左指针右移直到不多为止
-31                    while (curMap.get(w) > wordCount.get(w)) {
-32                        String leftWord = s.substring(left, left + wordLen);
-33                        curMap.put(leftWord, curMap.get(leftWord) - 1);
-34                        count--;
-35                        left += wordLen;
-36                    }
+4        Map<String, Integer> map = new HashMap<>();
+5        int n = words.length;
+6        int wordLen = words[0].length();
+7
+8        for (String word : words) {
+9            map.put(word, map.getOrDefault(word,0)+1);
+10        }
+11        
+12        for (int i = 0; i < wordLen; i++) {
+13            int l = i;
+14            int r = i;
+15            int count = 0;
+16            Map<String, Integer> windowMap = new HashMap<>();
+17
+18            while (r + wordLen <= s.length()) {
+19                String cur = s.substring(r,r+wordLen);
+20                r += wordLen; //move right pointer
+21
+22                if (map.containsKey(cur)) {
+23                    windowMap.put(cur, windowMap.getOrDefault(cur,0)+1);
+24                    count++;
+25                    while (windowMap.get(cur)> map.get(cur)) {
+26                        
+27                        String leftWord = s.substring(l, l + wordLen);
+28                        windowMap.put(leftWord, windowMap.get(leftWord)-1);
+29                        l = l+ wordLen;
+30                        count --;
+31                    }
+32
+33                    if (count == n) {
+34                        res.add(l);
+35                    }   
+36
 37
-38                    // 窗口大小符合要求
-39                    if (count == wordNum) {
-40                        res.add(left);
-41                    }
-42                } else {
-43                    // 遇到完全不在列表里的词，清空重新开始
-44                    curMap.clear();
-45                    count = 0;
-46                    left = right;
-47                }
-48            }
-49        }
-50        return res;
-51    }
-52}
-53
-54/**
-55多路并行的滑动窗口
-56 */
+38                } else {
+39                    count = 0;
+40                    l = r;
+41                    windowMap.clear();
+42                }
+43            }
+44
+45    
+46
+47        }
+48        return res;
+49    }
+50}
+51
+52/**
+53slide window:
+54words same
+55count same
+56Hashmap<Char -> Integer>
+57
+580,1,2 iterate word.len
+59l: r
+60
+61
+62
+63
+64 */
