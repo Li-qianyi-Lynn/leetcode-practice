af3c944a1b81ffaa2f71c1f610bd94089b6ba66a
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-28 00:30:51 -0800

    Add solution for Valid Sudoku

--- LeetCode/Valid-Sudoku/Solution.java
+++ LeetCode/Valid-Sudoku/Solution.java
@@ -1,47 +1,46 @@
-class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        // whether each row/cols/square has duplicated digits 1-9 
-        // the hardest point is how to locate square cols and rows
-        // square
-        for (int sqr = 0; sqr < 9; sqr++) {
-            Set <Character> seen = new HashSet<>();
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    int row = (sqr / 3) * 3 +i;
-                    int col = (sqr % 3) * 3 +j;
-                    if (board[row][col] == '.' ) { continue;}
-                    if (seen.contains(board[row][col])) { return false;}
-                    seen.add(board[row][col]);
-
-                }
-
-            }
-        }
-
-        // rows
-        for (int row = 0; row < 9; row++) {
-            Set <Character> seen = new HashSet<>();
-            for (int i = 0; i < 9; i++) {
-                if (board[row][i] == '.') { continue;}
-                if (seen.contains(board[row][i])) { return false;}
-                    seen.add(board[row][i]);
-            }
-        }
-
-        for (int col = 0; col < 9; col++) {
-            Set <Character> seen = new HashSet<>();
-            for (int i = 0; i < 9; i++) {
-                if (board[i][col] == '.') { continue;}
-                if (seen.contains(board[i][col])) { return false;}
-                    seen.add(board[i][col]);
-            }
-
-
-        }
-        return true;
-
-
-        
-        
-    }
-}
+1class Solution {
+2    public boolean isValidSudoku(char[][] board) {
+3        Set<String> seen = new HashSet<>();
+4        int rows = board.length;
+5        int cols = board[0].length;
+6        for (int i = 0; i < rows; i++) {
+7            for (int j = 0; j < cols; j++) {
+8                char c = board[i][j];
+9                if (c != '.') {
+10                    if (!seen.add(i + "row" + c) || 
+11                    !seen.add(j + "col" + c) ||
+12                    !seen.add(i/3 + "-" + j/3 + "sq" + c)) {
+13                        return false;
+14                    }
+15                }
+16            }
+17        }
+18        return true;
+19        
+20    }
+21}
+22
+23/**
+24set(): char + row/col/sq + i/j/ box
+25row i
+26col j
+27square i/3 - j/3
+28
+290-0
+300-1
+310-2
+32
+331-0
+341-1
+351-2
+36
+372-0
+382-1
+392-2
+40
+41
+421. iterate
+432. check set.add(): 
+44 false :has dups, return false
+45 true: no worries
+46 */
