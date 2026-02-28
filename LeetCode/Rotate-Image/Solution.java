fe06ceb841b768e9d6ea8cbfb337ac2ffa55c354
Qianyi Li <li.qianyi@northeastern.edu>
2026-02-27 22:54:16 -0800

    Add solution for Rotate Image

--- LeetCode/Rotate-Image/Solution.java
+++ LeetCode/Rotate-Image/Solution.java
@@ -1,20 +1,24 @@
-1class Solution {
+1public class Solution {
 2    public void rotate(int[][] matrix) {
-3        int n = matrix.length;
-4  
+3        // Reverse the matrix vertically
+4        reverse(matrix);
 5
-6        for (int i = 0; i < (n / 2); i++) { // 外部圈层
-7            for (int j = i; j <(n - 1-i); j++) {
-8                int temp = matrix[i][j];
-9
-10                matrix[i][j] = matrix[n-1-j][i];
-11                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
-12                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
-13                matrix[j][n-1-i] = temp;
-14
-15            }
-16
-17        } 
-18    
-19    }
-20}
+6        // Transpose the matrix
+7        for (int i = 0; i < matrix.length; i++) {
+8            for (int j = i; j < matrix[i].length; j++) {
+9                int temp = matrix[i][j];
+10                matrix[i][j] = matrix[j][i];
+11                matrix[j][i] = temp;
+12            }
+13        }
+14    }
+15
+16    private void reverse(int[][] matrix) {
+17        int n = matrix.length;
+18        for (int i = 0; i < n / 2; i++) {
+19            int[] temp = matrix[i];
+20            matrix[i] = matrix[n - 1 - i];
+21            matrix[n - 1 - i] = temp;
+22        }
+23    }
+24}
