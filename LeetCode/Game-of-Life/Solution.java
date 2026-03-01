5c86d047b7505e9e7e248442d1078ef124c7a739
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-01 14:46:28 -0800

    Add solution for Game of Life

--- LeetCode/Game-of-Life/Solution.java
+++ LeetCode/Game-of-Life/Solution.java
@@ -1,71 +1,70 @@
 1class Solution {
 2    public void gameOfLife(int[][] board) {
-3        int rows = board.length;
-4        int cols = board[0].length;
-5
-6        int[] dires = new int[]{0,1,-1}; // 8 directions
-7
-8        for (int i = 0; i < rows; i++) {
-9            for (int j = 0; j < cols; j++) {
-10                int lived = 0;
-11
-12                for (int m = 0; m < 3; m++) {
-13                    for (int n = 0; n < 3; n++) {
-14                        if (!(dires[m] ==0 && dires[n] == 0)) {// 不同时为0
-15                            int x = i+dires[m];
-16                            int y = j+dires[n];
-17
-18                            if (x >= 0 && x < rows && y >= 0 && y < cols && (board[x][y] == 1 || board[x][y] == 3)) {
+3
+4        int m = board.length; //rows
+5        int n = board[0].length; // cols
+6
+7        int[] dirs = new int[]{-1,0,1};
+8
+9        for (int i = 0; i < m; i++) {
+10            for (int j = 0; j < n; j++) {
+11                int lived = 0;
+12                // iterate 8 dirs
+13                for (int a= 0; a < 3; a++) {
+14                    for (int b = 0; b < 3; b++) {
+15                        if (!(dirs[a] == 0 && dirs[b] == 0)) {
+16                            int x = i + dirs[a];
+17                            int y = j + dirs[b];
+18                            if (x >= 0 && x < m && y >= 0 && y < n && (board[x][y] == 1 || board[x][y] == -1)) {
 19                                lived++;
-20
-21                            }
-22                        }    
-23                    }
-24                }
-25                // live to die
-26                if ((lived < 2 || lived > 3) && board[i][j] == 1) {
-27                    board[i][j] = 3;
-28
-29                } 
-30                // if ((lived ==2 || lived == 3) && board[i][j] == 1) {
-31                //     board[i][j] =1;
-32
-33                // } 
-34                if (lived == 3 && board[i][j] == 0) {
-35                    board[i][j] = 2;
+20                            }
+21                        }
+22                    }
+23                }
+24// live to died -1 | nei 1 <= 2 ; nei > 3
+25// die to live 2 | nei 1 == 3
+26// live to live 1 |  nei 1 == 2/3
+27                if (board[i][j] == 1 && (lived < 2 || lived > 3)) {
+28                    board[i][j] = -1;
+29                }
+30
+31                if (board[i][j] == 0 && lived == 3) {
+32                    board[i][j] = 2;
+33                }
+34
+35            }
 36
-37                }
-38
-39            }
-40
-41        }
+37        }
+38        for (int i = 0; i < m; i++) {
+39            for (int j = 0; j < n; j++) {
+40                if (board[i][j] == -1) {
+41                    board[i][j] = 0;
 42
-43        for (int i = 0; i < rows; i ++) {
-44            for (int j = 0; j < cols; j++) {
-45                if (board[i][j] == 2) {
-46                    board[i][j] = 1;
+43                } else if (board[i][j] == 2) {
+44                    board[i][j] = 1;
+45
+46                }
 47
-48                } else if (board[i][j] == 3) {
-49                    board[i][j] = 0;
-50
-51                }
-52            }
-53
-54        }
-55        
-56    }
-57}
-58
-59/**
-600: died-> died
-611: live -> live
-622: died -> live
-633: live -> died
-64
-651. for loop to check lived neibours, update the status
-662. 2 for loop: 3 -> 0, 2-> 1
+48            }
+49
+50        }
+51
+52        // after for loop: -1 -> 0
+53        //2 -> 1
+54
+55
+56
+57        
+58    }
+59}
+60/**
+61
+62live to died -1 | nei 1 <= 2 ; nei > 3
+63die to live 2 | nei 1 == 3
+64live to live 1 |  nei 1 == 2/3
+65die to die 0
+66
 67
 68
 69
-70
-71 */
+70 */
