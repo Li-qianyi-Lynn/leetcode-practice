f7deb0599b04ddd7a9465ad965434a378a81b3d0
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-01 15:47:14 -0800

    Add solution for Isomorphic Strings

--- LeetCode/Isomorphic-Strings/Solution.java
+++ LeetCode/Isomorphic-Strings/Solution.java
@@ -1,39 +1,37 @@
 1class Solution {
 2    public boolean isIsomorphic(String s, String t) {
-3        if (s.length() != t.length()) {
-4            return false;
+3        if (s.length() == 1) {
+4            return true;
 5
 6        }
-7        Map<Character, Integer> mapS = new HashMap<>();
-8        Map<Character, Integer> mapT = new HashMap<>();
+7        Map<Character, Integer> maps = new HashMap<>();
+8        Map<Character, Integer> mapt = new HashMap<>();
 9
 10        for (int i = 0; i < s.length(); i++) {
-11            char charS = s.charAt(i);
-12            char charT = t.charAt(i);
-13            
-14            if (!(mapS.getOrDefault(charS,-1)).equals(mapT.getOrDefault(charT,-1))) {
-15                return false;
-16
-17            }
-18            mapS.put(charS, i);
-19            mapT.put(charT, i);
-20            
-21        }
-22        return true;
-23        
-24    }
+11            char sc = s.charAt(i);
+12            char tc = t.charAt(i);
+13            if (maps.getOrDefault(sc,-1).equals(mapt.getOrDefault(tc,-1))) {
+14                maps.put(sc,i);
+15                mapt.put(tc,i);   
+16            } else {
+17                return false;
+18            }
+19        }
+20        return true;
+21
+22
+23    }
+24
 25}
 26
 27/**
-28we need check whether these two strings have same pattern or not.
-29
-30store the index 
-31s.charAt(i) 
-32
-33char -> index
-34
-35
+28f 0
+291 1
+30
+31b 0
+322 1
+33
+34f -> b 
+351 -> 2
 36
-37
-38
-39 */
+37 */
