3c9621fa39f937215fc10c085727a4d0d99c1cd0
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-02 13:03:30 -0800

    Add solution for Happy Number

--- LeetCode/Happy-Number/Solution.java
+++ LeetCode/Happy-Number/Solution.java
@@ -1,24 +1,45 @@
 1class Solution {
-2    private int getNext(int n) {
-3        int totalSum = 0;
-4        while (n > 0) {
-5            int d = n % 10;    
-6            n = n / 10;          
-7            totalSum += d * d;   
-8        }
-9        return totalSum;
-10    }
-11    
-12    public boolean isHappy(int n) {
-13        Set<Integer> seen = new HashSet<>();  
-14        
-15        
-16        while (n != 1 && !seen.contains(n)) {
-17            seen.add(n);         
-18            n = getNext(n);      
-19        }
-20        
-21
-22        return n == 1;
-23    }
-24}
+2    public boolean isHappy(int n) {
+3        int slow = n;
+4        int fast = n;
+5
+6        while (fast != 1) {
+7            slow = getNext(slow);
+8            fast = getNext(getNext(fast));
+9            if (slow == fast && slow != 1) {
+10                return false;
+11
+12            }
+13
+14        }
+15        return true;
+16        
+17    }
+18
+19    private int getNext(int n) {
+20        int res = 0; // total sum
+21        while (n != 0) {
+22            int d = n % 10;
+23            n = n / 10;
+24            res += d * d;
+25        }
+26        return res;
+27
+28    }
+29}
+30
+31/**
+32
+33get next num
+344
+3516
+361+36 = 37
+379 + 49 = 58
+3825+64 89
+3964 81 145
+401 16 25 42
+4120
+424
+43
+44
+45 */
