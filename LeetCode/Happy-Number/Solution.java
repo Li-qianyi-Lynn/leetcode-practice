7227616f33f150573a6634063ae2e809843f867c
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-02 12:46:20 -0800

    Add solution for Happy Number

--- LeetCode/Happy-Number/Solution.java
+++ LeetCode/Happy-Number/Solution.java
@@ -1,16 +1,24 @@
-    int newNumber = 0;
-    
-    if (n == 1 || n== 7) return true;
-    
-    else if (n < 10) return false;
-    
-    while (n != 0) {
-        
-        newNumber += Math.pow(n%10,2);
-        
-        n = n/10;
-    }
-    
-    return isHappy(newNumber);
-    
-}
+1class Solution {
+2    private int getNext(int n) {
+3        int totalSum = 0;
+4        while (n > 0) {
+5            int d = n % 10;    
+6            n = n / 10;          
+7            totalSum += d * d;   
+8        }
+9        return totalSum;
+10    }
+11    
+12    public boolean isHappy(int n) {
+13        Set<Integer> seen = new HashSet<>();  
+14        
+15        
+16        while (n != 1 && !seen.contains(n)) {
+17            seen.add(n);         
+18            n = getNext(n);      
+19        }
+20        
+21
+22        return n == 1;
+23    }
+24}
