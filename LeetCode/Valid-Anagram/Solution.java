3a5b004ce49aadd08e845042aa7f047ce585b0cf
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-02 13:18:49 -0800

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.java
+++ LeetCode/Valid-Anagram/Solution.java
@@ -4,20 +4,25 @@
 4            return false;
 5
 6        }
-7        int[] sarr = new int[26];
-8        int[] tarr = new int[26];
+7        int[] count = new int[26];
+8
 9
-10
-11        for (int i = 0; i < s.length(); i++) {
-12            int idx1 = s.charAt(i) - 'a';
-13            int idx2 = t.charAt(i) - 'a';
-14            sarr[idx1]++;
-15            tarr[idx2]++;
-16        }
-17
-18        return Arrays.equals(sarr,tarr);
-19
-20
-21        
-22    }
-23}
+10        for (int i = 0; i < s.length(); i++) {
+11            int idx1 = s.charAt(i) - 'a';
+12            int idx2 = t.charAt(i) - 'a';
+13            count[idx1]++;
+14            count[idx2]--;
+15        }
+16
+17        for (int i : count) {
+18            if (i != 0) {
+19                return false;
+20            }
+21
+22        }
+23        return true;
+24
+25
+26        
+27    }
+28}
