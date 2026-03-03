bf132d89db4a1b7d3dc32fe2ef2fca25e55370cc
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-02 20:28:19 -0800

    Add solution for Word Pattern

--- LeetCode/Word-Pattern/Solution.java
+++ LeetCode/Word-Pattern/Solution.java
@@ -1,38 +1,24 @@
 1class Solution {
 2    public boolean wordPattern(String pattern, String s) {
-3        String[] split = s.split(" ");
-4        if (pattern.length() != split.length) {
-5            return false;
-6
-7        }
-8
-9        Map<Character, Integer> map = new HashMap<>();
-10        Map<String, Integer> sMap = new HashMap<>();
-11        for (int i = 0; i < split.length; i++) {
-12            char c = pattern.charAt(i);
-13            // int val1 = map.getOrDefault(c, 0);
-14            // int val2 = sMap.getOrDefault(split[i], 0);
-15            if (!map.getOrDefault(c,0).equals(sMap.getOrDefault(split[i],0))) {
-16            
-17                return false;
-18
-19            }
-20            map.put(c, i+1);
-21            sMap.put(split[i],i+1);
-22
-23        }
-24        
-25        return true;
-26
-27        
-28    }
-29}
-30
-31/**
-32a -> 1
-33b -> 3
-34-------
-35dog -> 1
-36cat -> 3
-37
-38 */
+3        String[] sp = s.split(" ");
+4        int[] target = new int[26];
+5        Arrays.fill(target, -1);
+6        Map<String, Integer> map = new HashMap<>();
+7
+8        if (sp.length != pattern.length()) {
+9            return false;
+10
+11        }
+12        for (int i = 0; i < pattern.length();i++) {
+13            int idx = pattern.charAt(i) - 'a';
+14            if (target[idx] != map.getOrDefault(sp[i],-1)) {
+15                return false;
+16            }
+17
+18            target[idx] = i;
+19            map.put(sp[i],i);
+20        } 
+21        return true;
+22        
+23    }
+24}
