fb4075607ea3172718a684bb7f1609bd3b89219d
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-02 20:52:36 -0800

    Add solution for Happy Number

--- LeetCode/Happy-Number/Solution.java
+++ LeetCode/Happy-Number/Solution.java
@@ -1,39 +1,45 @@
 1class Solution {
 2    public boolean isHappy(int n) {
-3        Set<Integer> set = new HashSet<>();
-4        while (n != 1 && !set.contains(n)) {
-5            set.add(n);
-6            n = getNext(n);
-7        }
-8
-9        return n == 1;
-10        
-11    }
-12
-13    private int getNext(int n) {
-14        int res = 0; // total sum
-15        while (n != 0) {
-16            int d = n % 10;
-17            n = n / 10;
-18            res += d * d;
-19        }
-20        return res;
-21
-22    }
-23}
-24
-25/**
-26
-27get next num
-284
-2916
-301+36 = 37
-319 + 49 = 58
-3225+64 89
-3364 81 145
-341 16 25 42
-3520
-364
-37
-38
-39 */
+3        int slow = n;
+4        int fast = n;
+5
+6        while (fast != 1) {
+7            slow = getNext(slow);
+8            fast = getNext(getNext(fast));
+9            if (slow == fast && slow != 1) {
+10                return false;
+11
+12            }
+13
+14        }
+15        return true;
+16        
+17    }
+18
+19    private int getNext(int n) {
+20        int res = 0; // total sum
+21        while (n != 0) {
+22            int d = n % 10;
+23            n = n / 10;
+24            res += d * d;
+25        }
+26        return res;
+27
+28    }
+29}
+30
+31/**
+32
+33get next num
+344
+3516
+361+36 = 37
+379 + 49 = 58
+3825+64 89
+3964 81 145
+401 16 25 42
+4120
+424
+43
+44
+45 */
