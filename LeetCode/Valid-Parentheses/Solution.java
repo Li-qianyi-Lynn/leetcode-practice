02c9253838e709b9db60f3a7b50775814f141064
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-04 01:16:39 -0800

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.java
+++ LeetCode/Valid-Parentheses/Solution.java
@@ -1,23 +1,23 @@
-class Solution {
-    public boolean isValid(String s) {
-        if (s.length() ==1) return false;
-        Map<Character, Character> map = new HashMap<>();
-        map.put(')','(');
-        map.put('}','{');
-        map.put(']','[');
-
-        Deque<Character> deque = new ArrayDeque<>();
-        for (char c : s.toCharArray()) {
-            if (!map.containsKey(c)) {
-                deque.offerLast(c);
-            } else {
-                if (!deque.isEmpty() && deque.peekLast() == map.get(c)) {
-                    deque.removeLast();
-                } else {
-                    return false;
-                }
-            }
-        }
-        return deque.isEmpty();      
-    }
-}
+1class Solution {
+2    public boolean isValid(String s) {
+3        if (s.length() ==1) return false;
+4        Map<Character, Character> map = new HashMap<>();
+5        map.put(')','(');
+6        map.put('}','{');
+7        map.put(']','[');
+8
+9        Deque<Character> deque = new ArrayDeque<>();
+10        for (char c : s.toCharArray()) {
+11            if (!map.containsKey(c)) {
+12                deque.offerLast(c);
+13            } else {
+14                if (!deque.isEmpty() && deque.peekLast() == map.get(c)) {
+15                    deque.removeLast();
+16                } else {
+17                    return false;
+18                }
+19            }
+20        }
+21        return deque.isEmpty();      
+22    }
+23}
