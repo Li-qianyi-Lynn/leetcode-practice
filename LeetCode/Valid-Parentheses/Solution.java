a9e647b439ced358429ca88a67a2057b6ba20cd0
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-04 01:18:11 -0800

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.java
+++ LeetCode/Valid-Parentheses/Solution.java
@@ -1,23 +1,37 @@
 1class Solution {
 2    public boolean isValid(String s) {
-3        if (s.length() ==1) return false;
-4        Map<Character, Character> map = new HashMap<>();
-5        map.put(')','(');
-6        map.put('}','{');
-7        map.put(']','[');
-8
-9        Deque<Character> deque = new ArrayDeque<>();
-10        for (char c : s.toCharArray()) {
-11            if (!map.containsKey(c)) {
-12                deque.offerLast(c);
-13            } else {
-14                if (!deque.isEmpty() && deque.peekLast() == map.get(c)) {
-15                    deque.removeLast();
-16                } else {
-17                    return false;
-18                }
-19            }
-20        }
-21        return deque.isEmpty();      
-22    }
-23}
+3        Deque<Character> stack = new ArrayDeque<>();
+4        for (char c : s.toCharArray()) {
+5            if (c ==')' && !stack.isEmpty()) {
+6                char poll = stack.pollLast();
+7                if (poll !='(') {
+8                    return false;
+9                }
+10            } else if (c =='}' && !stack.isEmpty()){
+11                char poll = stack.pollLast();
+12                if (poll != '{') {
+13                    return false;
+14
+15                }
+16            } else if (c == ']' && !stack.isEmpty()) {
+17                char poll = stack.pollLast();
+18                if (poll != '[') {
+19                    return false;
+20                }    
+21            } else {
+22                stack.offerLast(c);
+23
+24            }
+25        }
+26        return stack.isEmpty();
+27        
+28    }
+29}
+30// stack
+31/**
+32( added to stack
+33) check if has (
+34
+35
+36
+37 */
