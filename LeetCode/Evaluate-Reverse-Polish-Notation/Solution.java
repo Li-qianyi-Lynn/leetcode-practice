fa48a455d0f0e4b47f525c45f4ef1094970d038f
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-06 19:32:01 -0800

    Add solution for Evaluate Reverse Polish Notation

--- LeetCode/Evaluate-Reverse-Polish-Notation/Solution.java
+++ LeetCode/Evaluate-Reverse-Polish-Notation/Solution.java
@@ -1,36 +1,36 @@
-class Solution {
-    public int evalRPN(String[] tokens) {
-        Deque<Integer> deque = new ArrayDeque<>();
-
-        for (String token : tokens) {
-            if (!"+-*/".contains(token)) {
-                deque.offerLast(Integer.valueOf(token));
-                continue;
-            }
-
-            int number2 = deque.pollLast();
-            int number1 = deque.pollLast();
-
-            int result = 0;
-
-            switch (token) {
-                case "+":
-                    result = number1 + number2;
-                    break;
-                case "-":
-                    result = number1 - number2;
-                    break;
-                case "*":
-                    result = number1 * number2;
-                    break;
-                case "/":
-                    result = number1 / number2;
-                    break;
-            }
-
-            deque.offerLast(result);
-        }
-
-        return deque.pollLast();
-    }
-}
+1class Solution {
+2    public int evalRPN(String[] tokens) {
+3        Deque<Integer> deque = new ArrayDeque<>();
+4
+5        for (String token : tokens) {
+6            if (!"+-*/".contains(token)) {
+7                deque.offerLast(Integer.valueOf(token));
+8                continue;
+9            }
+10
+11            int number2 = deque.pollLast();
+12            int number1 = deque.pollLast();
+13
+14            int result = 0;
+15
+16            switch (token) {
+17                case "+":
+18                    result = number1 + number2;
+19                    break;
+20                case "-":
+21                    result = number1 - number2;
+22                    break;
+23                case "*":
+24                    result = number1 * number2;
+25                    break;
+26                case "/":
+27                    result = number1 / number2;
+28                    break;
+29            }
+30
+31            deque.offerLast(result);
+32        }
+33
+34        return deque.pollLast();
+35    }
+36}
