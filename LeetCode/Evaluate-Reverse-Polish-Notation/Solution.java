128216607e4f6910ec73aacfff1be48bbdca0d3e
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-06 19:43:10 -0800

    Add solution for Evaluate Reverse Polish Notation

--- LeetCode/Evaluate-Reverse-Polish-Notation/Solution.java
+++ LeetCode/Evaluate-Reverse-Polish-Notation/Solution.java
@@ -1,36 +1,39 @@
 1class Solution {
 2    public int evalRPN(String[] tokens) {
 3        Deque<Integer> deque = new ArrayDeque<>();
-4
+4       
 5        for (String token : tokens) {
 6            if (!"+-*/".contains(token)) {
 7                deque.offerLast(Integer.valueOf(token));
 8                continue;
 9            }
 10
-11            int number2 = deque.pollLast();
-12            int number1 = deque.pollLast();
-13
-14            int result = 0;
-15
-16            switch (token) {
-17                case "+":
-18                    result = number1 + number2;
-19                    break;
-20                case "-":
-21                    result = number1 - number2;
-22                    break;
-23                case "*":
-24                    result = number1 * number2;
-25                    break;
-26                case "/":
-27                    result = number1 / number2;
-28                    break;
-29            }
-30
-31            deque.offerLast(result);
-32        }
-33
-34        return deque.pollLast();
-35    }
-36}
+11            int fi = deque.pollLast();
+12            int se = deque.pollLast();
+13            int res = 0;
+14
+15            if (token.equals("+")) {
+16                res = fi + se;
+17
+18            } else if (token.equals("-")) {
+19                res = se - fi;
+20
+21            } else if (token.equals("*")) {
+22                res = se * fi;
+23
+24            } else {
+25                res = se / fi;
+26            }
+27            deque.offerLast(res);
+28        }
+29        return deque.pollLast();
+30        
+31        
+32            
+33            
+34
+35           
+36
+37       
+38    }
+39}
