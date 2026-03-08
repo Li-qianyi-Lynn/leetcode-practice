3a539b56133281e67c996c8c4cb4048a0dfc8b17
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-07 22:53:59 -0800

    Add solution for Copy List with Random Pointer

--- LeetCode/Copy-List-with-Random-Pointer/Solution.java
+++ LeetCode/Copy-List-with-Random-Pointer/Solution.java
@@ -1,25 +1,52 @@
-def copyRandomList(self, head: 'Node') -> 'Node':
-    
-    dummy_new = Node(None, None, None)
-    mappings = collections.defaultdict()
-    
-    old_node = head
-    prev_new = dummy_new
-    while old_node:
-        new_node = Node(old_node.val, None, None)
-        mappings[id(old_node)] = new_node
-        prev_new.next = new_node
-        prev_new = new_node
-        old_node = old_node.next
-    
-    old_node = head
-    new_node = dummy_new.next
-    while old_node:
-        if old_node.random == None:
-            new_node.random = None
-        else:
-            new_node.random = mappings[id(old_node.random)]
-        old_node = old_node.next
-        new_node = new_node.next
-    
-    return dummy_new.next
+1/*
+2// Definition for a Node.
+3class Node {
+4    int val;
+5    Node next;
+6    Node random;
+7
+8    public Node(int val) {
+9        this.val = val;
+10        this.next = null;
+11        this.random = null;
+12    }
+13}
+14*/
+15
+16class Solution {
+17    public Node copyRandomList(Node head) {
+18        Node cur = head;
+19
+20        Map<Node, Node> map = new HashMap<>();
+21        while (cur != null) {
+22            map.put(cur, new Node(cur.val));
+23            cur = cur.next;
+24
+25        }
+26
+27        cur = head;
+28        while(cur != null) {
+29            map.get(cur).next = map.get(cur.next);
+30            map.get(cur).random = map.get(cur.random);
+31            cur = cur.next;
+32        }
+33        return map.get(head);
+34        
+35        
+36    }
+37}
+38
+39/**
+40copy the original list, we have the random pointer
+41we can not link directly (not build it)
+42
+43use map
+44oldNode -> newNode getthe val to build new node
+45
+46map.get(oldNode).next/random
+47
+48
+49
+50
+51
+52 */
