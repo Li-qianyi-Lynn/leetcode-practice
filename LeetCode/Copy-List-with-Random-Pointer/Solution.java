890dc3346fe05b22dc7b697e1ba841847f1677ee
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-08 13:47:47 -0700

    Add solution for Copy List with Random Pointer

--- LeetCode/Copy-List-with-Random-Pointer/Solution.java
+++ LeetCode/Copy-List-with-Random-Pointer/Solution.java
@@ -15,29 +15,38 @@
 15
 16class Solution {
 17    public Node copyRandomList(Node head) {
-18        Map<Node, Node> map = new HashMap<>();
-19        Node cur = head;
-20        while (cur != null) {
-21            map.put(cur, new Node(cur.val));
-22            cur = cur.next;
-23        }
-24
-25        cur = head;
-26        while (cur != null) {
-27            map.get(cur).next = map.get(cur.next);
-28            map.get(cur).random = map.get(cur.random);
-29            cur = cur.next;
-30        }
-31        return map.get(head);
-32        
-33    }
-34}
-35
-36/**
-37map:
-381. copy: old node -> new node
-39
-402. set the connection
-41
-42
-43 */
+18        // edge case
+19        if (head == null) {
+20            return null;   
+21        }
+22        // step1: Map: oldNode -> newNode(object)
+23        Node cur = head;
+24        Map<Node,Node> map = new HashMap<>();
+25        while (cur != null) {
+26            Node newNode = new Node(cur.val);
+27            map.put(cur,newNode);
+28            cur = cur.next;     
+29        }
+30        // step2: build connections
+31        cur = head;
+32        while (cur != null) {
+33            if (cur.next != null) {
+34                map.get(cur).next = map.get(cur.next);    
+35            }
+36            if (cur.random != null) {
+37                map.get(cur).random = map.get(cur.random);   
+38            }
+39            cur = cur.next;   
+40        }
+41        return map.get(head);  
+42    }
+43}
+44
+45/**
+46map:
+471. copy: old node -> new node
+48
+492. set the connection
+50
+51
+52 */
