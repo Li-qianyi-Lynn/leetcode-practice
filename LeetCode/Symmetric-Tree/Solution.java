27b69978b2e17f2262aa3f0b510f4fad4725c42c
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-11 11:55:32 -0700

    Add solution for Symmetric Tree

--- LeetCode/Symmetric-Tree/Solution.java
+++ LeetCode/Symmetric-Tree/Solution.java
@@ -1,33 +1,33 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        return compare(root.left, root.right);
-        
-    }
-
-    private boolean compare(TreeNode left, TreeNode right) {
-        if (left == null && right != null) { return false;} 
-        if (right == null && left != null) { return false;}
-        if (left == null && right == null ) { return true;}
-        if (left.val != right.val) { return false;}
-
-        boolean outside = compare(left.left, right.right);
-        boolean inside = compare(left.right, right.left);
-
-        return outside && inside;
-    }
-}
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean isSymmetric(TreeNode root) {
+18        return compare(root.left, root.right);
+19        
+20    }
+21
+22    private boolean compare(TreeNode left, TreeNode right) {
+23        if (left == null && right != null) { return false;} 
+24        if (right == null && left != null) { return false;}
+25        if (left == null && right == null ) { return true;}
+26        if (left.val != right.val) { return false;}
+27
+28        boolean outside = compare(left.left, right.right);
+29        boolean inside = compare(left.right, right.left);
+30
+31        return outside && inside;
+32    }
+33}
