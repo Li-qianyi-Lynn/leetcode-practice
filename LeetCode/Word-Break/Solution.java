53a348aaca49f8930eb7e76cf3f46016e1fe7346
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-17 01:19:00 -0700

    Add solution for Word Break

--- LeetCode/Word-Break/Solution.java
+++ LeetCode/Word-Break/Solution.java
@@ -2,51 +2,40 @@
 2    public boolean wordBreak(String s, List<String> wordDict) {
 3        Set<String> set = new HashSet<>(wordDict);
 4        Deque<Integer> dq = new ArrayDeque<>();
-5        boolean[] visited = new boolean[s.length()+1]; // all false
-6        dq.offerLast(0);
-7
-8        while (!dq.isEmpty()) {
-9            int start = dq.pollFirst();
-10            // base case
-11            if (start == s.length()) {
-12                return true;
-13            }
-14
-15            for (int end = start+1; end <= s.length(); end++) {
-16                if (visited[end]) {
+5        int len = s.length();
+6        boolean[] isVisited = new boolean[len+1];
+7        
+8        dq.offerLast(0);
+9
+10        while (!dq.isEmpty()) {
+11            int start = dq.pollFirst();
+12            if (start == len) {
+13                return true;
+14            }
+15            for (int i = start+1; i <= len; i++) {
+16                if (isVisited[i] == true) {
 17                    continue;
-18
-19                }
-20                String target = s.substring(start, end);
-21                if (set.contains(target)) {
-22                    dq.offerLast(end);
-23                    visited[end] = true;
-24                }
+18                }
+19                String tar = s.substring(start,i);
+20                if (set.contains(tar)) {
+21                    dq.offerLast(i);
+22                    isVisited[i] = true;
+23                }
+24            }
 25
-26            }
-27
-28        }
-29        return false;
-30
-31        
-32    }
-33}
-34
-35/**
-36bfs
-37boolean visited: 判断 end 有没有被用过
+26        }
+27        
+28        return false;
+29    }
+30}
+31/**
+32divided s into diff words that are in worddict
+33bfs, start from 0 , check if substring(start,end) in the worddict
+34if yes, add to the dq and set as the next start
+35
+36visited map: reduce dup
+37
 38
-39dq.offerLast(start) 
-40start, end 去构造word
-41
-42for: (start, end)
-43判断end
-44
-45word= s.subString(start, end)
-46判断word 是不是在wordDict
-47
-48dq.pullFirst(end)
-49visited[end] = true
-50
-51 */
-52
+39
+40
+41 */
