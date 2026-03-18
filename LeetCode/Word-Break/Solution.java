f39e4dac9dbba367febab0eb430f1570006ea5e0
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-17 21:52:47 -0700

    Add solution for Word Break

--- LeetCode/Word-Break/Solution.java
+++ LeetCode/Word-Break/Solution.java
@@ -1,36 +1,44 @@
 1class Solution {
 2    public boolean wordBreak(String s, List<String> wordDict) {
-3        int len = s.length();
-4        boolean[] isVisited = new boolean[len+1];
-5        Set<String> set = new HashSet<>(wordDict);
-6        Deque<Integer> dq = new ArrayDeque<>();
-7        dq.offerLast(0);
-8
+3        Deque<Integer> dq = new ArrayDeque<>(); // to store index
+4        Set<String> set = new HashSet<>(wordDict); // check words
+5        int len = s.length();
+6        boolean[] isVisited = new boolean[len+1];
+7
+8        dq.offerLast(0);
 9        while (!dq.isEmpty()) {
-10            int start =dq.pollFirst();
+10            int start = dq.pollFirst();
 11            if (start == len) {
-12                return true;
+12                return true; // s can be segmented 
 13            }
-14            for (int i = start+1; i <= len; i++) {
-15                if (isVisited[i]== true) {
-16                    continue;
-17                }
-18                String target = s.substring(start,i);
-19                if (set.contains(target)) {
-20                    dq.offerLast(i);
-21                    isVisited[i] = true;
-22                }
-23
+14            
+15            for(int i = start+1; i <= len; i++) { //i: end index
+16                String cur = s.substring(start,i);
+17                if (isVisited[i]== true) {
+18                    continue;
+19                }
+20                if (set.contains(cur)) {
+21                    dq.offerLast(i);
+22                    isVisited[i] = true;
+23                }
 24            }
-25
-26        }
-27        return false;
-28
-29        
-30    }
-31}
+25        }
+26        return false;  
+27    }
+28}
+29
+30/**
+31dq:0
 32
-33/**
-34divied string into diff parts
+33bfs to check if we can go from the index 0 in String s -> index end in String s
+34dq: store the idx -> build cur string using substring
 35
-36 */
+36set: check if we have this word in the dict
+37map: to optimize , store we visit this end index or not
+38
+39s: l-e-e-t-c-o-d-e
+40   0.1 2 3 4 5 6 7
+41
+42   apple pen apple
+43
+44 */
