dbd7778ae577c3fbd491e4339275a8920718dcef
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-19 21:57:59 -0700

    Add solution for Word Break II

--- LeetCode/Word-Break-II/Solution.java
+++ LeetCode/Word-Break-II/Solution.java
@@ -1,60 +1,32 @@
 1class Solution {
-2    private Map<Integer, List<String>> memo = new HashMap<>();
-3    public List<String> wordBreak(String s, List<String> wordDict) {
-4        Set<String> set = new HashSet<>(wordDict);
-5        return dfs(s, set, 0); // todo 
-6        
-7    }
-8
-9    private List<String> dfs (String s, Set<String> set, int start) {
-10        int len = s.length();
-11        List<String> res = new ArrayList<>();
-12        
-13        // pruning
-14        if (memo.containsKey(start)) {
-15            return memo.get(start);
-16        }
-17        //base case
-18        if (start == len) {
-19            res.add("");
-20            return res;
-21        }
-22
-23        // recursion rules -> start, end index
-24        for (int end = start+1;end <= len; end++) {
-25            String cur = s.substring(start,end);
-26            if (set.contains(cur)) { // find a word
-27                List<String> next = dfs(s,set,end);
-28                for (String ne : next) {
-29                    if (ne.isEmpty()) {
-30                        res.add(cur);
-31                    } else {
-32                        res.add(cur + " " + ne);
-33                    }
-34                }
-35            }
-36        }
-37        memo.put(start,res);
-38        return res;
-39
-40        // how to divide -> 切割成功，dfs,end -> start index
-41    }
-42}
-43/**
-44input; strings, list<String>
-45output: List<String>
-46
-471. add space: divide -> dfs // start end idex 
-482. words in worddict  -> set
-49
-50recursion
-51// base case
-52
-53// recursion rules
-54
-55
-56// how to divide 
-57
-58memo: Map<> start -> list<String>
-59
-60 */
+2    public List<String> wordBreak(String s, List<String> wordDict) {
+3        int n = s.length();
+4    Set<String> wordSet = new HashSet<>(wordDict);
+5    
+6    // dp[i] 存储 s.substring(0, i) 的所有合法拆分方案
+7    List<String>[] dp = new ArrayList[n + 1];
+8    for (int i = 0; i <= n; i++) {
+9        dp[i] = new ArrayList<>();
+10    }
+11
+12    // 初始状态：空字符串的一种方案
+13    dp[0].add("");
+14
+15    for (int i = 1; i <= n; i++) {
+16        for (int j = 0; j < i; j++) {
+17            String suffix = s.substring(j, i);
+18            // 如果后缀在词典中，且前缀 dp[j] 有解
+19            if (dp[j].size() > 0 && wordSet.contains(suffix)) {
+20                for (String prev : dp[j]) {
+21                    // 拼接：如果是第一个单词不加空格，否则加空格
+22                    String join = prev.isEmpty() ? suffix : prev + " " + suffix;
+23                    dp[i].add(join);
+24                }
+25            }
+26        }
+27    }
+28
+29    return dp[n];
+30        
+31    }
+32}
