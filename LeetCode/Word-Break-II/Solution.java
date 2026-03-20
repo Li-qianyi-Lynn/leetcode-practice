50eee532d9495753fc9c19d825ee65b036daf5f9
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-19 20:58:13 -0700

    Add solution for Word Break II

--- LeetCode/Word-Break-II/Solution.java
+++ LeetCode/Word-Break-II/Solution.java
@@ -1,32 +1,50 @@
 1class Solution {
-2    public List<String> wordBreak(String s, List<String> wordDict) {
-3        int n = s.length();
-4    Set<String> wordSet = new HashSet<>(wordDict);
-5    
-6    // dp[i] 存储 s.substring(0, i) 的所有合法拆分方案
-7    List<String>[] dp = new ArrayList[n + 1];
-8    for (int i = 0; i <= n; i++) {
-9        dp[i] = new ArrayList<>();
-10    }
-11
-12    // 初始状态：空字符串的一种方案
-13    dp[0].add("");
+2    Map<Integer, List<String>> map = new HashMap<>();// start index 
+3    public List<String> wordBreak(String s, List<String> wordDict) {
+4        Set<String> set = new HashSet<>(wordDict);
+5        return dfs(s,set,0);
+6        
+7    }
+8    private List<String> dfs(String s, Set<String> set, int i) {
+9        List<String> res = new ArrayList<>();
+10        // base case
+11        if (map.containsKey(i)) {
+12            return map.get(i);
+13        }
 14
-15    for (int i = 1; i <= n; i++) {
-16        for (int j = 0; j < i; j++) {
-17            String suffix = s.substring(j, i);
-18            // 如果后缀在词典中，且前缀 dp[j] 有解
-19            if (dp[j].size() > 0 && wordSet.contains(suffix)) {
-20                for (String prev : dp[j]) {
-21                    // 拼接：如果是第一个单词不加空格，否则加空格
-22                    String join = prev.isEmpty() ? suffix : prev + " " + suffix;
-23                    dp[i].add(join);
-24                }
-25            }
-26        }
-27    }
-28
-29    return dp[n];
-30        
-31    }
-32}
+15        int len = s.length();
+16        if (i == len) {
+17            res.add("");
+18            return res;
+19        }
+20
+21        for (int end = i+1; end <= len; end++) {
+22            String cur = s.substring(i,end);
+23            if (set.contains(cur)) {
+24
+25                List<String> next = dfs(s,set,end);
+26                for (String nex : next) {
+27                    if (nex.isEmpty()) {
+28                        res.add(cur);
+29
+30                    } else {
+31                        res.add(cur + " " + nex);
+32                    }  
+33                }
+34            }
+35        }
+36        map.put(i, res);
+37        return res;
+38
+39    }
+40}
+41/**
+42cut dicts
+43start index end index
+44
+45memo = m
+46dfs: 重复的字问题
+47
+48
+49
+50 */
