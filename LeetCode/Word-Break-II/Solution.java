dd4363e1cfea1b8170d46a93a924f1d2e6105be4
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-19 17:27:26 -0700

    Add solution for Word Break II

--- LeetCode/Word-Break-II/Solution.java
+++ LeetCode/Word-Break-II/Solution.java
@@ -1,54 +1,32 @@
 1class Solution {
-2    Map<Integer, List<String>> map = new HashMap<>();
-3
-4    public List<String> wordBreak(String s, List<String> wordDict) {
-5        Set<String> set = new HashSet<>(wordDict);
-6        return dfs(s,set,0); //
-7        
-8    }
-9
-10    private List<String> dfs (String s, Set<String> set, int start) {
-11        List<String> res = new ArrayList<>();
-12
-13        // base case
-14        if (map.containsKey(start)) {
-15            return map.get(start);
-16        }
-17        int len = s.length();
-18
-19        if (start == len) {
-20            res.add(""); //success signal
-21            return res;
-22        }
-23
-24        for (int end = start+1; end <= len; end++) {
-25            String cur = s.substring(start,end);
-26            if (set.contains(cur)) {
-27                List<String> next = dfs(s,set,end);
-28                
-29                for (String ne : next) {
-30                    if (ne.isEmpty()) {
-31                        res.add(cur);
-32
-33                    } else {
-34                        res.add(cur + " " + ne);
-35                    }
-36                
-37                }
-38            }
-39
-40        }
-41        map.put(start,res);
-42        return res;
-43
-44    }
-45}
-46/**
-47left s can be divided or not
-48memo  start -> list<String>
-49
-50dfs
-51
-52
-53 */
-54
+2    public List<String> wordBreak(String s, List<String> wordDict) {
+3        int n = s.length();
+4    Set<String> wordSet = new HashSet<>(wordDict);
+5    
+6    // dp[i] 存储 s.substring(0, i) 的所有合法拆分方案
+7    List<String>[] dp = new ArrayList[n + 1];
+8    for (int i = 0; i <= n; i++) {
+9        dp[i] = new ArrayList<>();
+10    }
+11
+12    // 初始状态：空字符串的一种方案
+13    dp[0].add("");
+14
+15    for (int i = 1; i <= n; i++) {
+16        for (int j = 0; j < i; j++) {
+17            String suffix = s.substring(j, i);
+18            // 如果后缀在词典中，且前缀 dp[j] 有解
+19            if (dp[j].size() > 0 && wordSet.contains(suffix)) {
+20                for (String prev : dp[j]) {
+21                    // 拼接：如果是第一个单词不加空格，否则加空格
+22                    String join = prev.isEmpty() ? suffix : prev + " " + suffix;
+23                    dp[i].add(join);
+24                }
+25            }
+26        }
+27    }
+28
+29    return dp[n];
+30        
+31    }
+32}
