6c579074190aa79d6cd98513fea039cdfa3ae3f5
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-23 23:38:57 -0700

    Add solution for Binary Tree Zigzag Level Order Traversal

--- LeetCode/Binary-Tree-Zigzag-Level-Order-Traversal/Solution.java
+++ LeetCode/Binary-Tree-Zigzag-Level-Order-Traversal/Solution.java
@@ -13,50 +13,42 @@
 13 *     }
 14 * }
 15 */
-16
-17
-18class Solution {
-19    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-20        List<List<Integer>> res = new ArrayList<>();
-21        if (root == null) return res;
-22        bfs(root, res);
-23        return res;   
-24    }
-25
-26    private void bfs(TreeNode root, List<List<Integer>>res) {
-27        if (root == null) return;
-28        Deque<TreeNode> deque = new LinkedList<>();
-29        boolean leftToRight = true;
-30
-31        deque.offerLast(root);
-32        while(!deque.isEmpty()) {
-33            int size = deque.size();
-34
-35            List<Integer> temp = new ArrayList<>();
-36
-37            for (int i = 0; i < size; i++) {
-38                if (leftToRight) {
-39                    TreeNode poll = deque.pollFirst();
-40                    if (poll.left != null) {
-41                        deque.offerLast(poll.left);
+16class Solution {
+17    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+18        List<List<Integer>> res = new ArrayList<>();
+19        if (root == null) return res;
+20        bfs(root, res);
+21        return res;   
+22    }
+23
+24    private void bfs(TreeNode root, List<List<Integer>>res) {
+25        if (root == null) return;
+26        Deque<TreeNode> deque = new LinkedList<>();
+27        boolean leftToRight = true;
+28
+29        deque.offerLast(root);
+30        while(!deque.isEmpty()) {
+31            int size = deque.size();
+32            List<Integer> levelNode= new LinkedList<>();
+33
+34            for (int i = 1; i <= size; i++) {
+35                TreeNode poll = deque.pollFirst();
+36                levelNode.add(poll.val);
+37                if (poll.left != null) {
+38                        deque.offerLast(poll.left);
+39                    } 
+40                    if (poll.right != null) {
+41                        deque.offerLast(poll.right);
 42                    }
-43                    if (poll.right != null) {
-44                        deque.offerLast(poll.right);
-45                    }
-46                    temp.add(poll.val);
-47                    continue;
-48                }
-49                TreeNode poll = deque.pollLast();
-50                if (poll.right != null) {
-51                    deque.offerFirst(poll.right);
-52                }
-53                if (poll.left != null) {
-54                    deque.offerFirst(poll.left);
-55                }
-56                temp.add(poll.val);
-57            }
-58            res.add(temp);
-59            leftToRight = !leftToRight;
-60        }
-61    }
-62}
+43                
+44            }
+45            if (!leftToRight) {
+46                Collections.reverse(levelNode);
+47            }
+48            res.add(levelNode);
+49            leftToRight = !leftToRight;
+50        }
+51        
+52    
+53    }
+54}
