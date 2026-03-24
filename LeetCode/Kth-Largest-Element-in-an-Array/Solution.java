63fade71f5bf1fbd36f6c0e860c4c0203b3128da
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-24 15:35:41 -0700

    Add solution for Kth Largest Element in an Array

--- LeetCode/Kth-Largest-Element-in-an-Array/Solution.java
+++ LeetCode/Kth-Largest-Element-in-an-Array/Solution.java
@@ -1,19 +1,18 @@
-class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        // use min pq to store the frequency
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int res = 0;
-
-        //put num into pq
-        for (int num : nums) {
-            pq.offer(num);
-
-            if (pq.size() > k) {
-                pq.poll(); // remove the smallest one (top of the heap)
-            }
-        }
-
-        return pq.peek();
-        
-    }
-}
+1class Solution {
+2    public int findKthLargest(int[] nums, int k) {
+3        // use min pq to store the frequency
+4        PriorityQueue<Integer> pq = new PriorityQueue<>();
+5
+6        //put num into pq
+7        for (int num : nums) {
+8            pq.offer(num);
+9
+10            if (pq.size() > k) {
+11                pq.poll(); // remove the smallest one (top of the heap)
+12            }
+13        }
+14
+15        return pq.peek();
+16        
+17    }
+18}
