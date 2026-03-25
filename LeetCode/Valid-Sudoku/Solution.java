bb89852cbf33af240734326fb1eb2337889d89c9
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-24 23:27:49 -0700

    Add solution for Valid Sudoku

--- LeetCode/Valid-Sudoku/Solution.java
+++ LeetCode/Valid-Sudoku/Solution.java
@@ -1,50 +1,52 @@
 1class Solution {
 2    public boolean isValidSudoku(char[][] board) {
-3
-4        boolean[][] row = new boolean[9][9]; 
-5        boolean[][] col = new boolean[9][9]; 
-6        boolean[][] sq = new boolean[9][9]; 
-7
-8        for (int i = 0; i < 9; i++) {
-9            for (int j = 0; j < 9; j++) {
-10                char c = board[i][j];
-11                if (c != '.') {
-12                    int num = c - '1';
-13                    int sqidx = (i / 3) * 3 + (j/3);
-14                    if (row[i][num] || col[j][num] || sq[sqidx][num]) {
-15                        return false;
-16                    }
-17                    row[i][num] = true;
-18                    col[j][num] = true;
-19                    sq[sqidx][num] = true;
-20                }
-21
-22            }
+3        int rows = board.length;
+4        int cols = board[0].length;
+5
+6        boolean[][] row = new boolean[9][9]; 
+7        boolean[][] col = new boolean[9][9]; 
+8        boolean[][] sq = new boolean[9][9]; 
+9
+10        for (int i = 0; i < rows; i++) {
+11            for (int j = 0; j < cols; j++) {
+12                char c = board[i][j];
+13                if (c != '.') {
+14                    int num = c - '1';
+15                    int sqidx = (i / 3) * 3 + (j/3);
+16                    if (row[i][num] || col[j][num] || sq[sqidx][num]) {
+17                        return false;
+18                    }
+19                    row[i][num] = true;
+20                    col[j][num] = true;
+21                    sq[sqidx][num] = true;
+22                }
 23
-24        }
-25        return true;
-26
-27
-28        
-29        
-30    }
-31}
-32
-33/**
-34boolean[][]
-35
-361.row/col/sq
-37row : i
-38col : j
-39sq : sdidx (i/ 3) * 3 + j/3
-40
-412.char num : c - '1'-> 0-8
+24            }
+25
+26        }
+27        return true;
+28
+29
+30        
+31        
+32    }
+33}
+34
+35/**
+36boolean[][]
+37
+381.row/col/sq
+39row : i
+40col : j
+41sq : sdidx (i/ 3) * 3 + j/3
 42
-431. iterate board, get char and num
-44  - char:
-45     if any boolean arry is true, return false;
-46
-47  - . skip
+432.char num : c - '1'-> 0-8
+44
+451. iterate board, get char and num
+46  - char:
+47     if any boolean arry is true, return false;
 48
-49  update boolean arrys
-50*/
+49  - . skip
+50
+51  update boolean arrys
+52*/
