66b8078065d4acb4eb6e6b93f142950ab17aee88
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-24 23:11:24 -0700

    Add solution for Search in Rotated Sorted Array

--- LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
+++ LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
@@ -1,31 +1,85 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        # 创建一个哈希表/字典，存储右括号到左括号的映射
-        # 这有助于快速查找匹配的左括号
-        mappings = {
-            ')': '(',
-            ']': '[',
-            '}': '{'
-        }
-
-        # 使用列表作为栈
-        stack = []
-
-        # 遍历输入字符串中的每个字符
-        for char in s:
-            # 如果当前字符是右括号
-            if char in mappings:
-                # 检查栈是否为空，如果为空，则 pop() 会报错
-                # 如果不为空，则弹出栈顶元素，否则使用一个哑元值'#'，确保不匹配
-                top_element = stack.pop() if stack else '#'
-                
-                # 检查弹出的左括号是否与当前右括号匹配
-                if mappings[char] != top_element:
-                    return False
-            else:
-                # 如果当前字符是左括号，直接压入栈中
-                stack.append(char)
-        
-        # 循环结束后，如果栈为空，说明所有括号都匹配了
-        # 如果栈不为空，说明有未匹配的左括号
-        return not stack
+1class Solution {
+2    public int search(int[] nums, int target) {
+3        int l = 0;
+4        int r = nums.length-1;
+5
+6        // while (l <= r) {
+7
+8        //     int mid = l + (r-l) / 2;
+9
+10        //     if (nums[mid] == target) {
+11        //         return mid;
+12        //     }
+13
+14        //     if (nums[l] <= nums[mid] ) { // left side in order
+15        //         if (target < nums[l] || target > nums[mid]) {
+16        //             l = mid +1;
+17        //         } else {
+18        //             r = mid -1;
+19        //         }
+20
+21        //     } else { // nums[mid]<=target<= nums[r]
+22        //         if (target < nums[mid] || target > nums[r]) {
+23        //             r = mid -1;
+24
+25        //         } else {
+26        //             l = mid+1;
+27        //         }
+28        //     }
+29
+30        // }
+31
+32
+33        while (l <= r) {
+34            int mid = l + (r-l) / 2;
+35            if (nums[mid] == target) {
+36                return mid;
+37            }
+38
+39            if (nums[l] <= nums[mid] ) { // left side in order
+40                if (target < nums[l] || target > nums[mid]) {
+41                    l = mid + 1;
+42                } else {
+43                    r = mid -1;
+44                    break;
+45                }
+46            } else {
+47                if (target < nums[mid] || target > nums[r]) {
+48                    r = mid - 1;
+49                } else {
+50                    l = mid + 1;
+51                    break;
+52                }
+53            }
+54        }
+55
+56        while (l <= r) {
+57            int mid = l + (r - l) / 2;
+58            if (nums[mid] == target) {
+59                return mid;
+60            }
+61
+62            if (nums[mid] < target) {
+63                l = mid + 1;
+64            } else {
+65                r = mid - 1;
+66            }
+67        }
+68        return -1;
+69    }
+70}
+71
+72/**
+73binary search
+74mid: base point 
+75
+76return mind
+77
+78-left side inorder  nums[l]<=target<=nums[mid]
+79    binary search 
+80-right side inorder
+81
+82return -1
+83
+84
+85 */
