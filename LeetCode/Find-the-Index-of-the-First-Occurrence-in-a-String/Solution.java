02d2f1d3f24b94a2d7ed2129f1af6dfd46da6257
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-24 21:57:35 -0700

    Add solution for Find the Index of the First Occurrence in a String

--- LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.java
+++ LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.java
@@ -1,34 +1,17 @@
 1class Solution {
 2    public int strStr(String haystack, String needle) {
-3        int n = haystack.length();
-4        int m = needle.length();
-5        if (m == 0) return 0;
-6
-7        // 1. 构建 next 数组
-8        int[] next = new int[m];
-9        for (int i = 1, j = 0; i < m; i++) {
-10            while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
-11                j = next[j - 1];
-12            }
-13            if (needle.charAt(i) == needle.charAt(j)) {
-14                j++;
-15            }
-16            next[i] = j;
-17        }
-18
-19        // 2. 匹配过程
-20        for (int i = 0, j = 0; i < n; i++) {
-21            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
-22                j = next[j - 1];
-23            }
-24            if (haystack.charAt(i) == needle.charAt(j)) {
-25                j++;
-26            }
-27            if (j == m) {
-28                return i - m + 1; // 找到匹配，返回起始索引
-29            }
-30        }
-31
-32        return -1;
-33    }
-34}
+3        int len1 = haystack.length();
+4        int len2 = needle.length();
+5
+6     
+7        for(int i = 0, j = len2; j <= len1; i++,j++) {
+8            if (haystack.substring(i,j).equals(needle)) {
+9                return i;
+10            }
+11
+12        }
+13        return -1;
+14
+15        
+16    }
+17}
