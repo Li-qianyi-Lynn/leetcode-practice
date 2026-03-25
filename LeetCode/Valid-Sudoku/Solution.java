19827a7de740ad54c2b383bc9cff1b94fd9ac47a
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-24 23:27:58 -0700

    Add solution for Valid Sudoku

--- LeetCode/Valid-Sudoku/Solution.java
+++ LeetCode/Valid-Sudoku/Solution.java
@@ -1,46 +1,47 @@
 1class Solution {
 2    public boolean isValidSudoku(char[][] board) {
-3        Set<String> seen = new HashSet<>();
-4        int rows = board.length;
-5        int cols = board[0].length;
-6        for (int i = 0; i < rows; i++) {
-7            for (int j = 0; j < cols; j++) {
-8                char c = board[i][j];
-9                if (c != '.') {
-10                    if (!seen.add(i + "row" + c) || 
-11                    !seen.add(j + "col" + c) ||
-12                    !seen.add(i/3 + "-" + j/3 + "sq" + c)) {
-13                        return false;
-14                    }
-15                }
-16            }
-17        }
-18        return true;
-19        
-20    }
-21}
-22
-23/**
-24set(): char + row/col/sq + i/j/ box
-25row i
-26col j
-27square i/3 - j/3
-28
-290-0
-300-1
-310-2
-32
-331-0
-341-1
-351-2
-36
-372-0
-382-1
-392-2
-40
-41
-421. iterate
-432. check set.add(): 
-44 false :has dups, return false
-45 true: no worries
-46 */
+3        // whether each row/cols/square has duplicated digits 1-9 
+4        // the hardest point is how to locate square cols and rows
+5        // square
+6        for (int sqr = 0; sqr < 9; sqr++) {
+7            Set <Character> seen = new HashSet<>();
+8            for (int i = 0; i < 3; i++) {
+9                for (int j = 0; j < 3; j++) {
+10                    int row = (sqr / 3) * 3 +i;
+11                    int col = (sqr % 3) * 3 +j;
+12                    if (board[row][col] == '.' ) { continue;}
+13                    if (seen.contains(board[row][col])) { return false;}
+14                    seen.add(board[row][col]);
+15
+16                }
+17
+18            }
+19        }
+20
+21        // rows
+22        for (int row = 0; row < 9; row++) {
+23            Set <Character> seen = new HashSet<>();
+24            for (int i = 0; i < 9; i++) {
+25                if (board[row][i] == '.') { continue;}
+26                if (seen.contains(board[row][i])) { return false;}
+27                    seen.add(board[row][i]);
+28            }
+29        }
+30
+31        for (int col = 0; col < 9; col++) {
+32            Set <Character> seen = new HashSet<>();
+33            for (int i = 0; i < 9; i++) {
+34                if (board[i][col] == '.') { continue;}
+35                if (seen.contains(board[i][col])) { return false;}
+36                    seen.add(board[i][col]);
+37            }
+38
+39
+40        }
+41        return true;
+42
+43
+44        
+45        
+46    }
+47}
