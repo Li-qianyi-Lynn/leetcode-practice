43c2619981ab3f2e1bc9a5d7f8a3a8f92a30c474
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-24 22:24:14 -0700

    Add solution for Letter Combinations of a Phone Number

--- LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.java
+++ LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.java
@@ -1,52 +1,53 @@
 1class Solution {
-2    
-3
-4    public List<String> letterCombinations(String digits) {
-5        Map<Character, String> map = new HashMap<>();
-6        map.put('2', "abc");
-7        map.put('3', "def");
-8        map.put('4', "ghi");
-9        map.put('5', "jkl");
-10        map.put('6', "mno");
-11        map.put('7', "pqrs");
-12        map.put('8', "tuv");
-13        map.put('9', "wxyz");
-14        List<String> res = new ArrayList<>();
-15        StringBuilder sb = new StringBuilder();
-16        backtracking(map, sb, digits, res, 0); //todo
-17
-18        return res;     
-19    }
-20
-21    private void backtracking(Map<Character, String> map, StringBuilder cur, String digits,List<String>res, int index) {
-22        //base case
-23        if (cur.length() == digits.length()) {
-24            res.add(cur.toString());
-25            return;
-26        }
-27
-28        String temp = map.get(digits.charAt(index));
-29        for (int i = 0; i < temp.length(); i++) {
-30            cur.append(temp.charAt(i));
-31            backtracking(map, cur, digits, res, index+1);
-32            cur.deleteCharAt(cur.length()-1);
-33
-34        }
-35
-36    }
-37}
-38
-39/**
-40map: Character -> String
-41iterate digits, for each digits, get the value
-42backtrack()
-431. base case: sb.size() == digits.length(); add cur sb to res;
-442. get mappings string
-453. iterate mapping string
-46    append charAt(i)
-47    backtrack() next num
-48    deleteCharAt(len-1);
-49
-50
-51
-52 */
+2    private List<String> combinations = new ArrayList<>();
+3    private Map<Character, String> letters = Map.of(
+4        '2',
+5        "abc",
+6        '3',
+7        "def",
+8        '4',
+9        "ghi",
+10        '5',
+11        "jkl",
+12        '6',
+13        "mno",
+14        '7',
+15        "pqrs",
+16        '8',
+17        "tuv",
+18        '9',
+19        "wxyz"
+20    );
+21    private String phoneDigits;
+22
+23    public List<String> letterCombinations(String digits) {
+24        // If the input is empty, immediately return an empty answer array
+25        if (digits.length() == 0) {
+26            return combinations;
+27        }
+28
+29        // Initiate backtracking with an empty path and starting index of 0
+30        phoneDigits = digits;
+31        backtrack(0, new StringBuilder());
+32        return combinations;
+33    }
+34
+35    private void backtrack(int index, StringBuilder path) {
+36        // If the path is the same length as digits, we have a complete combination
+37        if (path.length() == phoneDigits.length()) {
+38            combinations.add(path.toString());
+39            return; // Backtrack
+40        }
+41
+42        // Get the letters that the current digit maps to, and loop through them
+43        String possibleLetters = letters.get(phoneDigits.charAt(index));
+44        for (char letter : possibleLetters.toCharArray()) {
+45            // Add the letter to our current path
+46            path.append(letter);
+47            // Move on to the next digit
+48            backtrack(index + 1, path);
+49            // Backtrack by removing the letter before moving onto the next
+50            path.deleteCharAt(path.length() - 1);
+51        }
+52    }
+53}
