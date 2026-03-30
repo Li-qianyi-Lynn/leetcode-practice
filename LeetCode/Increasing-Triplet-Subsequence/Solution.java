37f9d2e048790c340ed2e9c140d3e0e919b60248
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-29 23:05:11 -0700

    Add solution for Increasing Triplet Subsequence

---
+++ LeetCode/Increasing-Triplet-Subsequence/Solution.java
@@ -0,0 +1,11 @@
+first_num = second_num = some very big number
+for n in nums:
+    if n is smallest number:
+        first_num = n
+    else if n is second smallest number:
+        second_num = n
+    else n is bigger than both first_num and second_num:
+        # We have found our triplet, return True
+
+# After loop has terminated
+# If we have reached this point, there is no increasing triplet, return False
