00f1cd08454044756c9471a26465e6b92830fbb8
Qianyi Li <li.qianyi@northeastern.edu>
2026-03-30 21:50:47 -0700

    Add solution for String Compression

--- LeetCode/String-Compression/Solution.java
+++ LeetCode/String-Compression/Solution.java
@@ -1,65 +1,60 @@
 1class Solution {
 2    public int compress(char[] chars) {
-3        // pruning
-4        if (chars.length == 1) {
-5            return 1;
-6        }
-7
-8        int n = chars.length;
-9        int s = 0;
-10        int f = 0;
-11        int cur = 0;
-12        while (f < n) {
-13            int count = 0;
-14            
-15            // len
-16            while (f < n && chars[f] == chars[s]) {
-17                f++;
-18                count++;
-19            }
-20            
-21            chars[cur++] = chars[s];
-22            // ? mark start index?
-23            int start = cur;
-24            if (count > 1) {
-25                while (count != 0) { 
-26                    chars[cur++] = (char)((count % 10) + '0');
-27                    count = count / 10;
-28                }
-29                reverse(chars, start, cur-1);
-30            }
+3        if (chars.length == 1) {
+4            return 1;
+5        }
+6        int n =chars.length;
+7        int slow = 0;
+8        int fast = 0;
+9        int cur = 0;
+10        while (fast < n) {
+11            // cur = slow;
+12            while (fast < n && chars[fast] == chars[slow]) {
+13                fast++;
+14            }
+15            int count = fast - slow;
+16            // write char
+17            chars[cur++] = chars[slow];
+18
+19            if (count > 1) {
+20                // String s = String.valueOf(count);
+21                // char[] temp = s.toCharArray();
+22                // for (char c : temp) {
+23                //     chars[cur++] = c;
+24                // }
+25                for (char c : String.valueOf(count).toCharArray()) {
+26                    chars[cur++] = c;
+27                }
+28
+29            }
+30            slow = fast;
 31
-32            s = f;
-33
-34        }
-35        return cur;    
+32        }
+33        return cur;
+34         
+35        
 36    }
-37
-38    private void reverse (char[] chars, int start, int end) {
-39        while (start < end) {
-40            char temp = chars[start];
-41            chars[start] = chars[end];
-42            chars[end] = temp;
-43            start ++;
-44            end --;
-45        }
-46    }
-47}
-48/**
-49group len 
-50- 1 append char
-51- > 1 append char, len
+37}
+38/**
+39
+40["a","a","b","b","c","c","c"]
+41  s
+42          f
+43 base
+44
+451. how many letters 
+462. write: chars[base++]=chars[s]
+473. if count > 1: digits -> string -> tochararray
+48chars[base++]= c;
+49
+50
+51s = f
 52
-5310 -> 1 0
+53return base
 54
-55modify chars[]
-56 
-57 ["a","a","b","b","c","c","c"]
-58           s
-59           f
-6012345
-61  cur 
-62 1. len (same chars) : while 
-63 2. 1) how to write the ans -> 
-64    2) 12 -> ?  count % 10  = 2 count / 10 1 -> reverse 
-65 */
+55
+56
+57
+58
+59
+60 */
