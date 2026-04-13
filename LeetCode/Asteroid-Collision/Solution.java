55d545c2d1f8e34586fac06145606277d44dbd82
Qianyi Li <li.qianyi@northeastern.edu>
2026-04-12 20:10:13 -0700

    Add solution for Asteroid Collision

--- LeetCode/Asteroid-Collision/Solution.java
+++ LeetCode/Asteroid-Collision/Solution.java
@@ -1,3 +1,55 @@
- a. If the `asteroid` is bigger than the asteroid on the top, then this asteroid on the top will explode, and we will pop it from the stack.
- b. If the `asteroid` has the same size as the asteroid on the top, then both will explode. Hence we will pop it from the stack; also, we will mark `flag` as `false` because this `asteroid` will also explode, and hence we won't save it into the stack.
- c. If the `asteroid` is smaller than the asteroid on the top, then the asteroid on the top will not explode, so we will not pop it. But the `asteroid` will explode and thus we will mark `flag` as `false`.
+1class Solution {
+2    public int[] asteroidCollision(int[] asteroids) {
+3        Deque<Integer> dq = new ArrayDeque<>();
+4        for (int a : asteroids) {
+5            boolean alive = true;
+6            while (alive && !dq.isEmpty() && dq.peekLast() > 0 && a < 0) { // todo ? 
+7                int cur = -a;
+8                int peak = dq.peekLast();
+9                if (cur == peak) { // boom
+10                    dq.pollLast();
+11                    alive = false;
+12                } else if (cur < peak) { // boom
+13                    alive = false;
+14                } else {
+15                    dq.pollLast();
+16                }
+17            }
+18            if (alive) {
+19                dq.offerLast(a);
+20            }
+21        }
+22        // build res 
+23        int n = dq.size();
+24        int[] res = new int[n];
+25        for (int i = 0; i < n; i++) {
+26            res[i] = dq.pollFirst();
+27        }
+28        return res;
+29
+30
+31        
+32    }
+33} 
+34/**
+35indices: relative position in space
+36absolute value: size
+37sign: dirs 
+38 + right; 
+39 - left
+40
+41rules:
+421. If two asteroids meet, the smaller(size) one will explode.
+432. same size, both boom
+443. ++ / -- / -+ nothing happened -> +- they will boom
+45
+46[3,5,-6,2,-1,4]​​​​​​​
+47
+48-6,2,4
+49
+50stack to store the num in  asteroids
+51for loop: if the peak and the num have the same directions, the size check which one will be added into the deque based on the rules;
+52
+53turn the dq into int[] -> res
+54
+55 */
