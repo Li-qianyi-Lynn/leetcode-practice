e524b35c463add33bcc7f877cc3c772e68de92db
Qianyi Li <li.qianyi@northeastern.edu>
2026-04-13 14:37:02 -0700

    Add solution for Guess Number Higher or Lower

---
+++ LeetCode/Guess-Number-Higher-or-Lower/Solution.java
@@ -0,0 +1,12 @@
+public int guessNumber(int n) {
+    int low = 1;
+    int high = n;
+    while (low < high) {
+        int mid = (low + high) >>> 1;
+        if (guess(mid) <= 0)
+            high = mid;
+        else
+            low = mid + 1;
+    }
+    return low;
+}
