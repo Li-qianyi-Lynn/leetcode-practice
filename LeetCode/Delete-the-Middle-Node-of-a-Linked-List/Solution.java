ba80822cdbfa3929821268b9f898bfd0c21c4c69
Qianyi Li <li.qianyi@northeastern.edu>
2026-04-13 21:38:44 -0700

    Add solution for Delete the Middle Node of a Linked List

--- LeetCode/Delete-the-Middle-Node-of-a-Linked-List/Solution.java
+++ LeetCode/Delete-the-Middle-Node-of-a-Linked-List/Solution.java
@@ -10,22 +10,36 @@
 10 */
 11class Solution {
 12    public ListNode deleteMiddle(ListNode head) {
-13        if (head.next == null) {
-14            return null;
-15
+13        // edge case
+14        if (head.next == null) {
+15            return null;
 16        }
-17        
-18        ListNode slow = head;
-19        ListNode fast = head;
-20        ListNode pre = head;
-21        while (fast != null && fast.next != null) {
-22            pre = slow;
-23            slow = slow.next;
-24            fast = fast.next.next;
-25        }
-26        ListNode nex = slow.next;
-27        pre.next = nex;
-28        return head;
-29        
-30    }
-31}
+17
+18        // step1: get the middle node -> pre ,next
+19        ListNode slow = head;
+20        ListNode fast = head;
+21        ListNode pre = head;
+22        while (fast != null && fast.next != null) {
+23            pre = slow;
+24            slow = slow.next;
+25            fast = fast.next.next;
+26        }
+27        // connext pre and next
+28        ListNode nex = slow.next;
+29        pre.next = nex;
+30        return head;
+31        
+32    }
+33}
+34/**
+35input： head of a linkedlist
+36output: head of a linkedlist(delete the middle node)
+37to find the middle node and connect the first part with the second part
+38
+39prev
+40next
+41
+42
+43
+44
+45 */
