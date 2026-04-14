306d65e206bafd3e303f2e8b7dd9c21cb3149808
Qianyi Li <li.qianyi@northeastern.edu>
2026-04-13 22:17:44 -0700

    Add solution for Maximum Twin Sum of a Linked List

--- LeetCode/Maximum-Twin-Sum-of-a-Linked-List/Solution.java
+++ LeetCode/Maximum-Twin-Sum-of-a-Linked-List/Solution.java
@@ -10,42 +10,49 @@
 10 */
 11class Solution {
 12    public int pairSum(ListNode head) {
-13        ListNode slow = head;
-14        ListNode fast = head;
-15        while (fast != null && fast.next != null) {
-16            slow = slow.next;
-17            fast = fast.next.next;
-18        }
-19        // slow : second half new head
-20        ListNode newHead = reverse(slow); 
-21        int res = 0;
-22        while (newHead != null) {
-23            int curSum = head.val + newHead.val;
-24            res = Math.max(res,curSum);
-25            head = head.next;
-26            newHead = newHead.next;
-27        }
-28        return res;
-29
-30        
-31    }
-32    private ListNode reverse(ListNode head) {
-33        ListNode pre = null;
-34        ListNode cur = head;
-35        while (cur != null) {
-36            ListNode nex = cur.next;
-37            cur.next = pre;
-38            pre = cur;
-39            cur = nex;
-40        }
-41        return pre;
-42    }
-43}
-44/**
-45
-467 6 5  4 <- 10 <- 11
-471. cut off
-482. reverse 
-493. calculate
-50
-51 */
+13        // step1:cut off the linkedlist in 2 parts -> slow and fast pointer
+14        ListNode slow = head;
+15        ListNode fast = head;
+16        int res = 0;
+17        while (fast != null && fast.next != null) {
+18            slow = slow.next;
+19            fast = fast.next.next;
+20        }
+21        // slow will be a new head of the second part 
+22        // step2: reverse the second part 
+23        ListNode newHead = reverse(slow);
+24        // step3: iterate the two list at the same time -> update the res
+25        // 5->4 1->2
+26        while (newHead != null) {
+27            int curSum = head.val + newHead.val;
+28            res = Math.max(curSum, res);
+29            head = head.next;
+30            newHead = newHead.next;
+31        }
+32        return res;
+33
+34        
+35    }
+36
+37    private ListNode reverse(ListNode node) {
+38        ListNode pre = null;
+39        ListNode cur = node;
+40        while (cur != null) {
+41            ListNode nex = cur.next;
+42            cur.next = pre;
+43            pre = cur;
+44            cur = nex;
+45        }
+46        return pre; // the new head
+47
+48    }
+49}
+50/**
+51input: head of a linkedlist
+52output: maximum twin sum of the linked list.
+53
+541. cut off the linkedlist in 2 parts -> slow and fast pointer
+552. reverse the second part 
+563. iterate the two list at the same time -> update the res
+57
+58 */
