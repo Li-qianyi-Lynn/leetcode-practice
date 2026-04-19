8c34e2a9425396a15839b0991d5121f5c577b903
Qianyi Li <li.qianyi@northeastern.edu>
2026-04-18 22:03:19 -0700

    Add solution for Nearest Exit from Entrance in Maze

--- LeetCode/Nearest-Exit-from-Entrance-in-Maze/Solution.java
+++ LeetCode/Nearest-Exit-from-Entrance-in-Maze/Solution.java
@@ -1,21 +1,9 @@
-class Solution:
-    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        ROWS, COLS = len(maze), len(maze[0])
-        r,c = entrance
-        queue = deque([(r,c,0)])
-        visited = set([(r,c)])
-        
-        while queue:
-            r,c,d = queue.popleft()
-            
-            if d != 0 and (r + 1 == ROWS or r - 1 == -1 or c + 1 == COLS or c - 1 == -1):
-                return d
-            
-            for dr,dc in [(0,1),(1,0),(0,-1),(-1,0)]:
-                nr,nc = r+dr, c+dc
-                
-                if ROWS > nr >= 0 <= nc < COLS and maze[nr][nc] != '+' and (nr,nc) not in visited:
-                    visited.add((nr,nc))
-                    queue.append((nr,nc,d+1))
-        
-        return -1
+0 (start)	|1			|3					|7				|15
+----------------------------------------------------------------
+2			|4,5		|8,9,11				|16,17,19,23	|
+----------------------------------------------------------------
+6			|10,12,13	|18,20,21,24,25,27	|				|
+----------------------------------------------------------------
+14			|22,26,28,29|					|				|
+----------------------------------------------------------------
+30			|			|					|				|
