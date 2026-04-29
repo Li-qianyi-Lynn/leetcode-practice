65c6ba9f6c9650d0d3157cf444cb582037b855b2
Qianyi Li <li.qianyi@northeastern.edu>
2026-04-29 00:55:11 -0700

    Add solution for Letter Combinations of a Phone Number

--- LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.java
+++ LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.java
@@ -1,52 +1,49 @@
 1class Solution {
-2    
-3
-4    public List<String> letterCombinations(String digits) {
-5        Map<Character, String> map = new HashMap<>();
-6        map.put('2', "abc");
-7        map.put('3', "def");
-8        map.put('4', "ghi");
-9        map.put('5', "jkl");
-10        map.put('6', "mno");
-11        map.put('7', "pqrs");
-12        map.put('8', "tuv");
-13        map.put('9', "wxyz");
-14        List<String> res = new ArrayList<>();
-15        StringBuilder sb = new StringBuilder();
-16        backtracking(map, sb, digits, res, 0); //todo
-17
-18        return res;     
-19    }
-20
-21    private void backtracking(Map<Character, String> map, StringBuilder cur, String digits,List<String>res, int index) {
-22        //base case
-23        if (cur.length() == digits.length()) {
-24            res.add(cur.toString());
-25            return;
-26        }
-27
-28        String temp = map.get(digits.charAt(index));
+2    public List<String> letterCombinations(String digits) {
+3        List<String> res = new ArrayList<>();
+4        Map<Character, String> map = new HashMap<>();
+5        map.put('2',"abc");
+6        map.put('3',"def");
+7        map.put('4',"ghi");
+8        map.put('5',"jkl");
+9        map.put('6', "mno");
+10        map.put('7', "pqrs");
+11        map.put('8',"tuv");
+12        map.put('9',"wxyz");
+13        StringBuilder sb = new StringBuilder();
+14        backtracking(res, sb, digits, 0, map);
+15        return res;
+16           
+17    }
+18
+19    private void backtracking(List<String> res, StringBuilder sb, String digits, int idx, Map<Character, String> map) {
+20        // base case
+21        if (sb.length() == digits.length()) {
+22            res.add(sb.toString());
+23            return;
+24
+25        }
+26
+27        // rules
+28        String temp = map.get(digits.charAt(idx));
 29        for (int i = 0; i < temp.length(); i++) {
-30            cur.append(temp.charAt(i));
-31            backtracking(map, cur, digits, res, index+1);
-32            cur.deleteCharAt(cur.length()-1);
+30            sb.append(temp.charAt(i));
+31            backtracking(res, sb, digits, idx+1, map);
+32            sb.deleteCharAt(sb.length()-1);
 33
-34        }
-35
+34
+35        }
 36    }
 37}
-38
-39/**
-40map: Character -> String
-41iterate digits, for each digits, get the value
-42backtrack()
-431. base case: sb.size() == digits.length(); add cur sb to res;
-442. get mappings string
-453. iterate mapping string
-46    append charAt(i)
-47    backtrack() next num
-48    deleteCharAt(len-1);
-49
-50
-51
-52 */
+38/**
+39hashmap
+40
+41int: String
+42
+43backtracking
+44
+45
+46
+47
+48
+49 */
