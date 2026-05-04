b7ba7a5c67ac9e217acffb078a28a7863b2713c6
Qianyi Li <li.qianyi@northeastern.edu>
2026-05-04 14:57:47 -0700

    Add solution for Letter Combinations of a Phone Number

--- LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.java
+++ LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.java
@@ -1,49 +1,45 @@
 1class Solution {
-2    public List<String> letterCombinations(String digits) {
-3        List<String> res = new ArrayList<>();
-4        Map<Character, String> map = new HashMap<>();
-5        map.put('2',"abc");
-6        map.put('3',"def");
-7        map.put('4',"ghi");
-8        map.put('5',"jkl");
-9        map.put('6', "mno");
-10        map.put('7', "pqrs");
-11        map.put('8',"tuv");
-12        map.put('9',"wxyz");
-13        StringBuilder sb = new StringBuilder();
-14        backtracking(res, sb, digits, 0, map);
-15        return res;
-16           
-17    }
-18
-19    private void backtracking(List<String> res, StringBuilder sb, String digits, int idx, Map<Character, String> map) {
-20        // base case
-21        if (sb.length() == digits.length()) {
-22            res.add(sb.toString());
-23            return;
-24
-25        }
-26
-27        // rules
-28        String temp = map.get(digits.charAt(idx));
-29        for (int i = 0; i < temp.length(); i++) {
-30            sb.append(temp.charAt(i));
-31            backtracking(res, sb, digits, idx+1, map);
-32            sb.deleteCharAt(sb.length()-1);
-33
-34
-35        }
-36    }
-37}
-38/**
-39hashmap
+2    List<String> res = new ArrayList<>();
+3
+4    public List<String> letterCombinations(String digits) {
+5        Map<Character, String> map = new HashMap<>();
+6        map.put('2',"abc");
+7        map.put('3',"def");
+8        map.put('4',"ghi");
+9        map.put('5',"jkl");
+10        map.put('6',"mno");
+11        map.put('7',"pqrs");
+12        map.put('8',"tuv");
+13        map.put('9',"wxyz");
+14
+15        int idx = 0;
+16        StringBuilder sb = new StringBuilder();
+17        backtracking(digits,0,map,sb);
+18        return res;
+19        
+20    }
+21
+22    private void backtracking(String digits, int idx, Map<Character, String> map, StringBuilder sb) {
+23        //base case
+24        if (idx == digits.length()) {
+25            res.add(sb.toString());
+26            return;
+27        }
+28
+29        // recursive rules
+30        char[] letters = map.get(digits.charAt(idx)).toCharArray();
+31        for (int i = 0; i < letters.length; i++) {
+32            sb.append(letters[i]);
+33            backtracking(digits, idx+1, map,sb);
+34            sb.deleteCharAt(sb.length()-1);
+35
+36        }
+37
+38    }
+39}
 40
-41int: String
+41/**
 42
 43backtracking
 44
-45
-46
-47
-48
-49 */
+45 */
