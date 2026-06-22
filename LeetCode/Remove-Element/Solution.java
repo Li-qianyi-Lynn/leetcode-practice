d7184abb505553d9bb3480c3decbbb83effc3680
Qianyi Li <li.qianyi@northeastern.edu>
2026-06-22 15:10:45 -0700

    Add solution for Remove Element

--- LeetCode/Remove-Element/Solution.java
+++ LeetCode/Remove-Element/Solution.java
@@ -3,18 +3,28 @@
 3        if (nums.length == 0) {
 4            return 0;
 5        }
-6        int r = nums.length-1;
-7        int l = 0;
-8        while (l <= r) {
-9            if (nums[l] == val) {
-10                nums[l] = nums[r];
-11                r--;
-12
-13            } else {
-14                l++;
-15            }
-16        }
-17        return l;
-18        
-19    }
-20}
+6
+7        int s = 0;
+8        for (int f = 0; f < nums.length; f++) {
+9            if (nums[f] != val) {
+10                nums[s] = nums[f];
+11                s++;
+12            }
+13        }
+14        return s;
+15        
+16    }
+17}
+18/**
+19
+20nums 
+21int val
+22
+23output: int  the number of elements in nums which are not equal to val
+24high level idea: quick fast pointer
+25
+26[0,1,3,0,4,0,4,2]
+27               f           
+28           s
+29
+30 */
