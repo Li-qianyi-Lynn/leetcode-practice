90a8ae0fcdf1ec9cad05c28b7c896a28200bb1c9
Qianyi Li <li.qianyi@northeastern.edu>
2026-06-22 17:24:17 -0700

    Add solution for Majority Element

--- LeetCode/Majority-Element/Solution.java
+++ LeetCode/Majority-Element/Solution.java
@@ -4,31 +4,24 @@
 4            return nums[0];
 5
 6        }
-7        int count = 1;
-8        int res = nums[0];
+7        int res = nums[0];
+8        int times = 1;
 9        for (int i = 1; i < nums.length; i++) {
-10            if (count == 0) {
+10            if (times == 0) {
 11                res = nums[i];
-12                count = 1;
-13            } else if (nums[i] == res) {
-14                count++;
-15
-16            } else {
-17                count --;
-18            }
-19
-20            if (count > nums.length / 2) {
-21                break;
+12                times = 1;
+13                continue;
+14
+15            } 
+16            if (nums[i] == res) {
+17                times++;
+18
+19            } else {
+20                times--;
+21            }
 22
-23            }
-24        }
-25        return res;
-26
-27        
-28    }
-29}
-30/**
-31
-32
-33
-34 */
+23        }
+24        return res;
+25        
+26    }
+27}
