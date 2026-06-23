341053a64c80b9a305225fe0af72329bfb9f2e6a
Qianyi Li <li.qianyi@northeastern.edu>
2026-06-22 18:09:47 -0700

    Add solution for Rotate Array

--- LeetCode/Rotate-Array/Solution.java
+++ LeetCode/Rotate-Array/Solution.java
@@ -1,42 +1,43 @@
 1class Solution {
 2    public void rotate(int[] nums, int k) {
 3        if (nums.length == 1 || k == 0) {
-4            return;   
+4            return;
 5        }
-6
-7        // get times
-8        int len = nums.length;
-9        k = k % len;
-10        if (k == 0) {
-11            return;
-12
-13        }
-14        reverse(nums, 0, len-1);
-15        reverse(nums, 0, k-1);
-16        reverse(nums, k, len-1);
+6        int len = nums.length;
+7        k = k % len;
+8        if (k == 0) {
+9            return;
+10        }
+11
+12        reverse(nums, 0, len-1);
+13        reverse(nums, 0, k-1);
+14        reverse(nums, k, len-1);
+15        
+16    }
 17
-18
-19    }
-20
-21    private void reverse(int[] nums, int l, int r) {
-22        while (l < r) {
-23            int temp = nums[l];
-24            nums[l] = nums[r];
-25            nums[r] = temp;
-26            l++;
-27            r--;
-28        }
-29
-30    }
-31}
+18    private void reverse(int[] nums, int start, int end) {
+19        if (nums.length == 0) {
+20            return;
+21        }
+22
+23        int temp = 0;
+24        while (start < end) {
+25            temp = nums[start];
+26            nums[start] = nums[end];
+27            nums[end] = temp;
+28            start++;
+29            end--;
+30        }
+31
 32
-33
-34/**
-35[1,2,3,4,｜ 5,6,7]
+33    }
+34}
+35/**
 36
-37reverse: 7 6 5 4 3 2 1
-38reverse first k 0 - k-1
-39reverse after k k - n-1
+37update k 
+38
+39reverse 3 times
 40
-41
-42 */
+417654321
+42
+43 */
