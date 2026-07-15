343e862414fba66b19270449cad01c949c30cc85
Qianyi Li <li.qianyi@northeastern.edu>
2026-07-14 18:25:18 -0700

    Add solution for Combination Sum

--- LeetCode/Combination-Sum/Solution.java
+++ LeetCode/Combination-Sum/Solution.java
@@ -1,61 +1,48 @@
 1class Solution {
-2    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-3        Arrays.sort(candidates);
-4        List<List<Integer>> res = new ArrayList<>();
-5        List<Integer> cur = new ArrayList<>();
-6        int sum = 0; // cur sum
-7        backtracking(candidates, target, sum, res, cur, 0); //todo
-8        return res;
-9        
-10    }
-11
-12    private void backtracking(int[] candidates, int target, int sum, List<List<Integer>> res, List<Integer> cur, int index) { //start
-13        // base case 
-14        if (sum == target) { // we find one possible List<Integer>
-15            res.add(new ArrayList<>(cur));
-16            return;
-17
-18        }
-19
-20        // if (sum > target) { // not meet the requirement
-21        //     return;
-22
-23        // }
-24
+2    private List<List<Integer>> res = new ArrayList<>();
+3    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+4        Arrays.sort(candidates);
+5        List<Integer> list = new ArrayList<>();
+6        if (candidates.length == 0) {
+7            return res;
+8        }
+9
+10        backtracking(candidates, target, list, 0, 0);
+11        return res;
+12        
+13    }
+14
+15    private void backtracking (int[] candidates, int target, List<Integer> list, int idx, int sum) {
+16        //base case
+17        if (target == sum) {
+18            res.add(new ArrayList<>(list));
+19            return;
+20        }
+21
+22        if (sum > target) {
+23            return;
+24        }
 25
-26        for (int i = index; i < candidates.length; i++ ) {
-27            if (sum > target) {
-28                break;
-29            }
-30            cur.add(candidates[i]);
-31            // sum += candidates[i];
-32            backtracking(candidates, target, sum + candidates[i], res, cur, i);
-33            // sum -= candidates[i];
-34            cur.remove(cur.size()-1);
-35
-36        }
+26
+27        //recursive rules
+28        for (int i = idx; i < candidates.length; i++) {
+29          
+30            list.add(candidates[i]);
+31            backtracking(candidates, target, list, i, sum + candidates[i]);
+32            list.remove(list.size()-1);
+33       
+34
+35        }
+36
 37
 38    }
 39}
 40
 41/**
-42delete duplication 
-43we can use one num multiple times
+42backtracking
+43
 44
-45[2, 3, 5]
-46[2,3,9,6,5,4]
+45
+46
 47
-48get one int from the candidates
-49[2, 2, 2, 2] the sum == target  one res
-50[2, 3, 3 ] one res
-51
-52recursion (backtracking)
-53add num - continue - remove
-54
-55base case:
-561. the sum == target return  
-572. iterate the candidates 
-583. add num - continue - remove
-59
-60
-61 */
+48 */
