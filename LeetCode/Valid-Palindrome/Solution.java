de04dcd1c2e2e7d81e0126288dc854f2795fe32e
Qianyi Li <li.qianyi@northeastern.edu>
2026-07-27 16:40:23 -0700

    Add solution for Valid Palindrome

--- LeetCode/Valid-Palindrome/Solution.java
+++ LeetCode/Valid-Palindrome/Solution.java
@@ -1,45 +1,48 @@
 1class Solution {
 2    public boolean isPalindrome(String s) {
-3        if (s.length() == 1) {
-4            return true;
-5        
+3        // edge case: 
+4        if (s.length() <= 1) {
+5            return true;
 6        }
-7        
-8
-9        int r = s.length()-1;
-10        int l = 0;
-11
-12        while (l < r) {
-13            if (l < r && alphaNum(s.charAt(l)) == false ) {
-14                l++;
-15                continue;
-16                }
-17            if (l < r && alphaNum(s.charAt(r)) == false ) {
-18                r--;
-19                continue;
-20                }
-21
-22            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
-23                return false;
-24
-25            }
-26
-27            l++;
-28            r--;
-29
-30        }
-31        return true;
-32       
-33        
-34    }
-35
-36    private boolean alphaNum(char c) {
-37        if (c >= '0' && c <= '9') { return true;}
-38        if (c >= 'A' && c <= 'Z') { return true;}
-39        if (c >= 'a' && c <= 'z') { return true;}
+7
+8        String spre = s.toLowerCase();
+9
+10        // logic two pointer
+11        int l = 0;
+12        int r = spre.length() -1;
+13        while (l < r) {
+14            // char cl = spre.charAt(l);
+15            // char cr = spre.charAt(r);
+16            //preprocess: space/ , . skip
+17            while (l < r && !Character.isLetterOrDigit(spre.charAt(l))) { // todo 
+18                l++;
+19            }
+20
+21            while (l < r && !Character.isLetterOrDigit(spre.charAt(r))) { // todo 
+22                r--;
+23            }
+24        
+25            if (spre.charAt(r)!= spre.charAt(l)) {
+26                return false;
+27            }
+28            l++;
+29            r--;
+30
+31        }
+32        return true; 
+33    }
+34}
+35/**
+36
+37palindrome:  lowercase , remove all non-alnu chars, the same forward and backward order
+38input: String s
+39output: boolean
 40
-41        return false;
-42
-43        
-44    }
-45}
+41amanaplanacanalpanama
+42|
+43                    |
+44
+451. pre process split
+462. two pointer
+47
+48 */
