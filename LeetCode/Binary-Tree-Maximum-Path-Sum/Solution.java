d6f71f9f04f34dbb4a47b05fab144f90fecd7908
Qianyi Li <li.qianyi@northeastern.edu>
2026-07-28 00:43:37 -0700

    Add solution for Binary Tree Maximum Path Sum

--- LeetCode/Binary-Tree-Maximum-Path-Sum/Solution.java
+++ LeetCode/Binary-Tree-Maximum-Path-Sum/Solution.java
@@ -1,3 +1,53 @@
-left_gain = max(gain_from_left_subtree, 0)
-right_gain = max(gain_from_right_subtree, 0)
-gain_from_subtree = max(left_gain, right_gain) + root.val
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    int maxV = Integer.MIN_VALUE;
+18
+19
+20    public int maxPathSum(TreeNode root) {
+21        if (root == null) {
+22            return 0;
+23        }
+24        maxGain(root);
+25        
+26        return maxV;
+27        
+28    }
+29
+30    private int maxGain(TreeNode node) {
+31        if (node == null) {
+32            return 0;
+33        }
+34        int leftGain = Math.max(maxGain(node.left),0);
+35        int rightGain = Math.max(maxGain(node.right),0);
+36
+37        maxV = Math.max(maxV, (node.val + leftGain + rightGain));
+38        return node.val + Math.max(leftGain,rightGain);
+39
+40    }
+41}
+42/**
+43
+44helper:
+45update max, pass child max to parent node
+46
+47curmax = node.val + leftmax + rightmax
+48compare with max
+49
+50
+51
+52
+53 */
