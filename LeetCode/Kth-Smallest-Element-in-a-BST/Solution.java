a0ee422444213551bc14f50ef74d28c95aa58e8e
Qianyi Li <li.qianyi@northeastern.edu>
2026-07-29 11:51:20 -0700

    Add solution for Kth Smallest Element in a BST

--- LeetCode/Kth-Smallest-Element-in-a-BST/Solution.java
+++ LeetCode/Kth-Smallest-Element-in-a-BST/Solution.java
@@ -1,40 +1,43 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int kthSmallest(TreeNode root, int k) {
-        int[] count = new int[1];
-        int[] res = new int[1];
-        inorder(root, k, count, res);
-        return res[0];
-    }
-
-    private void inorder(TreeNode root, int k, int[] count, int[] res) {
-        if (root == null) return;
-
-        inorder(root.left, k, count, res);
-        count[0] ++;
-        if (count[0] == k) {
-            res[0] = root.val;
-            return;
-
-        }
-        inorder(root.right, k, count, res);
-
-    }
-}
-
-// time complexity: O(h+k) h: height of the tree
-// space complexity: O(h) depth of the stack
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    private int count;
+18    private int res;
+19    public int kthSmallest(TreeNode root, int k) {
+20        count = 0;
+21        res = 0;
+22
+23        inorder(root, k);
+24        return res;
+25    }
+26
+27    private void inorder(TreeNode root, int k) {
+28        if (root == null) return;
+29
+30        inorder(root.left, k);
+31        count++;
+32        if (count == k) {
+33            res = root.val;
+34            return;
+35
+36        }
+37        inorder(root.right, k);
+38
+39    }
+40}
+41
+42// time complexity: O(h+k) h: height of the tree
+43// space complexity: O(h) depth of the stack
