b41ac52f72964f51d3b3f02891bd8c741bb746fd
Qianyi Li <li.qianyi@northeastern.edu>
2026-07-29 21:02:43 -0700

    Add solution for Kth Smallest Element in a BST

--- LeetCode/Kth-Smallest-Element-in-a-BST/Solution.java
+++ LeetCode/Kth-Smallest-Element-in-a-BST/Solution.java
@@ -1,43 +1,22 @@
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    private int count;
-18    private int res;
-19    public int kthSmallest(TreeNode root, int k) {
-20        count = 0;
-21        res = 0;
-22
-23        inorder(root, k);
-24        return res;
-25    }
-26
-27    private void inorder(TreeNode root, int k) {
-28        if (root == null) return;
-29
-30        inorder(root.left, k);
-31        count++;
-32        if (count == k) {
-33            res = root.val;
-34            return;
-35
-36        }
-37        inorder(root.right, k);
-38
-39    }
-40}
-41
-42// time complexity: O(h+k) h: height of the tree
-43// space complexity: O(h) depth of the stack
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        int position = 0;
+        return kthSmallestRec(root, k, position)->val;
+    }
+    TreeNode* kthSmallestRec(TreeNode* node, int k, int& position){
+        if(node == NULL){ return node;}
+        
+        TreeNode* left = kthSmallestRec(node->left, k, position);
+        if(left != NULL){
+            return left;
+        }
+        
+        position++;
+        if(position == k){
+            return node;
+        }
+        
+        return kthSmallestRec(node->right, k, position);
+    }
+};
