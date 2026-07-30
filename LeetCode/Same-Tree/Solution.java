94f940d2c00dc68c953c74fca871573ac8ae1713
Qianyi Li <li.qianyi@northeastern.edu>
2026-07-29 21:38:18 -0700

    Add solution for Same Tree

--- LeetCode/Same-Tree/Solution.java
+++ LeetCode/Same-Tree/Solution.java
@@ -1,16 +1,38 @@
-[1,2]
-[1,null,2]
-[1]
-[2]
-[1,null,2]
-[1,2]
-[1,null,2]
-[1,null,3]
-[1,null,null]
-[1,null,null]
-[]
-[]
-[1]
-[1,null,2]
-[1,null,2]
-[1]
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean isSameTree(TreeNode p, TreeNode q) {
+18        // base case
+19        if (p == null && q == null) {
+20            return true;
+21        }
+22
+23        if (p == null || q == null) {
+24            return false;
+25        }
+26        if (p. val != q.val) {
+27            return false;
+28
+29        }
+30
+31        // divided
+32        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+33
+34
+35
+36        
+37    }
+38}
