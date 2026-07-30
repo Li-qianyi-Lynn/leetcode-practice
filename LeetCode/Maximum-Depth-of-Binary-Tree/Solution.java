7092e8cfdf8a7073c3cb403a7ba98b64f4605bbc
Qianyi Li <li.qianyi@northeastern.edu>
2026-07-29 20:51:17 -0700

    Add solution for Maximum Depth of Binary Tree

--- LeetCode/Maximum-Depth-of-Binary-Tree/Solution.java
+++ LeetCode/Maximum-Depth-of-Binary-Tree/Solution.java
@@ -1,42 +1,8 @@
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public int maxDepth(TreeNode root) {
-18        if (root == null) return 0;
-19        int depth = 0;
-20        Deque<TreeNode> deque = new LinkedList<>();
-21        deque.offerLast(root);
-22        while (!deque.isEmpty()) {
-23            depth++;
-24            int size = deque.size();
-25            for (int i =1; i <= size; i++) {
-26                TreeNode poll = deque.pollFirst();
-27                if (poll.left != null) {
-28                    deque.offerLast(poll.left);
-29                }
-30                if (poll.right != null) {
-31                    deque.offerLast(poll.right);
-32                }
-33
-34            }
-35
-36        }
-37        return depth;
-38   
-39    }
-40
-41  
-42}
+[3,9,20,null,null,15,7]
+[1,null,2]
+[0,0,0,0,null,null,0,null,null,null,0]
+[1,2,null,3,null,4]
+[1,null,2,null,3,null,4]
+[1,2,3,4,5,null,null]
+[1]
+[]
