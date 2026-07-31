4aa9831083c1a8b771145fb706fc958122c3eca1
Qianyi Li <li.qianyi@northeastern.edu>
2026-07-30 22:23:29 -0700

    Add solution for Top K Frequent Elements

--- LeetCode/Top-K-Frequent-Elements/Solution.java
+++ LeetCode/Top-K-Frequent-Elements/Solution.java
@@ -1,33 +1,50 @@
-class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        int[] res = new int[k];
-        // map: store num(key) and its frequency(value)
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        //put K-V pait to the pq, and do the comperation
-        // min heap: heap topwill store the least frequent element, if the size is larger than k, we can just pop out he top
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        for(Map.Entry<Integer, Integer> pair: map.entrySet()) {
-            int freq = pair.getValue();
-            int num = pair.getKey();
-            int[] element = new int[]{num, freq};
-            pq.offer(element);
-
-            if (pq.size() > k) {
-                pq.poll();
-
-            }
-
-        }
-
-        // add num to the res list;
-        for (int i = 0; i < k; i++) {
-            res[i] = pq.poll()[0];
-
-        }
-        return res;
-        
-    }
-}
+1class Solution {
+2    public int[] topKFrequent(int[] nums, int k) {
+3        // pruning nums.length == 1 return nums
+4        if (nums.length == 1) {
+5            return nums;
+6        }
+7        //map: num -> freq
+8        HashMap<Integer, Integer> map = new HashMap<>();
+9        for (int num : nums) {
+10            map.put(num, map.getOrDefault(num, 0)+1);
+11        }
+12
+13        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]); //todo
+14        // iterate the map. put num, freq into pq
+15        for (Map.Entry<Integer, Integer> entry : map.entrySet()) { 
+16            int freq = entry.getValue(); 
+17            int num = entry.getKey();
+18
+19            if (pq.size() < k) {
+20                pq.offer(new int[]{freq,num});
+21
+22            } else {
+23                if (pq.peek()[0] < freq) {
+24                    pq.poll();
+25                    pq.offer(new int[]{freq,num});
+26                }
+27            }
+28        }
+29        //[4,1],[4,2],[2,3]
+30        // build res
+31        int[] res = new int[k];
+32        for (int i = 0; i < k; i++) {
+33            res[i] = pq.poll()[1];
+34
+35        }
+36        return res;     
+37    }
+38}
+39
+40/**
+41input： int[] nums, int k
+42putput: k most frequent elements.  int[] 
+43
+441->4
+452->4
+463->2
+47
+48map: num -> freq
+49pq minpq : compare freq 
+50 */
