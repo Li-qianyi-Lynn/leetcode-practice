b92ceacb6de0f7af922e224a68f107f3241cf2ef
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-03 01:20:49 -0700

    Add solution for Design Add and Search Words Data Structure

--- LeetCode/Design-Add-and-Search-Words-Data-Structure/Solution.java
+++ LeetCode/Design-Add-and-Search-Words-Data-Structure/Solution.java
@@ -6,62 +6,61 @@
 6class WordDictionary {
 7    private TrieNode root;
 8
-9    public WordDictionary() {
-10        root = new TrieNode();
-11        
-12    }
+9
+10
+11    public WordDictionary() {
+12        root = new TrieNode();
 13    
-14    public void addWord(String word) {
-15        TrieNode node = root;
-16        for (char c : word.toCharArray()) {
-17            int index = c - 'a';
-18            if (node.children[index] == null) {
-19                node.children[index] = new TrieNode();
-20            }
-21            node = node.children[index];
-22
-23        }
-24        node.isEnd = true;
-25        
-26    }
-27    
-28    public boolean search(String word) {
-29        return helper(root, word, 0);
+14        
+15    }
+16    
+17    public void addWord(String word) { // build tree
+18        TrieNode node = root; //pointer to root
+19        for (char c : word.toCharArray()) {
+20            int i = c - 'a';
+21            if (node.children[i] == null) {
+22                node.children[i] = new TrieNode();
+23            }
+24            node = node.children[i];
+25        }
+26        node.isEnd = true;
+27
+28        
+29    }
 30    
-31        
-32    }
-33
-34    private boolean helper(TrieNode node, String word, int index) {
-35        // base case
-36        if (index == word.length()) {
-37            return node.isEnd;
-38
-39        }
-40        if (word.charAt(index) == '.') {
-41            for (int i = 0; i < 26; i++) {
-42                if (node.children[i] != null) {
-43                    if (helper(node.children[i], word, index+1)) {
-44                        return true; //只要有一条通了
-45                    }
-46                }
-47            }
-48            return false;
-49        } else {
-50            int n = word.charAt(index) - 'a';
-51            if (node.children[n] != null) {
-52                return helper(node.children[n], word, index+1);
+31    public boolean search(String word) {// dfs
+32        return dfs(word, 0, root);
+33        
+34    }
+35
+36    private boolean dfs(String word, int idx, TrieNode node) {
+37        //base case
+38        if (idx == word.length()) {
+39            return node.isEnd;
+40        }
+41
+42        char c = word.charAt(idx);
+43        if (c == '.') {
+44            for (int i = 0; i < 26; i++) {
+45                if (node.children[i] != null && dfs(word, idx+1, node.children[i])){
+46                    return true;
+47                }
+48            }
+49            return false;
+50        } else {
+51            if (node.children[c-'a'] != null) {
+52                return dfs(word, idx+1, node.children[c-'a']);
 53
-54            } 
-55            return false; // 没路就false
+54            }
+55            return false;
 56        }
-57       
-58
-59    }
-60}
-61
-62/**
-63 * Your WordDictionary object will be instantiated and called as such:
-64 * WordDictionary obj = new WordDictionary();
-65 * obj.addWord(word);
-66 * boolean param_2 = obj.search(word);
-67 */
+57
+58    }
+59}
+60
+61/**
+62 * Your WordDictionary object will be instantiated and called as such:
+63 * WordDictionary obj = new WordDictionary();
+64 * obj.addWord(word);
+65 * boolean param_2 = obj.search(word);
+66 */
