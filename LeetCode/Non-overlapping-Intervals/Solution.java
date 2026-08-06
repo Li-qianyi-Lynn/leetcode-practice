5367ea0a73ef13d48e753230b6ab51a6dda1787d
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-06 14:34:53 -0700

    Add solution for Non-overlapping Intervals

--- LeetCode/Non-overlapping-Intervals/Solution.java
+++ LeetCode/Non-overlapping-Intervals/Solution.java
@@ -1,2 +1,11 @@
-[[-73,-26],[-65,-11],[-62,-49]]
-[[1,100],[11,22],[1,11],[2,12]]
+class Solution:
+    def eraseOverlapIntervals (self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        prev, i, ans = 0, 1, 0
+        while (i < len(intervals)):
+            if (intervals[i][0] < intervals[prev][1]):
+                ans += 1
+                if (intervals[i][1] < intervals[prev][1]): prev = i
+            else: prev = i
+            i += 1
+        return ans
