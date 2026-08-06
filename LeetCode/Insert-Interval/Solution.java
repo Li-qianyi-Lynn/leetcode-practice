bbccd8e8ba051799e4355ba37b1a6ea40391790c
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-05 19:25:51 -0700

    Add solution for Insert Interval

--- LeetCode/Insert-Interval/Solution.java
+++ LeetCode/Insert-Interval/Solution.java
@@ -2,42 +2,59 @@
 2    public int[][] insert(int[][] intervals, int[] newInterval) {
 3        if (intervals.length == 0) {
 4            return new int[][]{newInterval};
-5        }
-6
-7        List<int[]> res = new ArrayList<>();
-8
-9        int n = intervals.length;
-10        for (int i = 0; i < n; i++) {
-11            if (newInterval != null && intervals[i][0] > newInterval[1]) {
-12                res.add(newInterval);
-13                newInterval = null;
-14                res.add(intervals[i]);
-15            } else if (newInterval == null || intervals[i][1] < newInterval[0]) {
-16                res.add(intervals[i]);
-17            } else {
-18                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-19                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-20            }
-21        }
-22
-23        if (newInterval != null) {
-24            res.add(newInterval);
-25        }
-26        return res.toArray(new int[res.size()][]);
-27        
-28    }
-29}
-30/**
-31con1
-32...[ ] ....
-33[]....
+5
+6        }
+7        int len = intervals.length;
+8        List<int[]> res = new ArrayList<>();
+9
+10        for (int i = 0; i < len; i++) {
+11            if (newInterval == null) {
+12                res.add(intervals[i]);
+13                continue;
+14
+15            }
+16            /**
+17             [][]
+18          []
+19            
+20             */
+21            if (newInterval[1] < intervals[i][0]) {
+22                res.add(newInterval);
+23                newInterval = null;
+24                res.add(intervals[i]);
+25
+26            } else if (newInterval[0] > intervals[i][1]) {
+27                res.add(intervals[i]);
+28
+29            } else if (newInterval != null ) {
+30                newInterval[0] = Math.min(intervals[i][0],newInterval[0]);
+31                newInterval[1] = Math.max(intervals[i][1],newInterval[1]);
+32
+33            }
 34
-35con2
-36...[ ]....[ ]
-37......[ ].
-38
-39con3
-40...[ ]...
-41..[ ]...
-42
-43 */
+35        }
+36        if (newInterval != null) {
+37            res.add(newInterval);
+38        }
+39        
+40        return res.toArray(new int[res.size()][2]);
+41
+42        
+43    }
+44}
+45
+46/**
+47  [][]
+48[]..
+49
+50
+51[]..[]
+52  []
+53
+54
+55 [ ][ ]
+56  [. ]
+57
+58
+59
+60 */
