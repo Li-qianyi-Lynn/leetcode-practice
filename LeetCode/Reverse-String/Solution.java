2a8900a6dfd538722d1ec2ba6595af4d6fe17dc8
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-07 13:48:36 -0700

    Add solution for Reverse String

--- LeetCode/Reverse-String/Solution.java
+++ LeetCode/Reverse-String/Solution.java
@@ -1,17 +1,20 @@
-class Solution {
-    public void reverseString(char[] s) {
-        int i = 0;
-        int j = s.length-1;
-        while (i < j) {
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-            i++;
-            j--;
-
-        }
-
-        
-        
-    }
-}
+1class Solution {
+2    public void reverseString(char[] s) {
+3        if (s.length == 1) {
+4            return;
+5
+6        }
+7        int l = 0;
+8        int r = s.length -1;
+9        while (l < r) {
+10
+11            char temp = s[l];
+12            s[l] = s[r];
+13            s[r] = temp;
+14            l++;
+15            r--;
+16
+17        }
+18    
+19    }
+20}
