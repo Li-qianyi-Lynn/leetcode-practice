ed62ffae4a431853853c92d2ccfea6362650e40d
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-07 17:05:35 -0700

    Add solution for Group Anagrams

--- LeetCode/Group-Anagrams/Solution.java
+++ LeetCode/Group-Anagrams/Solution.java
@@ -1,26 +1,30 @@
 1class Solution {
 2    public List<List<String>> groupAnagrams(String[] strs) {
 3        List<List<String>> res = new ArrayList<>();
-4        Map<String,List<String>> map = new HashMap<>();
-5
-6        for (String s : strs) {
-7            char[] c = s.toCharArray();
-8            Arrays.sort(c);
-9            String key = String.valueOf(c);
-10            map.putIfAbsent(key, new ArrayList<>());
-11            map.get(key).add(s);
-12        }
-13        res.addAll(map.values());
-14        return res;
-15        
-16    }
-17}
-18/**
-19
-20char + num 
-21
-22get the string 
-23eat tae  sorted -> aet: eat tea ate 
-24
-25
-26 */
+4        // edge case
+5        // if (strs.length == 1) {
+6        //     return new ArrayList<>(strs); // todo
+7
+8        // }
+9        int n = strs.length;
+10        HashMap<String, List<String>> map = new HashMap<>();
+11        for (int i = 0; i < n; i++) {
+12            String cur = strs[i];
+13            char[] curChar = cur.toCharArray();
+14            Arrays.sort(curChar);
+15            map.putIfAbsent(String.valueOf(curChar), new ArrayList<>());
+16            map.get(String.valueOf(curChar)).add(cur);
+17        }
+18
+19        // res.add(map.values()); //todo
+20        // return res;
+21        return new ArrayList<>(map.values());
+22        
+23        
+24    }
+25}
+26/**
+27hashmap
+28eat -> [word]
+29
+30 */
