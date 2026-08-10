d7e2d927431b55d1db3bd3f6b82d51a85c4a752f
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-09 23:00:06 -0700

    Add solution for Find Median from Data Stream

--- LeetCode/Find-Median-from-Data-Stream/Solution.java
+++ LeetCode/Find-Median-from-Data-Stream/Solution.java
@@ -1,29 +1,56 @@
-Adding number 41
-MaxHeap lo: [41]           // MaxHeap stores the largest value at the top (index 0)
-MinHeap hi: []             // MinHeap stores the smallest value at the top (index 0)
-Median is 41
-=======================
-Adding number 35
-MaxHeap lo: [35]
-MinHeap hi: [41]
-Median is 38
-=======================
-Adding number 62
-MaxHeap lo: [41, 35]
-MinHeap hi: [62]
-Median is 41
-=======================
-Adding number 4
-MaxHeap lo: [35, 4]
-MinHeap hi: [41, 62]
-Median is 38
-=======================
-Adding number 97
-MaxHeap lo: [41, 35, 4]
-MinHeap hi: [62, 97]
-Median is 41
-=======================
-Adding number 108
-MaxHeap lo: [41, 35, 4]
-MinHeap hi: [62, 97, 108]
-Median is 51.5
+1class MedianFinder {
+2    PriorityQueue<Integer> minHeap;
+3    PriorityQueue<Integer> maxHeap;
+4
+5    public MedianFinder() {
+6        minHeap = new PriorityQueue<>();
+7        maxHeap = new PriorityQueue<>((a,b) -> b - a);
+8
+9    }
+10    
+11    public void addNum(int num) {
+12        maxHeap.offer(num);
+13        minHeap.offer(maxHeap.poll());// 始终保持minHeap >= maxHeap
+14        if (minHeap.size() > maxHeap.size()+1) {
+15            int poll = minHeap.poll();
+16            maxHeap.offer(poll);
+17        }
+18    
+19
+20    }
+21    
+22    public double findMedian() {
+23        double res = 0.0;
+24        if (minHeap.size()!= maxHeap.size()) {
+25            res = minHeap.peek() * 1.0;
+26    
+27        } else {
+28            res = ((long)minHeap.peek() + maxHeap.peek()) / 2.0; // There's a potential integer overflow risk here. If both values are very large, their sum might exceed Integer.MAX_VALUE before being divided by 2
+29
+30        }
+31        return res;
+32        
+33    }
+34}
+35
+36/**
+37 * Your MedianFinder object will be instantiated and called as such:
+38 * MedianFinder obj = new MedianFinder();
+39 * obj.addNum(num);
+40 * double param_2 = obj.findMedian();
+41 */
+42
+43 /**
+44 
+45 maxHeap: 2 1 0 -1 -2 -3 -4 -5
+46 minHeap: 3  4  5  6  7  8  9 
+47 
+48 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9
+49 
+50 all int in minHeap > maxHeap
+51 size 不要差太大
+52
+53 比较size 
+54 如果 == peak1 + peak2 / 2.0
+55 or minHeap.peak()
+56  */
