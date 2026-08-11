57ca6b105aab2a321b3062a3c6df1a0bc8a4bd08
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-11 00:37:56 -0700

    Add solution for Search in Rotated Sorted Array

--- LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
+++ LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
@@ -1,34 +1,50 @@
 1class Solution {
 2    public int search(int[] nums, int target) {
-3        int res = 0;
-4        int r = nums.length-1;
-5        int l = 0;
-6
-7        while (l <= r) {
-8            int mid = l + (r - l)/2;
-9            if (nums[mid] == target) {
-10                return mid;
-11            }
-12
-13            if (nums[l] <= nums[mid]) { // left in order
-14                if (nums[mid] < target || target < nums[l]) {
-15                    l = mid + 1;    
-16                } else {
-17                    r = mid -1;
-18                }
-19                
-20            } else { // right in order
-21                if (nums[mid] > target || target > nums[r]) { // check left
-22                    r = mid -1;
-23
-24                } else {
-25                    l = mid + 1;
-26                }
-27
-28            }
-29
-30        }
-31        return -1;
-32        
-33    }
-34}
+3        int n = nums.length;
+4        int l = 0;
+5        int r = n-1;
+6        while (l <= r) {
+7            int mid = l + (r-l) / 2;
+8            if (nums[mid] == target) {
+9                return mid;
+10
+11            } else if (nums[l] <= nums[mid]) {
+12                if (target > nums[mid] || target < nums[l]) {
+13                    l = mid +1;
+14
+15                } else {
+16                    r = mid -1;
+17                }
+18
+19            } else {
+20                if (target < nums[mid] || target > nums[r]) {
+21                    r = mid -1;
+22
+23                } else {
+24                    l = mid +1;
+25                }
+26
+27            }
+28
+29        }
+30        return -1;
+31        
+32    }
+33}
+34
+35/**
+36for loop O（n）
+37
+38binary search
+398
+40[7,8,0,1,2]
+41     |
+42
+43nums[l] <= nums[mid]: left in order
+44
+45target > nums[mid] || target < nums[l]
+46l = mid+1
+47
+48
+49
+50 */
