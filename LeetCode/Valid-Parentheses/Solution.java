cfa68b4539779afb073389353549cf84dd8fce53
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-11 17:38:37 -0700

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.java
+++ LeetCode/Valid-Parentheses/Solution.java
@@ -1,31 +1,25 @@
 1class Solution {
 2    public boolean isValid(String s) {
-3        Deque<Character> stack = new ArrayDeque<>();
+3        Deque<Character> dq = new ArrayDeque<>();
 4        Map<Character, Character> map = new HashMap<>();
 5        map.put(')','(');
 6        map.put(']','[');
 7        map.put('}','{');
-8        for (char c : s.toCharArray()) {
-9            if (map.containsKey(c) && !stack.isEmpty()) {
-10                char cur = stack.pollLast();
-11                if (cur != map.get(c)) {
-12                    return false;
-13                }
-14            } else {
-15                stack.offerLast(c);
-16
-17            }
-18            
+8
+9        for (char c : s.toCharArray()) {
+10            if (map.containsKey(c) && !dq.isEmpty() ) {
+11                char poll = dq.pollLast();
+12                if (map.get(c) != poll) {
+13                    return false;
+14                }
+15
+16            } else {
+17                dq.offerLast(c);
+18            }
 19        }
-20        return stack.isEmpty();
-21        
-22    }
-23}
-24// stack
-25/**
-26( added to stack
-27) check if has (
-28
-29
-30
-31 */
+20
+21        return dq.isEmpty();
+22
+23        
+24    }
+25}
