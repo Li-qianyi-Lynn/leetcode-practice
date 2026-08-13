74713500a0f589cd94cc3168aedb8d6d92188cc2
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-13 16:15:26 -0700

    Add solution for Course Schedule

--- LeetCode/Course-Schedule/Solution.java
+++ LeetCode/Course-Schedule/Solution.java
@@ -1,71 +1,61 @@
 1class Solution {
 2    public boolean canFinish(int numCourses, int[][] prerequisites) {
-3        if (numCourses == 1 || prerequisites.length == 0) {
-4            return true;
-5
+3        // edge case
+4        if (prerequisites.length == 0) {
+5            return true;
 6        }
 7
-8        int finished = 0;
-9        HashMap<Integer, List<Integer>> map = new HashMap<>();
-10        int[] indegree = new int[numCourses];
-11        for (int[] pres : prerequisites) {
-12            int pre = pres[1];
-13            int course = pres[0];
-14            indegree[course]++;
-15            map.putIfAbsent(pre,new ArrayList<>());
-16            map.get(pre).add(course);
-17
-18        }
-19
-20        Deque<Integer> dq = new ArrayDeque<>();
-21        for (int i = 0; i < numCourses; i++) {
-22            if (indegree[i] == 0) {
-23                dq.offerLast(i);
-24                finished++;
-25            }
-26        }
-27
-28        while (!dq.isEmpty()) {
-29            int cur = dq.pollFirst();
-30            List<Integer> courses = map.get(cur);
-31            if (courses == null) {
-32                continue;
-33
-34            }
-35            for (int i = 0; i < courses.size(); i++) {
-36                int c = courses.get(i);
-37                indegree[c]--;
-38                if (indegree[c] == 0) {
-39                    finished++;
-40                    dq.offerLast(c);
-41                }
-42            }
-43        }
-44
-45        return finished == numCourses;
-46  
+8        int[] indegree = new int[numCourses]; //  how many prerequists
+9        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+10        for (int[] pres : prerequisites) {
+11            int pre = pres[1];
+12            int course = pres[0];
+13            indegree[course]++;
+14            map.putIfAbsent(pre, new ArrayList<>());
+15            map.get(pre).add(course);
+16        }
+17        int finished = 0;
+18        Deque<Integer> dq = new ArrayDeque<>();
+19        for (int i = 0; i < numCourses; i++) {
+20            if (indegree[i] == 0) {
+21                finished ++;
+22                dq.offerLast(i);
+23            }
+24
+25        }
+26
+27        while (!dq.isEmpty()) {
+28            int cur = dq.pollFirst();
+29            if (map.containsKey(cur)) {
+30                List<Integer> list = map.get(cur);
+31                for (int i = 0; i < list.size(); i++ ) {
+32                    int processedCourse = list.get(i);
+33                    indegree[processedCourse]--;
+34                    if (indegree[processedCourse] == 0) {
+35                        finished ++;
+36                        dq.offerLast(processedCourse);
+37                    }
+38                }
+39            }
+40        }
+41        return finished == numCourses;
+42
+43
+44        
+45    }
+46}
 47
-48        
-49    }
-50}
-51
-52/**
-53
-54dependency problem
+48/**
+49indegree table: each course has how many prerequists
+50map: precourse -> unlocked courses Integer -> List<Integer>
+51int finished coures
+52
+53if all precourses of this course finished, add the course id to the dq 
+54dq: take this course as the precourse, check which course can be processed , add it to the dq
 55
-56bfs
+56
 57
-58indegree map: int[] to store each course has how many pres
-59
-60int finished course
-61hashmap pre: course
-62
-63bfs:
-64use deque to store finished courses and update courses num
-65
-66
-67
-68compare finished course num with numCourses
-69
-70
-71 */
+58
+59return finished == numcourse
+60
+61 */
