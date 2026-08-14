0947ca76f4a3c33160798533c9db71a172bb5ea3
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-14 00:00:05 -0700

    Add solution for H-Index

--- LeetCode/H-Index/Solution.java
+++ LeetCode/H-Index/Solution.java
@@ -1,55 +1,54 @@
 1class Solution {
 2    public int hIndex(int[] citations) {
 3        int n = citations.length;
-4
-5        // count[i] = number of papers with i citations
-6        // citations >= n are put into count[n]
-7        int[] count = new int[n + 1];
-8
-9        for (int citation : citations) {
-10            if (citation >= n) {
-11                count[n]++;
-12            } else {
-13                count[citation]++;
-14            }
-15        }
-16
-17        // papers = number of papers with citation >= h
-18        int papers = 0;
-19
-20        for (int h = n; h >= 0; h--) {
-21            papers += count[h];
-22
-23            if (papers >= h) {
-24                return h;
-25            }
-26        }
-27
-28        return 0;
-29    }
-30}
-31/**
-32input: int[]
-33output: int: h-idx
-34            
-35citations = [3,0,6,1,5]
-36             0  1 2 3 4
-37
-38h: 
-391. h <= len of citations 1-5 12345
-402. citations[i] >= h
-41
-42符合h-idx 要求
-43h-idx     understanding
-441         1 paper 1 citation
-452         2 paper each 2 citations
-463         3 paper each 3 citations
+4        
+5        //binary search
+6        int l = 1;
+7        int r = citations.length;
+8        int res = 0;
+9        while (l <= r) {
+10            int mid = l + (r-l)/2; 
+11            //for loop: how many paper meet the requirement
+12            int temp = 0;
+13            for (int p : citations) {
+14                if (p>= mid) { //  citations[i] >= h
+15                    temp++;
+16                }
+17            }
+18            if (temp >= mid) {
+19                res = Math.max(res, mid);
+20                l = mid + 1;
+21            } else {
+22                r = mid - 1;
+23            }
+24        }
+25        return res;
+26
+27        
+28    }
+29}
+30/**
+31input: int[]
+32output: int: h-idx
+33            
+34citations = [3,0,6,1,5]
+35             0  1 2 3 4
+36
+37h: 
+381. h <= len of citations 1-5 12345
+392. citations[i] >= h
+40
+41符合h-idx 要求
+42h-idx     understanding
+431         1 paper 1 citation
+442         2 paper each 2 citations
+453         3 paper each 3 citations
+46
 47
-48
-491-5 inorder -> binary search
-50l = 1
-51r = 5
+481-5 inorder -> binary search
+49l = 1
+50r = 5
+51
 52
 53
-54
-55 */
+54 */
