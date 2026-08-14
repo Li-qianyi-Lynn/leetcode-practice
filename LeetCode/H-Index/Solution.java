4f37b30e09cf9e9938fc4df5c45da15c3eb88417
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-13 23:42:01 -0700

    Add solution for H-Index

--- LeetCode/H-Index/Solution.java
+++ LeetCode/H-Index/Solution.java
@@ -1,54 +1,43 @@
 1class Solution {
 2    public int hIndex(int[] citations) {
-3        int n = citations.length;
-4        
-5        //binary search
-6        int l = 1;
-7        int r = citations.length;
-8        int res = 0;
-9        while (l <= r) {
-10            int mid = l + (r-l)/2; 
-11            //for loop: how many paper meet the requirement
-12            int temp = 0;
-13            for (int p : citations) {
-14                if (p>= mid) { //  citations[i] >= h
-15                    temp++;
-16                }
-17            }
-18            if (temp >= mid) {
-19                res = Math.max(res, mid);
-20                l = mid + 1;
+3        
+4
+5        int l = 0;
+6        int r = citations.length;
+7        int res = 0;
+8        while (l <= r) {
+9            int mid = l + (r-l)/2;
+10            int count = 0;
+11            for (int i = 0; i < citations.length; i++) {
+12                if (citations[i] >= mid) {
+13                    count++;
+14                }
+15
+16            }
+17            if (count >= mid) {
+18                res = Math.max(res,mid);
+19                l = mid+1;
+20
 21            } else {
-22                r = mid - 1;
+22                r = mid-1;
 23            }
 24        }
 25        return res;
-26
-27        
-28    }
-29}
-30/**
-31input: int[]
-32output: int: h-idx
-33            
-34citations = [3,0,6,1,5]
-35             0  1 2 3 4
-36
-37h: 
-381. h <= len of citations 1-5 12345
-392. citations[i] >= h
-40
-41符合h-idx 要求
-42h-idx     understanding
-431         1 paper 1 citation
-442         2 paper each 2 citations
-453         3 paper each 3 citations
-46
-47
-481-5 inorder -> binary search
-49l = 1
-50r = 5
-51
-52
-53
-54 */
+26        
+27    }
+28}
+29/**
+30
+31h: max
+321. h: 0-len
+332. each paper has been cited at least h times
+34
+35binary search
+360. 5
+37check if nums[i] >= mid
+38if yes , count ++
+39
+40check if count >= mid
+41if yes, answer = max mid, l = mid +1
+42if no, r = mid -1
+43 */
