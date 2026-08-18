8bbf388e783ea38a8723b3c94ce2699f95be9091
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-17 22:17:36 -0700

    Add solution for Median of Two Sorted Arrays

--- LeetCode/Median-of-Two-Sorted-Arrays/Solution.java
+++ LeetCode/Median-of-Two-Sorted-Arrays/Solution.java
@@ -1,83 +1,40 @@
 1class Solution {
 2    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-3        int m = nums1.length;
-4        int n = nums2.length;
-5        if (n < m) {
-6            return findMedianSortedArrays(nums2, nums1);
-7        }
-8
-9        // break point
-10        int left = 0;
-11        int right = m;
-12        int totalLeft = (m+n+1)/2;
+3        int n = nums1.length;
+4        int m = nums2.length;
+5        // 这里的 left 和 right 是指合并后的第几个数（从1开始算）
+6        int left = (n + m + 1) / 2;
+7        int right = (n + m + 2) / 2;
+8        
+9        // 将偶数和奇数的情况合并：如果是奇数，left == right；如果是偶数，求平均值
+10        return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) + 
+11                getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;
+12    }
 13
-14        while (left <= right) {
-15            int i = left + (right - left)/2;
-16            int j = totalLeft - i;
-17
-18            if (i > 0 && nums1[i-1] > nums2[j]) {
-19                right = i -1;
-20            } else if (i < m && j > 1 && nums1[i] < nums2[j-1] ) {
-21                left = i+1;
-22            } else {
-23                // 找到分割点了。要找最大值
-24                //left max
-25                int leftMax = Integer.MIN_VALUE;
-26                if (i == 0) {
-27                    leftMax = nums2[j-1];
-28                } else if (j == 0) {
-29                    leftMax = nums1[i-1];
-30                } else {
-31                    leftMax = Math.max(nums1[i-1], nums2[j-1]);
-32                }
-33                
-34                if ((m+ n) % 2 == 1) {
-35                    return leftMax;  
-36                }
-37
-38                int rightMin = Integer.MAX_VALUE;
-39                if (i == m) {
-40                    rightMin = nums2[j];
-41
-42                } else if (j == n) {
-43                    rightMin = nums1[i];
-44
-45                } else {
-46                    rightMin = Math.min(nums1[i], nums2[j]);
-47                }
-48                
-49                return (rightMin + leftMax) / 2.0; 
-50            }
-51
-52        }
-53        return 0.0;   
-54    }
-55}
-56
-57/**
-58[1 | 3].  [2]|
-59在两个数组里切分，i，j使得
-60if j < n && num1[i-1] <= nums2[j]
-61
-62or i < m && nums2[j-1] < nums1[i]
-63
-64【1 2 ｜ 3】
-65
-66找到切分点之后，如果m+n 是奇数，中位数就是切分后左边的最大值；偶数，是左边最大值+ 右边最小值取平均数
-67
-681. 找切分点： j = totalleft - i >= 0 所以我们会希望i 在短数组里取，binary search 来做
-69left = 0;
-70right = m;
-71i = left + (right - left)/2
-72j = （m+ n +1）/ 2 - i
-73num1[i-1] > nums2[j]
-74right = i -1;
-75
-76nums1[i] <  nums2[j-1] 
-77left = i+1
-78
-79
-802. 找左右边最大值
-81
-82
-83 */
+14    /**
+15     * 辅助函数：在两个数组的指定区间内，寻找合并后的第 k 小的元素
+16     */
+17    private int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+18        int len1 = end1 - start1 + 1;
+19        int len2 = end2 - start2 + 1;
+20
+21        // 始终让 len1 <= len2，确保如果有一个数组空了，一定是 nums1
+22        if (len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
+23        
+24        // 边界情况
+25        if (len1 == 0) return nums2[start2 + k - 1]; // nums1 空了，直接在 nums2 找
+26        if (k == 1) return Math.min(nums1[start1], nums2[start2]); // 找最小的，看两个数组头谁小
+27
+28        // 技巧 3: 核心逻辑 - 每次排除 k/2 个元素，防止数组长度不够，所以取 min(长度, k/2)
+29        int i = start1 + Math.min(len1, k / 2) - 1;
+30        int j = start2 + Math.min(len2, k / 2) - 1;
+31
+32        if (nums1[i] > nums2[j]) {
+33            // 说明 nums2 的前半部分（到 j 为止）一定不包含第 k 小，排除掉
+34            return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+35        } else {
+36            // 说明 nums1 的前半部分一定不包含第 k 小，排除掉
+37            return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+38        }
+39    }
+40}
