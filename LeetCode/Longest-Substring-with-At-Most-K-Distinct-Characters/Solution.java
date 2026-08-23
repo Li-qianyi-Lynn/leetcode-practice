099de1235320077cd5ea9bc2cce57b4125506e0a
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-22 22:11:27 -0700

    Add solution for Longest Substring with At Most K Distinct Characters

--- LeetCode/Longest-Substring-with-At-Most-K-Distinct-Characters/Solution.java
+++ LeetCode/Longest-Substring-with-At-Most-K-Distinct-Characters/Solution.java
@@ -1,41 +1,34 @@
-1class Solution {
-2    public int lengthOfLongestSubstringKDistinct(String s, int k) {
-3        if (k == 0) {
-4            return 0;
-5
-6        }
-7        HashMap<Character, Integer> map = new HashMap<>();
-8        int l = 0;
-9        int n = s.length();
-10        int res = 0;
-11        for (int r = 0; r < n; r++) {
-12            map.put(s.charAt(r), map.getOrDefault(s.charAt(r),0)+1);
-13            while (map.size() > k) {
-14                char c = s.charAt(l);
-15                map.put(c,map.get(c)-1);
-16                if (map.get(c) == 0) {
-17                    map.remove(c);
-18                }
-19                l++;
-20            }
-21            res = Math.max(r-l+1, res);
-22        }
-23        return res;
-24        
-25    }
-26}
-27/**
-28
-29sliding window
-30set
-31eceba
-32｜
-33  ｜
-34
-35compare set.size() with k
-36valid: size <= k
-37invalid: size > k
-38move left pointer, update set
-39
-40
-41 */
+1import java.util.HashMap;
+2import java.util.Map;
+3
+4class Solution {
+5    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+6        if (s == null || s.length() == 0 || k == 0) {
+7            return 0;
+8        }
+9
+10        Map<Character, Integer> countMap = new HashMap<>();
+11        int left = 0;
+12        int maxLen = 0;
+13
+14        for (int right = 0; right < s.length(); right++) {
+15            char rChar = s.charAt(right);
+16            countMap.put(rChar, countMap.getOrDefault(rChar, 0) + 1);
+17
+18            // 当不同字符数量超过 k 时，收缩左边界
+19            while (countMap.size() > k) {
+20                char lChar = s.charAt(left);
+21                countMap.put(lChar, countMap.get(lChar) - 1);
+22                if (countMap.get(lChar) == 0) {
+23                    countMap.remove(lChar);
+24                }
+25                left++;
+26            }
+27
+28            // 更新满足条件的最大长度
+29            maxLen = Math.max(maxLen, right - left + 1);
+30        }
+31
+32        return maxLen;
+33    }
+34}
