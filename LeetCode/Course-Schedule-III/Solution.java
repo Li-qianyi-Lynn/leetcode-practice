c607a8b51fe6c48fcaa7225644f7edfb7970a236
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-24 23:47:52 -0700

    Add solution for Course Schedule III

--- LeetCode/Course-Schedule-III/Solution.java
+++ LeetCode/Course-Schedule-III/Solution.java
@@ -1,12 +1,26 @@
-courses[i] = [days[i], deadline[i]]
-
-courses = [[100,200],[200,1300],[1000,1250],[2000,3200]]
-
-Arrange courses = [[100, 200], [1000, 1250], [200, 1300], [2000, 3200]]
-
-i = 0, 100 <= 200 , True
-i = 1, 100 + 1000 <= 1250, True
-i = 2, 100 + 1000 + 200 <= 1300, True
-i = 3, 100 + 1000 + 200 + 2000 <= 3200, False
-
-return 3
+1class Solution {
+2    public int scheduleCourse(int[][] courses) {
+3        // 按 deadline 升序排序
+4        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+5        
+6        // 大顶堆，存已选课程的 duration
+7        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+8        
+9        long totalTime = 0;
+10        
+11        for (int[] course : courses) {
+12            int duration = course[0];
+13            int lastDay = course[1];
+14            
+15            totalTime += duration;
+16            maxHeap.offer(duration);
+17            
+18            if (totalTime > lastDay) {
+19                // 超时，弹出当前耗时最大的课程
+20                totalTime -= maxHeap.poll();
+21            }
+22        }
+23        
+24        return maxHeap.size();
+25    }
+26}
