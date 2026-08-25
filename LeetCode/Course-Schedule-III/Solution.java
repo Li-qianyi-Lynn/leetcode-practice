f5ae2ed2c18cfd7020beb89c62d630d2bdae5eec
Qianyi Li <li.qianyi@northeastern.edu>
2026-08-24 23:54:21 -0700

    Add solution for Course Schedule III

--- LeetCode/Course-Schedule-III/Solution.java
+++ LeetCode/Course-Schedule-III/Solution.java
@@ -1,26 +1,35 @@
 1class Solution {
 2    public int scheduleCourse(int[][] courses) {
-3        // 按 deadline 升序排序
-4        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
-5        
-6        // 大顶堆，存已选课程的 duration
-7        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-8        
-9        long totalTime = 0;
-10        
-11        for (int[] course : courses) {
-12            int duration = course[0];
-13            int lastDay = course[1];
-14            
-15            totalTime += duration;
-16            maxHeap.offer(duration);
-17            
-18            if (totalTime > lastDay) {
-19                // 超时，弹出当前耗时最大的课程
-20                totalTime -= maxHeap.poll();
-21            }
-22        }
-23        
-24        return maxHeap.size();
-25    }
-26}
+3        Arrays.sort(courses, (a,b) -> Integer.compare(a[1],b[1]));
+4
+5        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+6        int total = 0;
+7
+8        for (int[] course : courses) {
+9            int duration = course[0];
+10            int lastday = course[1];
+11            total += duration;
+12
+13            maxHeap.offer(duration);
+14            if (total > lastday) {
+15                total -= maxHeap.poll();
+16            }
+17
+18        }
+19        return maxHeap.size();
+20
+21
+22
+23    }
+24}
+25/**
+26
+27sort using lastDay
+28
+29maxHeap -> duration
+30
+31
+32
+33
+34
+35 */
