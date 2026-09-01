33818cb5edbe1ff4de244ab075e2e964885718ef
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-01 11:29:46 -0700

    Add solution for Copy List with Random Pointer

--- LeetCode/Copy-List-with-Random-Pointer/Solution.java
+++ LeetCode/Copy-List-with-Random-Pointer/Solution.java
@@ -17,30 +17,42 @@
 17    public Node copyRandomList(Node head) {
 18        if (head == null) return null;
 19        Node cur = head;
-20        HashMap<Node, Node> map = new HashMap<>();
-21        while (cur != null) {
-22            map.put(cur, new Node(cur.val));
-23            cur = cur.next;
-24        }
-25        cur = head;
-26        while (cur != null) {
-27            Node copyNode = map.get(cur);
-28            copyNode.random = map.get(cur.random);
-29            copyNode.next = map.get(cur.next);
-30            cur = cur.next;
-31        }
-32
-33        return map.get(head);
-34       
-35        
-36    }
-37}
-38/**
-39hashmap node -> copy node (val)
-40node.random = map.get(node.random)
-41node.next = map.get(node.next)
-42
-43
-44
+20        while (cur != null) {
+21            Node newNode = new Node (cur.val);
+22            newNode.next = cur.next;
+23            cur.next = newNode;
+24            cur = newNode.next;
+25           
+26        }
+27        cur = head;
+28        while (cur != null) {
+29            if (cur.random != null) {
+30                cur.next.random = cur.random.next;
+31            }
+32            cur = cur.next.next;
+33        }
+34        cur = head;
+35        Node dummyHead = new Node(0);
+36        Node copyCur = dummyHead;
+37
+38        while (cur != null) {
+39            Node copy = cur.next;
+40            cur.next = copy.next;
+41            copyCur.next = copy;
+42            copyCur = copy;
+43            cur = cur.next;
+44        }
 45
-46 */
+46        return dummyHead.next;
+47        
+48    }
+49}
+50/**
+51hashmap node -> copy node (val)
+52node.random = map.get(node.random)
+53node.next = map.get(node.next)
+54
+55
+56
+57
+58 */
