85a1249a43369ca048789df7fe2a6477f6a6704d
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-02 20:55:46 -0700

    Add solution for Nth Digit

--- LeetCode/Nth-Digit/Solution.java
+++ LeetCode/Nth-Digit/Solution.java
@@ -1,42 +1,41 @@
 1class Solution {
 2    public int findNthDigit(int n) {
-3        int len = 1;
-4        long count = 9;
-5        long start = 1;
-6
-7        while (n > count) {
-8            n -= count;
-9            len++;
-10            start *= 10;
-11            count = len * start * 9;
-12
-13        }
-14
-15        long num = start + (n-1)/len;
-16        int index = (n - 1) % len;
-17        return Long.toString(num).charAt(index) - '0';
-18    }
-19}
-20/**
-21 n.        count
-220-9         9
-2310-99       180
-24100-999      270
-25
-2610 1
-2711 0
-2812 1
-2913 1
-3014 1
-3115 2
-32...
-3330.2
-342  2
+3        if (n <= 9) {
+4            return n;
+5
+6        }
+7        int len = 1;
+8        long count = 9;
+9        long start = 1;
+10
+11        while (n > count) {
+12            n -= count;
+13            len++;
+14            start *= 10;
+15            count = len * start * 9;
+16
+17        }
+18
+19        long num = start + (n - 1)/ len;
+20        int idx = (n-1) % len;
+21
+22        return Long.toString(num).charAt(idx) - '0';
+23        
+24    }
+25}
+26/**
+27
+28
+291-9 9 n
+3010-99 90* 2 = 180 digits
+31100-999 900* 3 = 2700 digits
+32
+33n > 9
+34n - 9 去比180
 35
-36len
-37count: 
-38start
+36
+37num ?
+38第几位
 39
 40
-41
-42 */
+41 */
