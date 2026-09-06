d964908b0a2edfb20d2b6b257b53da9f9d5a33e0
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-05 21:46:59 -0700

    Add solution for Top K Frequent Elements

--- LeetCode/Top-K-Frequent-Elements/Solution.java
+++ LeetCode/Top-K-Frequent-Elements/Solution.java
@@ -1,40 +1,33 @@
-1
-2
-3class Solution {
-4    public int[] topKFrequent(int[] nums, int k) {
-5        HashMap<Integer, Integer> map = new HashMap<>();
-6        for (int num : nums) {
-7            map.put(num, map.getOrDefault(num,0)+1);
+1class Solution {
+2    public int[] topKFrequent(int[] nums, int k) {
+3        int[] res = new int[k];
+4        // map: store num(key) and its frequency(value)
+5        Map<Integer, Integer> map = new HashMap<>();
+6        for (int i = 0; i < nums.length; i++) {
+7            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
 8        }
-9
-10        int[] res = new int[k];
-11        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)-> Integer.compare(a[1],b[1]));
-12        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-13            int freq = entry.getValue();
-14            int num = entry.getKey();
-15            int[] cur = new int[]{num, freq};
-16
-17            if (minHeap.size() < k) {
-18                minHeap.offer(cur);
-19            } else {
-20                if (minHeap.peek()[1] < freq) {
-21                    minHeap.poll();
-22                    minHeap.offer(cur);
-23                }
-24            }
-25        }
+9        //put K-V pait to the pq, and do the comperation
+10        // min heap: heap topwill store the least frequent element, if the size is larger than k, we can just pop out he top
+11        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+12        for(Map.Entry<Integer, Integer> pair: map.entrySet()) {
+13            int freq = pair.getValue();
+14            int num = pair.getKey();
+15            int[] element = new int[]{num, freq};
+16            pq.offer(element);
+17
+18            if (pq.size() > k) {
+19                pq.poll();
+20
+21            }
+22
+23        }
+24
+25        // add num to the res list;
 26        for (int i = 0; i < k; i++) {
-27            res[i] = minHeap.poll()[0];
-28        }
-29        return res;   
-30    }
-31}
-32
-33/**
-34
-35
-36heap <Integer, freq>
-37
-38
-39
-40 */
+27            res[i] = pq.poll()[0];
+28
+29        }
+30        return res;
+31        
+32    }
+33}
