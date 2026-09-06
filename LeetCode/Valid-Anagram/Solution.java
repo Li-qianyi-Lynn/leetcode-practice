9448e2698ecc42fad68d1e13597897a1abccda01
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-05 22:55:46 -0700

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.java
+++ LeetCode/Valid-Anagram/Solution.java
@@ -2,27 +2,27 @@
 2    public boolean isAnagram(String s, String t) {
 3        if (s.length() != t.length()) {
 4            return false;
-5        }
-6        HashMap<Character, Integer> mapS = new HashMap<>();
-7        HashMap<Character, Integer> mapT = new HashMap<>();
-8        int n = s.length();
-9        for (int i = 0; i < n; i++) {
-10            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i),0)+1);
-11            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i),0)+1);
-12        }
-13
-14        for (Map.Entry<Character, Integer> entryS : mapS.entrySet()) {
-15            char ch = entryS.getKey();
-16            int countS = entryS.getValue();
-17
-18            if (!mapT.getOrDefault(ch, 0).equals(countS)) {
+5
+6        }
+7        int[] arrS = new int[26];
+8        int[] arrT = new int[26];      
+9
+10        for (char c : s.toCharArray()) {
+11            arrS[c-'a']++;
+12        } 
+13        for (char c : t.toCharArray()) {
+14            arrT[c-'a']++;
+15        } 
+16
+17        for (int i = 0; i< 26; i++) {
+18            if (arrS[i] != arrT[i]) {
 19                return false;
 20
 21            }
 22
 23        }
 24        return true;
-25
+25        
 26        
 27    }
 28}
