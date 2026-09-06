d5cec84c717b44308378049347fa36bc1633707e
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-05 22:02:37 -0700

    Add solution for Top K Frequent Elements

--- LeetCode/Top-K-Frequent-Elements/Solution.java
+++ LeetCode/Top-K-Frequent-Elements/Solution.java
@@ -1,33 +1,50 @@
 1class Solution {
 2    public int[] topKFrequent(int[] nums, int k) {
 3        int[] res = new int[k];
-4        // map: store num(key) and its frequency(value)
-5        Map<Integer, Integer> map = new HashMap<>();
-6        for (int i = 0; i < nums.length; i++) {
-7            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+4        int len = nums.length;
+5        HashMap<Integer, Integer> map = new HashMap<>();
+6        for (int num : nums) {
+7            map.put(num, map.getOrDefault(num,0)+1);
 8        }
-9        //put K-V pait to the pq, and do the comperation
-10        // min heap: heap topwill store the least frequent element, if the size is larger than k, we can just pop out he top
-11        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-12        for(Map.Entry<Integer, Integer> pair: map.entrySet()) {
-13            int freq = pair.getValue();
-14            int num = pair.getKey();
-15            int[] element = new int[]{num, freq};
-16            pq.offer(element);
-17
-18            if (pq.size() > k) {
-19                pq.poll();
-20
-21            }
-22
-23        }
-24
-25        // add num to the res list;
-26        for (int i = 0; i < k; i++) {
-27            res[i] = pq.poll()[0];
-28
-29        }
-30        return res;
-31        
-32    }
-33}
+9
+10        List<Integer>[] bucket = new List[len + 1];
+11        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+12            int num = entry.getKey();
+13            int freq = entry.getValue();
+14            if (bucket[freq] == null) {
+15                bucket[freq] = new ArrayList<>();
+16            }
+17            bucket[freq].add(num);
+18
+19    
+20        }
+21
+22        for (int i = len; i >= 0; i--) {
+23            if (bucket[i] != null && k > 0) {
+24                for (int bu : bucket[i]) {
+25                    res[k-1] = bu;
+26                    k--;
+27
+28                }
+29            }
+30
+31        }
+32        return res;
+33        
+34    }
+35}
+36
+37/**
+38bucket sort
+39
+40map: num -> freq
+41int[len+1] bucket -> 
+42i: freq 
+43bucket[i] : num
+44
+45from end to the begining
+46if bucket[i] != null, add bucket[i] to the res
+47
+48
+49
+50 */
