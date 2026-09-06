96e429008a2fd53003fe6052469e910b4b54aef6
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-05 22:52:51 -0700

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.java
+++ LeetCode/Valid-Anagram/Solution.java
@@ -1,33 +1,28 @@
 1class Solution {
 2    public boolean isAnagram(String s, String t) {
-3        // pruning 
-4        if (s.length() != t.length()) {
-5            return false;
-6        }
-7        int n = s.length();
-8        int[] map = new int[26];
+3        if (s.length() != t.length()) {
+4            return false;
+5        }
+6        HashMap<Character, Integer> mapS = new HashMap<>();
+7        HashMap<Character, Integer> mapT = new HashMap<>();
+8        int n = s.length();
 9        for (int i = 0; i < n; i++) {
-10            int sc = s.charAt(i) - 'a';
-11            int tc = t.charAt(i) - 'a';
-12            // count chars in s and t
-13            map[sc] ++;
-14            map[tc] --;
-15        }
-16
-17        // iterate to check if it has non-0
-18        for (int m : map) {
-19            if (m != 0) {
-20                return false;
+10            mapS.put(s.charAt(i), mapS.getOrDefault(s.charAt(i),0)+1);
+11            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i),0)+1);
+12        }
+13
+14        for (Map.Entry<Character, Integer> entryS : mapS.entrySet()) {
+15            char ch = entryS.getKey();
+16            int countS = entryS.getValue();
+17
+18            if (!mapT.getOrDefault(ch, 0).equals(countS)) {
+19                return false;
+20
 21            }
-22        }
-23        return true;
-24    }
-25}
-26/**
-27
-28anagram:chars same , count same
-29input: two strings 
-30output: boolean
-31
-32int[]-> char:count
-33 */
+22
+23        }
+24        return true;
+25
+26        
+27    }
+28}
