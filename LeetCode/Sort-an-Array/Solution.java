64a2aa286d4c3acb84a70253b7b2dfdc664f3304
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-08 11:00:11 -0700

    Add solution for Sort an Array

--- LeetCode/Sort-an-Array/Solution.java
+++ LeetCode/Sort-an-Array/Solution.java
@@ -1,38 +1,42 @@
 1class Solution {
 2    public int[] sortArray(int[] nums) {
-3        quickSort(nums, 0, nums.length - 1);
-4        return nums;
-5    }
-6    
-7    private void quickSort(int[] nums, int left, int right) {
-8        if (left >= right) return;
+3        if (nums.length == 1) {
+4            return nums;
+5
+6        }
+7        mergeSort(nums, 0, nums.length-1);
+8        return nums;
 9        
-10        int pivotIndex = partition(nums, left, right);
-11        quickSort(nums, left, pivotIndex - 1);
-12        quickSort(nums, pivotIndex + 1, right);
-13    }
-14    
-15    private int partition(int[] nums, int left, int right) {
-16        // 随机选择 pivot 避免最坏情况
-17        int randomIndex = left + (int)(Math.random() * (right - left + 1));
-18        swap(nums, randomIndex, right);
-19        
-20        int pivot = nums[right];
-21        int i = left;
-22        
-23        for (int j = left; j < right; j++) {
-24            if (nums[j] < pivot) {
-25                swap(nums, i, j);
-26                i++;
-27            }
-28        }
-29        swap(nums, i, right);
-30        return i;
-31    }
-32    
-33    private void swap(int[] nums, int i, int j) {
-34        int temp = nums[i];
-35        nums[i] = nums[j];
-36        nums[j] = temp;
-37    }
-38}
+10    }
+11
+12    private void mergeSort(int[] nums, int l, int r) {
+13        if (l >= r) return; 
+14        int mid = l + (r-l)/2;
+15        mergeSort(nums, l, mid);
+16        mergeSort(nums, mid+1, r);
+17        merge(nums, l, mid, r);
+18
+19    }
+20
+21    private void merge(int[] nums, int left, int mid, int right) {
+22        int[] temp = new int[right - left + 1]; // left- mid  mid+1 - right
+23        int s1 = left;
+24        int s2 = mid+1;
+25        int k = 0;
+26
+27        while (s1 <= mid && s2 <= right) {
+28            if (nums[s1] <= nums[s2]) {
+29                temp[k++] = nums[s1++];
+30            } else {
+31                temp[k++] = nums[s2++];
+32            }
+33
+34        }
+35
+36        while (s1 <= mid) temp[k++] = nums[s1++];
+37        while (s2 <= right) temp[k++] = nums[s2++];
+38
+39        System.arraycopy(temp, 0, nums, left, temp.length);
+40
+41    }
+42}
