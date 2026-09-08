8b63615ea334541b305a245c3027b16251ee9361
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-08 15:01:10 -0700

    Add solution for House Robber II

--- LeetCode/House-Robber-II/Solution.java
+++ LeetCode/House-Robber-II/Solution.java
@@ -1,8 +1,37 @@
-[2,3,2]
-[1,2,3,1]
-[1,2,3]
-[1,2,1,1]
-[1,2,3,4,5,1,2,3,4,5]
-[200,3,140,20,10]
-[1]
-[94,40,49,65,21,21,106,80,92,81,679,4,61,6,237,12,72,74,29,95,265,35,47,1,61,397,52,72,37,51,1,81,45,435,7,36,57,86,81,72]
+1class Solution {
+2    public int rob(int[] nums) {
+3        
+4        if (nums.length <= 1) {
+5            return nums[0];
+6
+7        }
+8        int n = nums.length-1;
+9        int p1 =  robHelper(nums, 0, n-1);
+10        int p2 =  robHelper(nums, 1, n);
+11        return Math.max(p1,p2);
+12
+13
+14    }
+15    private int robHelper(int[] nums, int start, int end) {
+16        int[] dp = new int[nums.length];
+17        if (start == end) {
+18            return nums[start];
+19        }
+20        
+21        dp[start] = nums[start];
+22        dp[start+1] = Math.max(nums[start], nums[start+1]);
+23        for (int i = start+2; i <= end; i++) {
+24            dp[i] = Math.max(dp[i-1], dp[i-2]+ nums[i]);
+25
+26        }
+27        return dp[end];
+28        
+29    }
+30}
+31/**
+32
+331- n
+340- n-1
+35
+36
+37 */
