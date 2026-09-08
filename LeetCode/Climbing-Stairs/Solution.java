6b6bbc0e0add90a3f55aa6d0b351941780e21456
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-08 14:43:32 -0700

    Add solution for Climbing Stairs

--- LeetCode/Climbing-Stairs/Solution.java
+++ LeetCode/Climbing-Stairs/Solution.java
@@ -1,18 +1,27 @@
 1class Solution {
 2    public int climbStairs(int n) {
-3        if ( n <= 2) {
+3        if (n <= 2) {
 4            return n;
-5
-6        }
-7        int first = 1;
-8        int second = 2;
+5        }
+6        int first = 1;
+7        int second = 2;
+8        int third = 0;
 9        for (int i = 3; i <= n; i++) {
-10            int third = first + second;
+10            third = first + second;
 11            first = second;
 12            second = third;
-13        }
-14        return second;
-15
-16        
-17    }
-18}
+13
+14
+15        }
+16        return third;
+17
+18
+19        
+20    }
+21}
+22/**
+23dp[]: 
+24
+251.
+262.
+27 */
