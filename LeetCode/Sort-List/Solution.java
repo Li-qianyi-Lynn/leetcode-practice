7b5f854a36bdc4781a40f66f63068736cb0e0e39
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-08 10:28:56 -0700

    Add solution for Sort List

--- LeetCode/Sort-List/Solution.java
+++ LeetCode/Sort-List/Solution.java
@@ -10,30 +10,61 @@
 10 */
 11class Solution {
 12    public ListNode sortList(ListNode head) {
-13        if (head == null) {
-14            return null;
-15
-16        }
-17        ListNode cur = head;
+13    // 1. 递归基准条件：空链表或只有一个节点，天然有序
+14        if (head == null || head.next == null) {
+15            return head;
+16
+17        }
 18
-19        PriorityQueue <ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
-20        while (cur != null) {
-21            pq.offer(cur);
-22            cur = cur.next;
-23
-24        }
-25
-26        ListNode dummy = new ListNode(-1);
-27        ListNode tail = dummy;
-28        while (!pq.isEmpty()) {
-29            ListNode poll = pq.poll();
-30            tail.next = poll;
-31            tail = tail.next;
-32
-33        }
-34        tail.next = null;
-35        return dummy.next;
-36        
-37        
-38    }
-39}
+19        ListNode prev = null;
+20        ListNode slow = head;
+21        ListNode fast = head;   
+22
+23        while (fast != null && fast.next != null) {
+24            prev = slow;
+25            slow = slow.next;
+26            fast = fast.next.next;
+27
+28        }
+29
+30        prev.next = null;
+31
+32        ListNode left = sortList(head);
+33        ListNode right = sortList(slow);
+34
+35        return merge(left, right);
+36    }
+37
+38    private ListNode merge(ListNode l1, ListNode l2) {
+39        ListNode dummy = new ListNode (0);
+40        ListNode cur = dummy;
+41
+42        while (l1 != null && l2 != null) {
+43            if (l1.val < l2.val) {
+44                cur.next = l1;
+45                l1 = l1.next;
+46
+47            } else {
+48                cur.next = l2;
+49                l2 = l2.next;
+50
+51            }
+52            cur = cur.next;
+53        }
+54
+55        if (l1 != null ) {
+56            cur.next = l1;
+57        } 
+58
+59        if (l2 != null ) {
+60            cur.next = l2;
+61        }
+62
+63        return dummy.next;
+64    }
+65}
+66
+67/**
+68space O(1)
+69
+70 */
