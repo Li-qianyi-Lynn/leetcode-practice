afc1e78cfeac7988ad44a23df02dce6b2924f070
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-08 14:37:22 -0700

    Add solution for House Robber

--- LeetCode/House-Robber/Solution.java
+++ LeetCode/House-Robber/Solution.java
@@ -4,25 +4,19 @@
 4            return nums[0];
 5
 6        }
-7        int n = nums.length;
-8        int[] dp = new int[n];
+7
+8        int[] dp = new int[nums.length];
 9        dp[0] = nums[0];
-10        dp[1] = Math.max(nums[1], nums[0]);
-11        for (int i = 2; i < n; i++) {
-12            dp[i] = Math.max(dp[i-1], dp[i-2]+ nums[i]);
-13
-14        }
-15        return dp[n-1];
-16
-17        
-18    }
-19}
-20/**
+10        dp[1] = Math.max(nums[0], nums[1]);
+11        for (int i = 2; i < nums.length; i++) {
+12            dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
+13        }
+14        return dp[nums.length-1];
+15
+16        
+17    }
+18}
+19/**
+20dp[i] = max(dp[i-1], dp[i-2]+nums[i])
 21
-22dp[]: max money
-23
-24
-25
-26
-27
-28 */
+22 */
