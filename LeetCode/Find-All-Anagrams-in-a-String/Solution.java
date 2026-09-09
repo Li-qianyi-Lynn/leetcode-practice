e434f4a55dbfb815525ffd4e4850c08341e83aa0
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-08 22:39:24 -0700

    Add solution for Find All Anagrams in a String

--- LeetCode/Find-All-Anagrams-in-a-String/Solution.java
+++ LeetCode/Find-All-Anagrams-in-a-String/Solution.java
@@ -1,43 +1,44 @@
 1class Solution {
 2    public List<Integer> findAnagrams(String s, String p) {
-3       int[] mapS = new int[26];  //window
-4        int[] mapP = new int[26];
-5        int k = p.length();
-6
-7        
-8        for (char c : p.toCharArray()) {
-9            mapP[c-'a']++;
+3        int[] window = new int[26]; // keep k 
+4        int[] target = new int[26];
+5
+6        // init target
+7        for (char c : p.toCharArray()) {
+8            target[c - 'a']++;
+9
 10        }
-11        /**
-12        
-13        Input: s = "cbaebabacd", p = "abc"
-14                     ｜
-15                       |
-16                     l:i-k
-17        
-18         */
-19        
-20        int n = s.length();
-21        List<Integer> list = new ArrayList<>();
-22        for (int i = 0; i < n; i++) {
-23            // i add right.- window uodate
-24            mapS[s.charAt(i) - 'a']++;
-25            
-26            // if i>=k  remove left -window update
-27            if (i>=k) {
-28                int idx = s.charAt(i-k) - 'a';
-29                mapS[idx]--;
-30            }
-31            //  check boundary and compare if meet target
+11        int k = p.length();
+12        int n = s.length();
+13        List<Integer> res = new ArrayList<>();
+14        // int l = r -k+1;
+15
+16        // sliding window
+17        for (int r = 0; r < n; r++) {
+18            //add r to the window
+19            char c = s.charAt(r);
+20            window[c-'a']++;
+21            // if length > k ,remove l in the window
+22            if (r >= k) {
+23                char leftChar = s.charAt(r-k);
+24                window[leftChar - 'a']--;
+25            }
+26
+27            // check freq and char
+28            if (r >= k-1 && Arrays.equals(window, target)) {
+29                res.add(r-k+1);
+30
+31            }
 32
-33            if (i >= k-1 && Arrays.equals(mapS,mapP)) {
-34                list.add(i-k+1);
-35                    
-36            }
-37        }
-38        return list;
-39
-40
-41
-42    }
-43}
+33        }
+34        return res;
+35
+36        
+37        
+38    }
+39}
+40/**
+41k 
+42
+43
+44 */
