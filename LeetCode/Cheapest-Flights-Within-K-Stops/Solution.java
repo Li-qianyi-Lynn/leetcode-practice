40217b95ac2f53eb82c58652e8225fe13549dc35
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-12 20:29:18 -0700

    Add solution for Cheapest Flights Within K Stops

--- LeetCode/Cheapest-Flights-Within-K-Stops/Solution.java
+++ LeetCode/Cheapest-Flights-Within-K-Stops/Solution.java
@@ -1,51 +1,48 @@
 1class Solution {
 2    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
 3        Map<Integer, List<int[]>> map = new HashMap<>();
-4        for (int i = 0; i < n; i++) {
-5            map.put(i, new ArrayList<>());
-6        }
-7
-8        for (int[] flight : flights) {
-9            int f = flight[0];
-10            int t = flight[1];
-11            int p = flight[2];
-12            map.get(f).add(new int[]{t,p});
-13        }
-14
-15        Deque<int[]> dq = new ArrayDeque<>();
-16        dq.offerLast(new int[]{src,0});
-17
-18        int[] minDist = new int[n];
-19        Arrays.fill(minDist, Integer.MAX_VALUE);
-20        minDist[src] = 0;
-21
-22        int stops = 0;
-23        while (!dq.isEmpty() && stops <= k) {
-24            int size = dq.size();
-25            for (int i = 0; i < size; i++) {
-26                int[] cur = dq.pollFirst();
-27                int toCity = cur[0];
-28                int cost = cur[1];
-29
-30                for (int[] edge : map.get(toCity)) {
-31                    int toNext = edge[0];
-32                    int price = edge[1];
-33
-34                    if (cost + price < minDist[toNext]) {
-35                        minDist[toNext] = cost + price;
-36                        dq.offerLast(new int[]{toNext, minDist[toNext]});
-37
-38                    }
-39
-40                }
+4        for (int[] flight : flights) {
+5            map.putIfAbsent(flight[0], new ArrayList<>());
+6            map.get(flight[0]).add(new int[]{flight[1],flight[2]});
+7        }
+8
+9
+10        Deque<int[]> dq = new ArrayDeque<>();
+11        dq.offerLast(new int[]{src,0});
+12
+13        int[] minDist = new int[n];
+14        Arrays.fill(minDist, Integer.MAX_VALUE);
+15        minDist[src] = 0;
+16
+17        int stops = 0;
+18        while (!dq.isEmpty() && stops <= k) {
+19            int size = dq.size();
+20            for (int i = 0; i < size; i++) {
+21                int[] cur = dq.pollFirst();
+22                int toCity = cur[0];
+23                int cost = cur[1];
+24                if (map.containsKey(toCity)) {
+25                    for (int[] edge : map.get(toCity)) {
+26                        int toNext = edge[0];
+27                        int price = edge[1];
+28
+29                        if (cost + price < minDist[toNext]) {
+30                            minDist[toNext] = cost + price;
+31                            dq.offerLast(new int[]{toNext, minDist[toNext]});
+32
+33                        }
+34                    }
+35
+36                }
+37                
+38
+39            }
+40            stops ++;
 41
-42            }
-43            stops ++;
-44
-45        }
-46
-47        return minDist[dst] == Integer.MAX_VALUE ? -1 :minDist[dst];
-48        
-49        
-50    }
-51}
+42        }
+43
+44        return minDist[dst] == Integer.MAX_VALUE ? -1 :minDist[dst];
+45        
+46        
+47    }
+48}
