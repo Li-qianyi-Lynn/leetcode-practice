268fbda98c1832adc92b9a7d90cc0819f0e8de63
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-14 15:40:45 -0700

    Add solution for Network Delay Time

--- LeetCode/Network-Delay-Time/Solution.java
+++ LeetCode/Network-Delay-Time/Solution.java
@@ -1,65 +1,63 @@
 1class Solution {
 2    public int networkDelayTime(int[][] times, int n, int k) {
-3     
-4        // adj map ui -> vi, wi
-5        HashMap<Integer, List<int[]>> map = new HashMap<>();
-6        for (int[] time : times) {
-7            map.putIfAbsent(time[0], new ArrayList<>());
-8            map.get(time[0]).add(new int[]{time[1],time[2]});
-9        }
+3        // adj map node -> nei
+4        HashMap<Integer,List<int[]>> adj = new HashMap<>();
+5        for (int[] time : times) {
+6            adj.putIfAbsent(time[0], new ArrayList<>());
+7            adj.get(time[0]).add(new int[]{time[1], time[2]});
+8        }
+9
 10
-11        // dist[]: to store nodes distance
-12        int[] dist = new int[n+1];
-13        Arrays.fill(dist, Integer.MAX_VALUE);
-14        dist[k] = 0;
-15
-16        // pq: minHeap dist[i], node i
-17        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0],b[0]));
-18        pq.offer(new int[]{0,k});
-19
+11        // minheap: sort by time
+12
+13        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0],b[0]));// time, targetNode
+14        // int[] dist: to store distance from source to  the node i
+15        int[] dist = new int[n+1];
+16        Arrays.fill(dist, Integer.MAX_VALUE);
+17        dist[k] = 0;
+18        // process node
+19        pq.offer(new int[]{0,k});
 20        while (!pq.isEmpty()) {
 21            int[] poll = pq.poll();
-22            int d = poll[0];
-23            int node = poll[1];
+22            int node = poll[1];
+23            int t = poll[0]; // t+ dist[Node]
 24
-25            if (d > dist[node]) {
+25            if (dist[node]< t) {
 26                continue;
-27
-28            }
-29
-30            if (map.containsKey(node)) {
-31                for (int[] nei : map.get(node)) {
-32                    int nextNode = nei[0];
-33                    int weight = nei[1];
-34
-35                    if (dist[node] + weight < dist[nextNode]) {
-36                       dist[nextNode] =  dist[node] + weight;
-37                       pq.offer(new int[]{dist[nextNode],nextNode});
-38
-39
-40
-41                    }
-42
-43                }
-44
-45            }
-46        }
+27            }
+28
+29            if (adj.containsKey(node)) {
+30                for (int[] nei : adj.get(node)) {
+31                    int nexNode = nei[0];
+32                    int weight = nei[1];
+33
+34                    if ( weight+ dist[node] < dist[nexNode]) {
+35                        dist[nexNode] =  weight+ dist[node];
+36                        pq.offer(new int[]{dist[nexNode], nexNode});
+37                    }
+38                }
+39            }
+40        }
+41
+42        // for loop dist to get the max num
+43        int res = dist[1];
+44        for (int i = 1; i <= n; i++) {
+45            if (dist[i] == Integer.MAX_VALUE) {
+46                return -1;
 47
-48
-49        // for loop to get res
+48            }
+49            res = Math.max(res,dist[i]);
 50
-51        int res = 0;
-52        for (int i = 1; i < n+1; i++) {
-53            if (dist[i] == Integer.MAX_VALUE) {
-54                return -1;
-55
-56            }
-57
-58            res = Math.max(res, dist[i]);
-59
-60        }
-61        return res;
-62        
-63    }
-64}
-65
+51
+52        }
+53        return res;
+54        
+55    }
+56}
+57/**
+58
+591. adj map
+60
+61
+62
+63 */
