169df47d418cf2fd9524947d0701833461eb33b1
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-14 17:03:33 -0700

    Add solution for Longest Common Subsequence

--- LeetCode/Longest-Common-Subsequence/Solution.java
+++ LeetCode/Longest-Common-Subsequence/Solution.java
@@ -7,37 +7,37 @@
 7        for (int i = 1; i <= m; i++) {
 8            char c1 = text1.charAt(i-1);
 9            for (int j = 1; j <= n; j++) {
-10                char c2 = text2.charAt(j-1);
-11                if (c1 == c2) {
-12                    dp[i][j] = dp[i-1][j-1]+1;
-13
-14                } else {
-15                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-16
-17                }
-18
-19            }
-20
-21        }
-22        return dp[m][n];
-23
+10               
+11                char c2 = text2.charAt(j-1);
+12
+13                if (c1 == c2) {
+14                    dp[i][j] = dp[i-1][j-1] + 1;
+15
+16                } else {
+17                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+18                }
+19
+20            }
+21
+22        }
+23        return dp[m][n];
 24        
-25    }
-26}
-27/**
-28
-29dp[i][j]
-30tex1 i tex2 j common subsequence
-31
-32  - a b c d e
-33-
-34a
-35
-36c
+25        
+26        
+27    }
+28}
+29/**
+30
+31dp
+32i：代表 text1 的前 i 个字符（即 text1 中下标从 0 到 i-1 的子串）。
+33j：代表 text2 的前 j 个字符（即 text2 中下标从 0 到 j-1 的子串）
+34
+35匹配上：
+36dp[i][j] = dp[i-1][j-1] +1
 37
-38e
-39
-40char1 == char2 dp[i][j] = dp[i-1][j-1]+1;
-41
-42!=         dp[i][j] = Math.max(dp[i-1][j]+dp[i][j-1]);
+38没匹配上
+39放弃 text2 的第 j 个字符，看 text1 的前 i 个字符和 text2 的前 j-1 个字符能匹配多少，即 dp[i][j-1]。
+40放弃 text1 的第 i 个字符，看 text1 的前 i-1 个字符和 text2 的前 j 个字符能匹配多少，即 dp[i-1][j]。
+41dp[i][j] = Math.max(dp[i-1][j]，dp[i][j-1])
+42
 43 */
