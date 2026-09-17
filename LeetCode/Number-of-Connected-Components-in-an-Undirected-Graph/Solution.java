20e1b8e1d9c57004758d406ea7009020754c80cd
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-17 16:41:03 -0700

    Add solution for Number of Connected Components in an Undirected Graph

--- LeetCode/Number-of-Connected-Components-in-an-Undirected-Graph/Solution.java
+++ LeetCode/Number-of-Connected-Components-in-an-Undirected-Graph/Solution.java
@@ -1,55 +1,31 @@
-1class Solution {
-2    class UnionFind {
-3        private int[] parent;
-4        private int[] rank;
-5        private int count;
-6
-7        public UnionFind(int n) {
-8            this.count = n;
-9            this.parent = new int[n];
-10            this.rank = new int[n];
-11            for (int i = 0; i < n; i++) {
-12                parent[i] = i;
-13                rank[i] = 1;
-14            }
-15        }
-16
-17        public int find(int i) {
-18            if (parent[i] != i) {
-19                parent[i] = find(parent[i]); // 路径压缩
-20            }
-21            return parent[i];
-22        }
-23
-24        public boolean union(int x, int y) {
-25            int rootX = find(x);
-26            int rootY = find(y);
-27
-28            if (rootX != rootY) {
-29                if (rank[rootX] < rank[rootY]) {
-30                    parent[rootX] = rootY;
-31                } else if (rank[rootX] > rank[rootY]) {
-32                    parent[rootY] = rootX;
-33                } else {
-34                    parent[rootY] = rootX;
-35                    rank[rootX]++;
-36                }
-37                count--; // 连通分量数 -1
-38                return true;
-39            }
-40            return false;
-41        }
-42
-43        public int getCount() {
-44            return count;
-45        }
-46    }
-47
-48    public int countComponents(int n, int[][] edges) {
-49        UnionFind uf = new UnionFind(n);
-50        for (int[] edge : edges) {
-51            uf.union(edge[0], edge[1]);
-52        }
-53        return uf.getCount();
-54    }
-55}
+1public class Solution {
+2    public int countComponents(int n, int[][] edges) {
+3        List<List<Integer>> adj = new ArrayList<>();
+4        boolean[] visit = new boolean[n];
+5        for (int i = 0; i < n; i++) {
+6            adj.add(new ArrayList<>());
+7        }
+8        for (int[] edge : edges) {
+9            adj.get(edge[0]).add(edge[1]);
+10            adj.get(edge[1]).add(edge[0]);
+11        }
+12
+13        int res = 0;
+14        for (int node = 0; node < n; node++) {
+15            if (!visit[node]) {
+16                dfs(adj, visit, node);
+17                res++;
+18            }
+19        }
+20        return res;
+21    }
+22
+23    private void dfs(List<List<Integer>> adj, boolean[] visit, int node) {
+24        visit[node] = true;
+25        for (int nei : adj.get(node)) {
+26            if (!visit[nei]) {
+27                dfs(adj, visit, nei);
+28            }
+29        }
+30    }
+31}
