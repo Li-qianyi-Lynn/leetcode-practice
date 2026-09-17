50e387c45ae8e14e438f6653432394ad850d9c81
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-17 16:44:46 -0700

    Add solution for Number of Connected Components in an Undirected Graph

--- LeetCode/Number-of-Connected-Components-in-an-Undirected-Graph/Solution.java
+++ LeetCode/Number-of-Connected-Components-in-an-Undirected-Graph/Solution.java
@@ -1,31 +1,66 @@
-1public class Solution {
-2    public int countComponents(int n, int[][] edges) {
-3        List<List<Integer>> adj = new ArrayList<>();
-4        boolean[] visit = new boolean[n];
-5        for (int i = 0; i < n; i++) {
-6            adj.add(new ArrayList<>());
-7        }
-8        for (int[] edge : edges) {
-9            adj.get(edge[0]).add(edge[1]);
-10            adj.get(edge[1]).add(edge[0]);
-11        }
-12
-13        int res = 0;
-14        for (int node = 0; node < n; node++) {
-15            if (!visit[node]) {
-16                dfs(adj, visit, node);
-17                res++;
-18            }
-19        }
-20        return res;
+1class UnionFind {
+2    int[] parents;
+3    int counts;
+4    UnionFind (int n) {
+5        parents = new int[n];
+6        counts = n;
+7        for (int i = 0; i < n; i++) {
+8            parents[i] = i;
+9        }
+10    }
+11
+12    public int find(int i) {
+13        if (parents[i] == i) {
+14            parents[i] = i;
+15        } else {
+16            parents[i] = find(parents[i]);
+17        }
+18 
+19        return parents[i];
+20
 21    }
 22
-23    private void dfs(List<List<Integer>> adj, boolean[] visit, int node) {
-24        visit[node] = true;
-25        for (int nei : adj.get(node)) {
-26            if (!visit[nei]) {
-27                dfs(adj, visit, nei);
-28            }
-29        }
-30    }
-31}
+23    public boolean union(int i, int j) {
+24        int rootI = find(i);
+25        int rootJ = find(j);
+26   
+27
+28        if (rootI == rootJ) {
+29            return false;
+30
+31        } 
+32            
+33        parents[rootI] = rootJ;
+34        counts--;
+35        return true;
+36        
+37
+38    }
+39}
+40
+41class Solution {
+42    public int countComponents(int n, int[][] edges) {
+43        if (n == 1) {
+44            return 1;
+45
+46        }
+47        UnionFind uf = new UnionFind(n);
+48        for (int[] edge : edges) {
+49            uf.union(edge[0],edge[1]);
+50        }
+51
+52        
+53        return uf.counts;
+54        
+55    }
+56}
+57/**
+58union find
+59
+60find node roots 
+61
+62union them 
+63count how many roots?
+64
+65
+66 */
