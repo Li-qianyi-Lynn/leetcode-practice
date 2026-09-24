19e645fd18b1b6a051043dda6ab08f4409fd0b99
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-23 21:52:23 -0700

    Add solution for Subsets II

--- LeetCode/Subsets-II/Solution.java
+++ LeetCode/Subsets-II/Solution.java
@@ -1,30 +1,34 @@
 1import java.util.ArrayList;
 2import java.util.Arrays;
 3import java.util.List;
-4
-5class Solution {
-6    public List<List<Integer>> subsetsWithDup(int[] nums) {
-7        List<List<Integer>> result = new ArrayList<>();
-8        // 1. 排序，让相同元素相邻
-9        Arrays.sort(nums);
-10        backtrack(nums, 0, new ArrayList<>(), result);
-11        return result;
-12    }
-13
-14    private void backtrack(int[] nums, int start, List<Integer> current, List<List<Integer>> result) {
-15        // 收集当前子集
-16        result.add(new ArrayList<>(current));
+4// [[],[2],[2,2],[2,2,2]]
+5// start: 2
+6// cur: [2,2]
+7// res: [[],[2], [2,2],[2,2,2]]
+8
+9class Solution {
+10    public List<List<Integer>> subsetsWithDup(int[] nums) {
+11        List<List<Integer>> result = new ArrayList<>();
+12        // 1. 排序，让相同元素相邻
+13        Arrays.sort(nums);
+14        backtrack(nums, 0, new ArrayList<>(), result);
+15        return result;
+16    }
 17
-18        for (int i = start; i < nums.length; i++) {
-19            // 同一树层遇到重复元素，跳过以避免重复子集
-20            if (i > start && nums[i] == nums[i - 1]) {
-21                continue;
-22            }
-23            current.add(nums[i]);
-24            // 递归进入下一层
-25            backtrack(nums, i + 1, current, result);
-26            // 回溯撤销选择
-27            current.remove(current.size() - 1);
-28        }
-29    }
-30}
+18    private void backtrack(int[] nums, int start, List<Integer> current, List<List<Integer>> result) {
+19        // 收集当前子集
+20        result.add(new ArrayList<>(current));
+21
+22        for (int i = start; i < nums.length; i++) { //0
+23            // 同一树层遇到重复元素，跳过以避免重复子集 【2，2, 2】 
+24            if (i > start && nums[i] == nums[i - 1]) {
+25                continue;
+26            }
+27            current.add(nums[i]);
+28            // 递归进入下一层
+29            backtrack(nums, i + 1, current, result);
+30            // 回溯撤销选择
+31            current.remove(current.size() - 1);
+32        }
+33    }
+34}
