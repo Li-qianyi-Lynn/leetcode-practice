a289128cbe86e2f8a0060a2f7dd5e9418b167a6d
Qianyi Li <li.qianyi@northeastern.edu>
2026-09-24 20:51:06 -0700

    Add solution for Combination Sum III

--- LeetCode/Combination-Sum-III/Solution.java
+++ LeetCode/Combination-Sum-III/Solution.java
@@ -1,46 +1,40 @@
 1class Solution {
 2    List<List<Integer>> res = new ArrayList<>();
 3    public List<List<Integer>> combinationSum3(int k, int n) {
-4        if (n ==1) {
-5            return new ArrayList<>();
-6
-7        }
-8        backtracking(k,n,0,new ArrayList<>(),1);
-9        return res;
-10        
-11    }
-12
-13    private void backtracking(int k, int n, int curSum, List<Integer> cur, int num) {
-14        // base case
-15        int size = cur.size();
-16        if (size == k && curSum == n) {
-17            res.add(new ArrayList<>(cur));
-18            return;
-19        }
-20
-21        if (size > k || curSum > n || num > 9 || num < 1) {
-22            return;
-23        }
-24
-25      
-26        cur.add(num);
-27        backtracking(k,n, curSum + num, cur ,num+1);
-28        cur.remove(cur.size()-1);
+4        
+5        backtracking(k,n,0,new ArrayList<>(),1);
+6        return res;
+7        
+8    }
+9
+10    private void backtracking(int k, int n, int curSum, List<Integer> cur, int num) {
+11        // base case
+12        int size = cur.size();
+13        if (size == k && curSum == n) {
+14            res.add(new ArrayList<>(cur));
+15            return;
+16        }
+17
+18        if (size > k || curSum > n ) {
+19            return;
+20        }
+21        for (int i = num; i <= 9; i++) {
+22            cur.add(i);
+23            backtracking(k,n, curSum + i, cur ,i+1);
+24            cur.remove(cur.size()-1);
+25
+26        }        
+27
+28
 29
-30
-31        backtracking(k,n, curSum, cur ,num+1);
-32        
-33
+30    }
+31}
+32/**
+33backtracking: 
 34
-35
-36    }
-37}
-38/**
-39backtracking: 
-40
-411- 9 
-42if curSum == n && size == k , add the list to the res List
-43
-44
-45
-46 */
+351- 9 
+36if curSum == n && size == k , add the list to the res List
+37
+38
+39
+40 */
